910a36771de0b755eaae979ee78bed6adbf7e537
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-20 13:29:58 +0200

    backend api models.py - fix user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,3 +11,15 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
         fields = ('url', 'subject', 'body', 'pk')
+
+##################################################################
+
+class User(models.Model):
+
+    class Role(models.IntegerChoices):
+        AGGREGATOR = 1
+        BUILDING_MANAGER = 2
+
+    role_id = models.IntegerField(choices=Role.choices)
+    password = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
