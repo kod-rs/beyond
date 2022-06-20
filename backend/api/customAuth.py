9e9b9f3e5995e37b5c9d903f9575689f4ce106c4
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-20 19:44:18 +0200

    backend api customAuth.py - add get_user

--- backend/api/customAuth.py
+++ backend/api/customAuth.py
@@ -1,13 +1,41 @@
 from django.contrib.auth.backends import BaseBackend
 
+from .models import User
+
 class ModelBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None):
         # Check the username/password and return a user.
-        ...
+        # ...
 
-        print("custom auth")
+        if not request:
+            return None
 
-        return True
+        print("custom auth", username, password)
+
+        print(f"{User.objects=}")
+        for i in User.objects.all():
+            if i.username == username:
+                print("-------------------------------")
+            print(i.username, i.id)
+
+        print(20 * " -")
+
+        try:
+            user = User.objects.all().get(username=username)
+            # print(f"{User.objects=}")
+            # print(user)
+            print(80 * "-")
+            print("found user")
+            print(user)
+
+            return user
+
+        except User.DoesNotExist:
+            return None
+
+        # # return User()
+        #
+        # # return True
         # UserModel = get_user_model()
         # if username is None:
         #     username = kwargs.get(UserModel.USERNAME_FIELD)
@@ -19,3 +47,10 @@ class ModelBackend(BaseBackend):
         #     # Run the default password hasher once to reduce the timing
         #     # difference between an existing and a non-existing user (#20760).
         #     UserModel().set_password(password)
+
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
