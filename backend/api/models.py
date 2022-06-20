a9a49eb94da7c19898528249bb92e3e029f2664a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-20 19:44:18 +0200

    backend api models.py - test

--- backend/api/models.py
+++ backend/api/models.py
@@ -14,11 +14,13 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
 
 ##################################################################
 
+
 class User(models.Model):
 
     class Role(models.IntegerChoices):
         AGGREGATOR = 1
         BUILDING_MANAGER = 2
+        AGGREGATOR_AND_BUILDING_MANAGER = 3
 
     role_id = models.IntegerField(choices=Role.choices)
     password = models.CharField(max_length=200)
