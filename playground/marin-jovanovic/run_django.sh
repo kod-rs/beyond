3405fde17eccf2473e4ae667a86643681f957830
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-20 13:29:58 +0200

    playground marin-jovanovic run_django.sh - add commands

--- playground/marin-jovanovic/run_django.sh
+++ playground/marin-jovanovic/run_django.sh
@@ -1,8 +1,18 @@
-#source venv/bin/activate
-#python3 manage.py migrate
-#python3 manage.py runserver
+# collection of commands
+# not intended to be run as a script
 
+# activate venv
+source venv/bin/activate
 
 # to add new model
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+# run backend
+python3 manage.py runserver
+
+# check tables postgresql
+select * from information_schema.tables where table_name like 'api%'
+
+# seed database
+python3 src_db/seed.py
