f49e7ee126a1d724755658d5b539968f384db6e4
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-20 12:13:31 +0200

    backend api customAuth.py - migrate file

--- backend/api/customAuth.py
+++ backend/api/customAuth.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.backends import BaseBackend
 
-class MyBackend(BaseBackend):
+class ModelBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None):
         # Check the username/password and return a user.
         ...
