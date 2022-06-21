591dff20ebc861a16544215248700317ff9d272a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-21 15:40:59 +0200

    playground marin-jovanovic post.py - add keycloak test functions

--- playground/marin-jovanovic/post.py
+++ playground/marin-jovanovic/post.py
@@ -3,17 +3,48 @@ import json
 import requests
 
 
-def main():
+def login():
     url = 'http://127.0.0.1:8000/login/'
     data = {
-        "username": "Tracey_Ramos",
-        "password": "c"
+        # "username": "Tracey_Ramos",
+        "username": "Tracey_Ramosfff",
+        "password": "lhecqksgih"
     }
 
     t = requests.post(url, data=data, verify=False)
 
     print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
 
+def keycloak_list_users():
+    url = 'http://localhost:8080/admin'
+    data = {
+        # "grant_type": "password",
+        # "client_id": "myclient",
+        # "username": "myuser",
+        # "password": "myuser"
+    }
+
+    t = requests.get(url, data=data, verify=False)
+    print(t)
+    print(t.text)
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def keycloak_obtain_token():
+    url = 'http://localhost:8080/realms/myuser/protocol/openid-connect/token'
+    data = {
+        "grant_type": "password",
+        "client_id": "myclient",
+        "username": "myuser",
+        "password": "myuser"
+    }
+
+    t = requests.post(url, data=data, verify=False)
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def main():
+    keycloak_list_users()
 
 if __name__ == '__main__':
     main()
