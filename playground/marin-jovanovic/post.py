ba96a8e500d75fb82fabe1552c19806d1932ff45
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-21 20:07:24 +0200

    playground marin-jovanovic post.py - fix login test

--- playground/marin-jovanovic/post.py
+++ playground/marin-jovanovic/post.py
@@ -31,12 +31,14 @@ def keycloak_list_users():
     print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
 
 def keycloak_obtain_token():
-    url = 'http://localhost:8080/realms/myuser/protocol/openid-connect/token'
+
+    # url = 'http://localhost:8080/realms/myuser/protocol/openid-connect/token'
+    url = "http://localhost:8080/realms/beyond/protocol/openid-connect/token"
     data = {
         "grant_type": "password",
-        "client_id": "myclient",
-        "username": "myuser",
-        "password": "myuser"
+        "client_id": "beyond_cli",
+        "username": "user1",
+        "password": "user1"
     }
 
     t = requests.post(url, data=data, verify=False)
@@ -44,7 +46,8 @@ def keycloak_obtain_token():
     print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
 
 def main():
-    keycloak_list_users()
+    # keycloak_list_users()
+    keycloak_obtain_token()
 
 if __name__ == '__main__':
     main()
