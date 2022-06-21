7614c0b89415859a901d336cf1baf38c9ea4a8e1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-06-21 15:40:59 +0200

    playground marin-jovanovic common.sh - rename & add keycloak commands

--- playground/marin-jovanovic/common.sh
+++ playground/marin-jovanovic/common.sh
@@ -20,3 +20,13 @@ DELETE FROM x
 # seed database
 python3 src_db/seed.py
 
+# run vue
+nvm use 16.15.1
+npm run serve
+
+# keycloak-18.0.1
+# serves on localhost:8080
+./bin/kc.sh start-dev
+
+#
+http://localhost:8080/realms/beyond_realm/
