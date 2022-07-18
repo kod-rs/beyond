da7e0f8cb662aafaf6f2db4f48a74ef28b4d0689
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-18 20:22:46 +0200

    webpack.config.js - update wp

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue?$/,
+                exclude: /(node_modules)/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true,
+        
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+          },
+        //   ,
+
+        proxy: {
+            '/api*': {
+            // '/login*': {
+                    // Forward frontend dev server request for /api to django dev server
+              target: 'http://localhost:8000/',
+            }
+        }
+
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000',
+            djangoApi: 'http://localhost:8000'
+        })
+    }
+}
