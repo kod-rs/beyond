4f5264ff34f5991547351fafc32a5f433699e5eb
Ivan <ivan.cindric95@gmail.com>
2022-07-19 10:23:37 +0200

    webpack config js - OWASP - Fingerprint Web Server mitigate

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+            "X-Powered-By": 'Beyond'
           },
         //   ,
 
