fde483956085ebfef841fea96e7560b80566e159
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-20 16:32:44 +0200

    playground marin-jovanovic git.sh -

---
+++ playground/marin-jovanovic/git.sh
@@ -0,0 +1,18 @@
+git add /home/marin/Documents/git/kod-rs/beyond/backend/urls.py
+git commit -m "backend urls.py - add test paths"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/views.py
+git commit -m "backend api views.py - add views"
+git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/git.sh
+git commit -m "playground marin-jovanovic git.sh - "
+git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/kc.py
+git commit -m "playground marin-jovanovic kc.py - wip"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/customAuth.py
+git commit -m "backend api customAuth.py - remove custom auth"
+git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/post.py
+git commit -m "playground marin-jovanovic post.py - wip"
+git add /home/marin/Documents/git/kod-rs/beyond/kc/main.py
+git commit -m "kc main.py - wip"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/settings/dev.py
+git commit -m "backend settings dev.py - update dependency"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/tests.py
+git commit -m "backend api tests.py - add tests"
