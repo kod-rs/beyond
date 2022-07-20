0064b5b261578a84319ba4eaf2f6d990eafe8cbc
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-20 16:32:44 +0200

    backend api views.py - add views

--- backend/api/views.py
+++ backend/api/views.py
@@ -95,3 +95,81 @@ class SnippetDetail(APIView):
         #
         # else:
         #     return JsonResponse({'is_auth_correct': True})
+
+
+from rest_framework import viewsets, views
+from . import models
+    # , serializers
+from rest_framework import status
+from django.http.response import JsonResponse
+from rest_framework.exceptions import PermissionDenied
+
+class BankViewSet(viewsets.ModelViewSet):
+    """
+    Bank endpoint
+    This endpoint has all configured keycloak roles
+    """
+    # serializer_class = serializers.BankSerializer
+    # queryset = models.Bank.objects.all()
+    keycloak_roles = {
+        'GET': ['director', 'judge', 'employee'],
+        'POST': ['director', 'judge', ],
+        'UPDATE': ['director', 'judge', ],
+        'DELETE': ['director', 'judge', ],
+        'PATCH': ['director', 'judge', 'employee'],
+    }
+
+    def list(self, request):
+        """
+        Overwrite method
+        You can especify your rules inside each method
+        using the variable 'request.roles' that means a
+        list of roles that came from authenticated token.
+        See the following example bellow:
+        """
+        # list of token roles
+        print(request.roles)
+
+        # Optional: get userinfo (SUB attribute from JWT)
+        print(request.userinfo)
+
+        return super().list(self, request)
+
+
+class CarViewSet(viewsets.ViewSet):
+    """
+    Car endpoint
+    This endpoint has not configured keycloak roles.
+    That means all methods will be allowed to access.
+    """
+    def list(self, request):
+        return JsonResponse({"detail": "success"}, status=status.HTTP_200_OK)
+
+
+class JudgementView(views.APIView):
+    """
+    Judgement endpoint
+    This endpoint has configured keycloak roles only GET method.
+    Other HTTP methods will be allowed.
+    """
+    keycloak_roles = {
+        'GET': ['judge'],
+    }
+
+    def get(self, request, format=None):
+        """
+        Overwrite method
+        You can especify your rules inside each method
+        using the variable 'request.roles' that means a
+        list of roles that came from authenticated token.
+        See the following example bellow:
+        """
+        # list of token roles
+        print(request.roles)
+
+        # Optional: get userinfo (SUB attribute from JWT)
+        print(request.userinfo)
+
+        return super().get(self, request)
+
+# http://127.0.0.1:8000/api/callback
