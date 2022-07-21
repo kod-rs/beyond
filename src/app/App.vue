a42c50e5d4a1e47c111218bdd75566306778828a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-21 16:30:35 +0200

    src app App.vue - add header

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,13 +1,29 @@
 <template>
-    <div class="jumbotron">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 offset-sm-3">
-                    <router-view></router-view>
+
+    <div>
+        <div>
+            <router-link :to="{ name: 'dashboard' }">dashboard</router-link> |
+            <router-link :to="{ name: 'login' }">login</router-link> |
+            <router-link :to="{ name: 'home' }">home</router-link> |
+            <router-link :to="{ name: 'messages' }">db communicator test</router-link>
+        </div>
+
+
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 offset-sm-3">
+                        <router-view>
+                        </router-view>
+                    </div>
                 </div>
             </div>
         </div>
+
+
     </div>
+
+
 </template>
 
 <script>
