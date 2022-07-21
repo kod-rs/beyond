4d284e57b161e4b5948794430a35e20ce9fd8bb9
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-21 16:30:35 +0200

    playground marin-jovanovic common.sh - update

--- playground/marin-jovanovic/common.sh
+++ playground/marin-jovanovic/common.sh
@@ -2,16 +2,7 @@
 # not intended to be run as a script
 
 
-# check tables postgresql
-select * from information_schema.tables where table_name like 'api%'
 
-# clear table 'x' postgresql
-DELETE FROM x
 
-# seed database
-python3 src_db/seed.py
 
-# run vue
-nvm use 16.15.1
-npm run serve
-
+pip3 freeze > requirements.txt
