8a18f668b2d5e7204dcda01ed0c477e7ab9bc193
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-21 11:32:43 +0200

    playground marin-jovanovic common.sh - refactor

--- playground/marin-jovanovic/common.sh
+++ playground/marin-jovanovic/common.sh
@@ -1,15 +1,6 @@
 # collection of commands
 # not intended to be run as a script
 
-# activate venv
-source venv/bin/activate
-
-# to add new model
-python3 manage.py makemigrations
-python3 manage.py migrate
-
-# run backend
-python3 manage.py runserver
 
 # check tables postgresql
 select * from information_schema.tables where table_name like 'api%'
@@ -24,15 +15,3 @@ python3 src_db/seed.py
 nvm use 16.15.1
 npm run serve
 
-# keycloak-18.0.1
-# serves on localhost:8080
-./bin/kc.sh start-dev
-
-#
-http://localhost:8080/realms/beyond_realm/
-
-# todo for prod
-https://www.keycloak.org/docs/latest/server_installation/
-ssl
-
-curl -L -X GET 'http://localhost:8080/realms/beyond/protocol/openid-connect/certs'
