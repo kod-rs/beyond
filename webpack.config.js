cea9673100a11d416560cef26fd4efd5b1b238a1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-21 16:30:35 +0200

    webpack.config.js - cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path');
+// var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -26,12 +26,12 @@ module.exports = {
     devServer: {
         historyApiFallback: true,
         
-        // headers: {
+        headers: {
         //     "Access-Control-Allow-Origin": "*",
         //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
         //     "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
-        //     "X-Powered-By": 'Beyond'
-        //   },
+            "X-Powered-By": 'Beyond'
+          },
 
         proxy: {
             '/api*': {
