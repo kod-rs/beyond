8cbe66c1d53defc1d0f67841f742073ecc6e7d99
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-21 11:32:43 +0200

    playground marin-jovanovic git.sh - update

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,16 +1,14 @@
+git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/common.sh
+git commit -m "playground marin-jovanovic common.sh - refactor"
+git add /home/marin/Documents/git/kod-rs/beyond/req
+git commit -m "req - wip"
+git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/django.sh
+git commit -m "playground marin-jovanovic django.sh - refactor"
 git add /home/marin/Documents/git/kod-rs/beyond/backend/api/views.py
-git commit -m "backend api views.py - update"
+git commit -m "backend api views.py - cleanup"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/tests.py
+git commit -m "backend api tests.py - wip"
+git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/keycloak.sh
+git commit -m "playground marin-jovanovic keycloak.sh - refactor"
 git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/git.sh
-git commit -m "playground marin-jovanovic git.sh - "
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak_manager.py
-git commit -m "backend api keycloak_manager.py - move"
-git add /home/marin/Documents/git/kod-rs/beyond/src/login/LoginPage.vue
-git commit -m "src login LoginPage.vue - cleanup "
-git add /home/marin/Documents/git/kod-rs/beyond/src/index/IndexPage.vue
-git commit -m "src index IndexPage.vue - cleanup"
-git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/post.py
-git commit -m "playground marin-jovanovic post.py - add tests"
-git add /home/marin/Documents/git/kod-rs/beyond/src/_services/user.service.js
-git commit -m "src _services user.service.js - fix api calls"
-git add /home/marin/Documents/git/kod-rs/beyond/package.json
-git commit -m "package.json - change vue port"
+git commit -m "playground marin-jovanovic git.sh - update"
