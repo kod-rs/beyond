a8b7ae3c254a4b11f96b34a5a5fc52a997fb01a2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-21 16:30:34 +0200

    src page1  - add test page

---
+++ src/page1/Page1.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>page 1</p>
+    </div>
+</template>
+
+<script>
+import { userService } from '../_services';
+
+export default {
+    data () {
+        return {
+            user: {},
+            users: []
+        }
+    },
+    created () {
+        this.user = JSON.parse(localStorage.getItem('user'));
+        this.users.loading = true;
+        userService.getAll().then(users => this.users = users);
+    }
+};
+</script>
