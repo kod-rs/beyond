488d1fbd294cf76cdcc205ceade7af08adab8ace
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-22 17:53:48 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,14 +1,16 @@
-git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/common.sh
-git commit -m "playground marin-jovanovic common.sh - refactor"
-git add /home/marin/Documents/git/kod-rs/beyond/req
-git commit -m "req - wip"
-git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/django.sh
-git commit -m "playground marin-jovanovic django.sh - refactor"
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/views.py
-git commit -m "backend api views.py - cleanup"
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/tests.py
-git commit -m "backend api tests.py - wip"
-git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/keycloak.sh
-git commit -m "playground marin-jovanovic keycloak.sh - refactor"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/settings/dev.py
+git commit -m "backend settings dev.py - update"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/urls.py
+git commit -m "backend urls.py - start script"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/RS256.key
+git commit -m "backend api keycloak RS256.key - "
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/pem.key
+git commit -m "backend api keycloak pem.key - "
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/pem.py
+git commit -m "backend api keycloak pem.py - generate keys"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/keycloak_manager.py
+git commit -m "backend api keycloak keycloak_manager.py - wip"
+git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/RSA-OAEP.key
+git commit -m "backend api keycloak RSA-OAEP.key - "
 git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/git.sh
-git commit -m "playground marin-jovanovic git.sh - update"
+git commit -m "playground marin-jovanovic git.sh - "
