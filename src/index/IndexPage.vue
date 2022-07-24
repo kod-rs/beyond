9e8890bf8bef1653f74439c81dbb0a5f5e17adb1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-24 21:38:39 +0200

    src index IndexPage.vue - ref

--- src/index/IndexPage.vue
+++ src/index/IndexPage.vue
@@ -1,36 +1,32 @@
 <template>
     <div>
         <p>map placeholder</p>
-        <!-- <h1>Hi {{user.firstName}}!</h1> -->
-
-        <p>this is  index</p>
+        <h1>Hi {{ user.username }}!</h1>
 
         <em v-if="users.loading">Loading users...</em>
         <ul v-if="users.length">
             <li v-for="user in users" :key="user.id">
-                {{user.firstName + ' ' + user.lastName}}
+                {{ user.firstName + ' ' + user.lastName }}
             </li>
         </ul>
-        <p>
-            <router-link to="/login">Logout</router-link>
-        </p>
+
     </div>
 </template>
 
 <script>
-import { userService } from '../_services';
+// import { userService } from '../_services';
 
 export default {
-    data () {
+    data() {
         return {
             user: {},
             users: []
         }
     },
-    created () {
+    created() {
         this.user = JSON.parse(localStorage.getItem('user'));
         this.users.loading = true;
-        userService.getAll().then(users => this.users = users);
+        // userService.getAll().then(users => this.users = users);
     }
 };
 </script>
