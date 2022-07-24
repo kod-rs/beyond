9f06426e9a79ee5df5705ef2c3d3ed447f2541e7
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-24 21:38:39 +0200

    src index.js - ref

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import Vue from 'vue';
 import { router } from './_helpers';
 import App from './app/App';
 
-// setup fake backend
-import { configureFakeBackend } from './_helpers';
-configureFakeBackend();
-
 new Vue({
     el: '#app',
     router,
