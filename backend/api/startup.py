b8f98178820b6aef325f423e1ba31151e334b160
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-24 18:48:45 +0200

    backend api startup.py - add startup script

---
+++ backend/api/startup.py
@@ -0,0 +1,24 @@
+import pathlib
+
+from .keycloak.pem import generate_keys
+from .keycloak.keys_manager import create_tables
+from .assets.printer import print_logo
+
+def run_startup():
+
+    print_logo()
+    print("setting up")
+    create_tables()
+    print(" 1 key table created")
+    generate_keys()
+    print(" 2 keycloak keys generated")
+
+    from .keycloak.keycloak_manager import test
+    test()
+
+def main():
+    run_startup()
+
+
+if __name__ == '__main__':
+    main()
