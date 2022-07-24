0143831461b7217a0b90dadc8726e54458ec8827
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-24 19:49:43 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,16 +1,23 @@
-git add /home/marin/Documents/git/kod-rs/beyond/backend/settings/dev.py
-git commit -m "backend settings dev.py - update"
-git add /home/marin/Documents/git/kod-rs/beyond/backend/urls.py
-git commit -m "backend urls.py - start script"
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/RS256.key
-git commit -m "backend api keycloak RS256.key - "
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/pem.key
-git commit -m "backend api keycloak pem.key - "
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/pem.py
-git commit -m "backend api keycloak pem.py - generate keys"
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/keycloak_manager.py
-git commit -m "backend api keycloak keycloak_manager.py - wip"
-git add /home/marin/Documents/git/kod-rs/beyond/backend/api/keycloak/RSA-OAEP.key
-git commit -m "backend api keycloak RSA-OAEP.key - "
-git add /home/marin/Documents/git/kod-rs/beyond/playground/marin-jovanovic/git.sh
+git add /home/tea/Documents/github/kod-rs/beyond/src/app/App.vue
+git commit -m "src app App.vue - update route"
+git add /home/tea/Documents/github/kod-rs/beyond/src/page1/Page1.vue
+git commit -m "src page1 Page1.vue - test page"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/keycloak/keycloak_manager.py
+git commit -m "backend api keycloak keycloak_manager.py - cleanup"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/views.py
+git commit -m "backend api views.py - update response"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/keycloak/keys_manager.py
+git commit -m "backend api keycloak keys_manager.py - cleanup"
+git add /home/tea/Documents/github/kod-rs/beyond/src/page2/Page2.vue
+git commit -m "src page2 Page2.vue - test page"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/startup.py
+git commit -m "backend api startup.py - cleanup"
+git add /home/tea/Documents/github/kod-rs/beyond/src/_services/user.service.js
+git commit -m "src _services user.service.js - remove authdata from localstorage"
+git add /home/tea/Documents/github/kod-rs/beyond/src/_helpers/router.js
+git commit -m "src _helpers router.js - fix routing for page1 & page2"
+
+git add /home/tea/Documents/github/kod-rs/beyond/src/index/IndexPage.vue
+git commit -m "src index IndexPage.vue - "
+git add /home/tea/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
