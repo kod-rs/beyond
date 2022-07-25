914f78a08cf6e9d07ed04190a5c6c9084f415885
kim <kim@pop-os.localdomain>
2022-07-25 22:53:22 +0200

    backend api view dbView.py - wip

--- backend/api/view/dbView.py
+++ backend/api/view/dbView.py
@@ -1,12 +1,18 @@
 from django.http import JsonResponse
 
 from backend.api.model.ip import IpCounter
+import json
 
 def pureDjangoView(request):
     # return HttpResponse("home page")
     t = IpCounter.objects.all()
+    print(t)
+    # print(json.loads(t))
+    print("----")
+    for i in t:
+        print(i, i.ip, i.counter)
     # for i in t:
     #     print(i, i["ip"], i["counter"])
     print(t)
-    print(json(t))
+    # print(json.loads(t))
     return JsonResponse({"a": "b"})
