6b159467a272853ca2170951a004a8a7c5f744d4
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-25 14:49:25 +0200

    backend api authenticate.py - add auth

---
+++ backend/api/authenticate.py
@@ -0,0 +1,32 @@
+from backend.api.keycloak.keycloak_manager import keycloak_obtain_token, get_roles, get_user_info
+
+
+def login(username, password):
+    res = keycloak_obtain_token(username, password)
+
+    if all((i in res) for i in
+           ["access_token", "expires_in", "refresh_expires_in", "refresh_token", "token_type", "id_token"]):
+        access_token = res["access_token"]
+        refresh_token = res["refresh_token"]
+
+        responseJson = {
+            "ok": True,
+            "id": "user.id",
+            "username": username,
+
+            "access_token": access_token,
+            "refresh_token": refresh_token
+        }
+    else:
+        responseJson = {
+            "ok": False,
+
+        }
+
+    return responseJson
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
