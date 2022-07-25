058bfd9cabce144da67b7d6e365484c2256f508a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-25 14:49:25 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,23 +1,22 @@
-git add /home/tea/Documents/github/kod-rs/beyond/src/app/App.vue
-git commit -m "src app App.vue - update route"
-git add /home/tea/Documents/github/kod-rs/beyond/src/page1/Page1.vue
-git commit -m "src page1 Page1.vue - test page"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/keycloak/keycloak_manager.py
-git commit -m "backend api keycloak keycloak_manager.py - cleanup"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/views.py
-git commit -m "backend api views.py - update response"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/keycloak/keys_manager.py
-git commit -m "backend api keycloak keys_manager.py - cleanup"
-git add /home/tea/Documents/github/kod-rs/beyond/src/page2/Page2.vue
-git commit -m "src page2 Page2.vue - test page"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/startup.py
-git commit -m "backend api startup.py - cleanup"
-git add /home/tea/Documents/github/kod-rs/beyond/src/_services/user.service.js
-git commit -m "src _services user.service.js - remove authdata from localstorage"
-git add /home/tea/Documents/github/kod-rs/beyond/src/_helpers/router.js
-git commit -m "src _helpers router.js - fix routing for page1 & page2"
-
-git add /home/tea/Documents/github/kod-rs/beyond/src/index/IndexPage.vue
-git commit -m "src index IndexPage.vue - "
+git add /home/tea/Documents/github/kod-rs/beyond/backend/settings/dev.py
+git commit -m "backend settings dev.py - add middleware"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/middleware
+git commit -m "backend api middleware  - "
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/authenticate.py
+git commit -m "backend api authenticate.py - add auth"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/view
+git commit -m "backend api view  - "
+git add /home/tea/Documents/github/kod-rs/beyond/playground/marin-jovanovic/django.sh
+git commit -m "playground marin-jovanovic django.sh - "
 git add /home/tea/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
+git add /home/tea/Documents/github/kod-rs/beyond/backend/urls.py
+git commit -m "backend urls.py - update paths"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/migrations/0007_ip.py
+git commit -m "backend api migrations 0007_ip.py - "
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/views.py
+git commit -m "backend api views.py - update view"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/model
+git commit -m "backend api model  - extract"
+git add /home/tea/Documents/github/kod-rs/beyond/backend/api/migrations/0008_rename_ip_ipcounter.py
+git commit -m "backend api migrations 0008_rename_ip_ipcounter.py - "
