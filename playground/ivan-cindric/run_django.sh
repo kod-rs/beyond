ee62be3e808ac2946afc931768735fc0c5749cbb
Ivan <ivan.cindric95@gmail.com>
2022-07-26 11:03:57 +0200

    playground ivan cindric - update run django

--- playground/ivan-cindric/run_django.sh
+++ playground/ivan-cindric/run_django.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#python3 ../manage.py migrate
-#python3 ../manage.pymakemigrations
+#python3 ../../manage.py migrate
+#python3 ../../manage.py makemigrations
 python3 ../../manage.py runserver
