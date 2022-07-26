1b4de3ff3d21f1763dc8414ce565c14f6f86358c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 11:56:21 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,22 +1,18 @@
-git add /home/tea/Documents/github/kod-rs/beyond/backend/settings/dev.py
-git commit -m "backend settings dev.py - add middleware"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/middleware
-git commit -m "backend api middleware  - "
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/authenticate.py
-git commit -m "backend api authenticate.py - add auth"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/view
-git commit -m "backend api view  - "
-git add /home/tea/Documents/github/kod-rs/beyond/playground/marin-jovanovic/django.sh
-git commit -m "playground marin-jovanovic django.sh - "
-git add /home/tea/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
+git add /home/kim/Documents/github/kod-rs/beyond/test/django.py
+git commit -m "test django.py - add login tests"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/urls.py
+git commit -m "backend urls.py - add index"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
-git add /home/tea/Documents/github/kod-rs/beyond/backend/urls.py
-git commit -m "backend urls.py - update paths"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/migrations/0007_ip.py
-git commit -m "backend api migrations 0007_ip.py - "
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/views.py
-git commit -m "backend api views.py - update view"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/model
-git commit -m "backend api model  - extract"
-git add /home/tea/Documents/github/kod-rs/beyond/backend/api/migrations/0008_rename_ip_ipcounter.py
-git commit -m "backend api migrations 0008_rename_ip_ipcounter.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/views.py
+git commit -m "backend api views.py - update index"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/keycloak/keycloak_manager.py
+git commit -m "backend api keycloak keycloak_manager.py - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.py
+git commit -m "playground marin-jovanovic git.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/keycloak/pem.py
+git commit -m "backend api keycloak pem.py - update key gen"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/ipCheckMiddleware.py
+git commit -m "backend api middleware ipCheckMiddleware.py - ip"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/keycloak.sh
+git commit -m "playground marin-jovanovic keycloak.sh - "
