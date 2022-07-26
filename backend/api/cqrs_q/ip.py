cd5a17b8dcef9d3da6a5ba55dad1420f2178a2b7
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 16:18:26 +0200

    backend api cqrs_q ip.py - move methods

--- backend/api/cqrs_q/ip.py
+++ backend/api/cqrs_q/ip.py
@@ -1,8 +1,7 @@
 from backend.api.model.ip import IpCounter
 
 
-def check_max_count(ip, max_brute_force_count):
+def check_max_count(ip, max_count):
     if IpCounter.objects.filter(ip=ip):
         ip_obj = IpCounter.objects.get(ip=ip)
-        print("current count", ip_obj.counter)
-        return ip_obj.counter > max_brute_force_count
+        return ip_obj.counter > max_count
