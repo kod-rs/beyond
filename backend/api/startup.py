81c8cc5185306bc4b5f220ae25451401014b40b7
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 21:34:23 +0200

    backend api startup.py - ref

--- backend/api/startup.py
+++ backend/api/startup.py
@@ -1,26 +1,9 @@
-import pathlib
-
-from .keycloak.pem import generate_keys
-# from .keycloak.keys_manager import create_tables
-from .assets.printer import print_logo
-
-# from django.apps import AppConfig
-# class MyAppConfig(AppConfig):
-#     name = 'beyond-api'
-#     verbose_name = "beyond api"
-#     def ready(self):
-#         print("startup code")
-#         pass # startup code here
+from backend.api.assets.printer import print_logo
 
 
 def run_startup():
-
     print_logo()
-    # print("setting up")
-    # create_tables()
-    # print(" 1 key table created")
-    # generate_keys()
-    # print(" 2 keycloak keys generated")
+
 
 def main():
     run_startup()
