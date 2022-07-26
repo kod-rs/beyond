aa49a92f007575cf354ee810333a78ee3b901138
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 21:34:23 +0200

    backend api migrations 0006_user_delete_snippet.py -

--- backend/api/migrations/0006_user_delete_snippet.py
+++ backend/api/migrations/0006_user_delete_snippet.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('api', '0005_alter_snippet_code_alter_snippet_created_and_more'),
     ]
@@ -13,8 +12,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('role_id', models.IntegerField(choices=[(1, 'Aggregator'), (2, 'Building Manager'), (3, 'Aggregator And Building Manager')])),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                                        serialize=False, verbose_name='ID')),
+                ('role_id', models.IntegerField(
+                    choices=[(1, 'Aggregator'), (2, 'Building Manager'),
+                             (3, 'Aggregator And Building Manager')])),
                 ('password', models.CharField(max_length=200)),
                 ('username', models.CharField(max_length=200)),
             ],
