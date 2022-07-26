30b41ec690292647d1062ead0d6c9fd495fd4134
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 21:34:23 +0200

    backend api migrations 0009_pemkeys.py -

--- backend/api/migrations/0009_pemkeys.py
+++ backend/api/migrations/0009_pemkeys.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('api', '0008_rename_ip_ipcounter'),
     ]
@@ -13,7 +12,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PemKeys',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                                        serialize=False, verbose_name='ID')),
                 ('key_type', models.CharField(max_length=200)),
                 ('key_value', models.CharField(max_length=2000)),
             ],
