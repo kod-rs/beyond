643d5836ab24afa9f1e9cd004eaa0388a37ebf5e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 13:22:04 +0200

    backend api authenticate.py - fix interface

--- backend/api/authenticate.py
+++ backend/api/authenticate.py
@@ -5,11 +5,18 @@ def login(username, password):
     res = keycloak_obtain_token(username, password)
 
     if all((i in res) for i in
-           ["access_token", "expires_in", "refresh_expires_in", "refresh_token", "token_type", "id_token"]):
+           [
+               "access_token",
+               "expires_in",
+               "refresh_expires_in",
+               "refresh_token",
+               "token_type",
+               "id_token"
+           ]):
         access_token = res["access_token"]
         refresh_token = res["refresh_token"]
 
-        responseJson = {
+        response = {
             "ok": True,
             "id": "user.id",
             "username": username,
@@ -18,24 +25,12 @@ def login(username, password):
             "refresh_token": refresh_token
         }
     else:
-        responseJson = {
+        response = {
             "ok": False,
-
         }
 
-    return responseJson
+    return response
+
 
 def check_tokens(access_token, refresh_token):
-    return is_valid(access_token)
-
-    # responseJson = {
-    #     "ok": is_valid(access_token),
-    # }
-    #
-    # return responseJson
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
+    return is_valid(access_token, refresh_token)
