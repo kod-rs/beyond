80cb32a1c65fa39b6de0660412ddc35dcac78976
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 17:41:15 +0200

    backend api model pem_keys.py - ref

--- backend/api/model/pem_keys.py
+++ backend/api/model/pem_keys.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class PemKeys(models.Model):
 
+class PemKeys(models.Model):
     key_type = models.CharField(max_length=200)
     key_value = models.CharField(max_length=2000)
