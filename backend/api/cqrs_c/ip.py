d923ea002a0f856a876ae84bd9956d8ef1b2d707
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 16:18:26 +0200

    backend api cqrs_c ip.py - move methods

--- backend/api/cqrs_c/ip.py
+++ backend/api/cqrs_c/ip.py
@@ -1,33 +1,31 @@
 from backend.api.model.ip import IpCounter
 
 
-def log_user_auth_atempt(ip):
+def log_user_auth_attempt(ip):
+    """
+    counter++
+
+    """
+
     if IpCounter.objects.filter(ip=ip):
-        # if in db increment
         ip_entry = IpCounter.objects.get(ip=ip)
-        print("in db", ip_entry, ip_entry.counter)
         ip_entry.counter += 1
         ip_entry.save()
     else:
-        # else add with def val 1
-        counter = 1
-        ip_entry = IpCounter(ip=ip, counter=counter)
-        print("not in db", ip_entry, ip_entry.counter)
+        ip_entry = IpCounter(ip=ip, counter=1)
         ip_entry.save()
 
 
-def auth_user(ip, count):
-    """reset user """
+def auth_user(ip):
+    """
+    reset counter to 0
+
+    """
 
     if IpCounter.objects.filter(ip=ip):
-        # if in db increment
         ip_entry = IpCounter.objects.get(ip=ip)
-        print("in db", ip_entry, ip_entry.counter)
-        ip_entry.counter = count
+        ip_entry.counter = 0
         ip_entry.save()
     else:
-        # else add with def val 1
-        counter = count
-        ip_entry = IpCounter(ip=ip, counter=counter)
-        print("not in db", ip_entry, ip_entry.counter)
+        ip_entry = IpCounter(ip=ip, counter=0)
         ip_entry.save()
