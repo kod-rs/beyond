711a16b10d10e3c5e30c5817025c21a010663488
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 16:18:26 +0200

    backend api keycloak keycloak_manager.py - wip

--- backend/api/keycloak/keycloak_manager.py
+++ backend/api/keycloak/keycloak_manager.py
@@ -1,45 +1,54 @@
 import json
-
 import jwt
 import requests
 from decouple import config
 
-from backend.api.keycloak.keys_manager import get_all_keys, remove_key
-
-
-def pretty_print_json(payload):
-    print(json.dumps(payload, indent=4, sort_keys=True))
-
-
-def get_links():
-    base = config("KEYCLOAK_URL")
-
-    links = {
-        "authorization-uri": base + "auth",
-        "user-info-uri": base + "userinfo",
-        "token-uri": base + "token",
-        "logout": base + "logout",
-        "jwk-set-uri": base + "certs"
-    }
-
-    return links
-
-
-def get_con():
-    return {
-        "realm": config("KEYCLOAK_REALM"),
-        "client id": config("KEYCLOAK_CLIENT_ID"),
-        "authorization grant type": "password",
-        "client secret": config("KEYCLOAK_CLIENT_SECRET"),
-        "scope": "openid profile"
-    }
-
+from backend.api.keycloak.config import get_links, get_con
+from backend.api.comm.comm import pretty_print_json
+from backend.api.cqrs_q.pem_keys import get_all_keys
+from backend.api.cqrs_c.pem_keys import remove_key
+from backend.api.keycloak.pem import generate_keys
+from django.core import serializers
 
 def get_roles(access_token):
+    # todo fix
     keys = get_all_keys()
-    if keys["status"]:
+    print("printing keys")
+    # for k in keys.iterator():
+    #     print(k)
+    # keys = serializers.serialize("python", keys)
 
-        for k_type, k in keys["payload"]:
+    print(30 * "-", "get roles", 30 * "-")
+    # print(keys)
+
+    # if not keys.exists():
+    if not keys:
+        generate_keys()
+        print("keys not exist")
+    else:
+        print("keys exist")
+
+    print("printing keys")
+    for k in keys.iterator():
+        print(k, k.key_type, k.key_value[:10])
+
+    # keys = serializers.serialize("python", keys)
+    keys = get_all_keys()
+    if not keys:
+        print("no keys")
+    else:
+
+        for i in keys.iterator():
+            k_type, k = i.key_type, i.key_value
+            print(f"{k_type=}")
+            print(f"{k=}")
+            # k = str.encode(k)
+            print(f"{access_token[:10]=}")
+
+
+
+            # k = bytes(k, "utf-8")
+            # print(type(k), k[:-1])
             try:
                 jwt_ = jwt.decode(
                     access_token,
@@ -51,10 +60,16 @@ def get_roles(access_token):
                 remove_key(k_type)
                 continue
 
+            except jwt.exceptions.DecodeError:
+                print("error decoding")
+                return []
+
+            except jwt.exceptions.ExpiredSignatureError:
+                print("refresh token dead?")
+                return []
+
             return jwt_["resource_access"][get_con()["client id"]]["roles"]
 
-    else:
-        print("no keys")
 
 
 def keycloak_obtain_token(username, password):
@@ -72,7 +87,7 @@ def keycloak_obtain_token(username, password):
     res = requests.post(url, data=data, verify=False)
 
     res_text = json.loads(res.text)
-
+    pretty_print_json(res_text)
     return res_text
 
 
@@ -112,8 +127,10 @@ def get_user_info(token):
 
 
 def is_valid(access_token, refresh_token):
+    print("is valid", access_token)
     res = get_user_info(access_token)
     if all((i in res) for i in ["email_verified", "preferred_username", "sub"]):
+        print(1)
         return {
             "is_valid": True,
             "access_token": access_token,
@@ -121,9 +138,15 @@ def is_valid(access_token, refresh_token):
             "test is_current_valid": True
         }
     else:
+        print(2)
+        roles = get_roles(access_token)
+        print(f"{roles=}")
+
         res = try_refresh_token(refresh_token)
+        print("res after refresh", res)
 
         if all((i in res) for i in ["access_token", "refresh_token"]):
+
 
             return {
                 "is_valid": True,
