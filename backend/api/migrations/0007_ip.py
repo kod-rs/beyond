c70cb9fd96e2e34fc3204da063d28dd09e4677c7
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-26 21:34:23 +0200

    backend api migrations 0007_ip.py -

--- backend/api/migrations/0007_ip.py
+++ backend/api/migrations/0007_ip.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('api', '0006_user_delete_snippet'),
     ]
@@ -13,7 +12,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Ip',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                                        serialize=False, verbose_name='ID')),
                 ('ip', models.CharField(max_length=200)),
                 ('counter', models.IntegerField()),
             ],
