8903aef0dd983adbb7c92ca6b939d2a55bd9526a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-27 19:10:31 +0200

    backend settings prod.py - reload

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -2,7 +2,8 @@
 
 import os
 import dj_database_url
-from .dev import *
+# from .dev import *
+from .dev import *  # NOQA
 
 ############
 # DATABASE #
@@ -23,3 +24,14 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 
 # Set to your Domain here (eg. 'django-vue-template-demo.herokuapp.com')
 ALLOWED_HOSTS = ['*']
+
+from .dev import *  # NOQA
+DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+MIDDLEWARE = [*MIDDLEWARE,
+              'django.middleware.security.SecurityMiddleware']
+SECURE_HSTS_SECONDS = 1
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_PRELOAD = True
