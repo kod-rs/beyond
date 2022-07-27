a77fecad1664f9d1bd36478325a649ec7a93703d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-27 11:59:38 +0200

    backend api middleware ipCheckMiddleware.py - add support for data and headers credentials

--- backend/api/middleware/ipCheckMiddleware.py
+++ backend/api/middleware/ipCheckMiddleware.py
@@ -1,3 +1,5 @@
+import json
+
 from decouple import config
 from django.http import JsonResponse
 from ipware import get_client_ip
@@ -32,43 +34,96 @@ class IpCheckMiddleware:
             print("The client's IP address is private")
 
         rejection = {
-            "rejected": True
+            "auth": {
+                "status": False,
+                "access-token": "",
+                "refresh-token": ""
+            },
+            "payload": {},
+            "debug": ""
         }
 
         if not ip:
             print("unable to get clients ip")
-
+            rejection["debug"] = "unable to get clients ip"
             return JsonResponse(rejection)
 
         if check_max_count(ip, self.max_brute_force_count):
             print("max try exceeded")
+            rejection["debug"] = "max try exceeded"
             return JsonResponse(rejection)
 
         log_user_auth_attempt(ip)
 
-        is_validated = None
+        # print("has headers", request.headers)
+        print("has body", request.body,  type(request.body))
+
+        username = None
+        password = None
+        access_token = None
+        refresh_token = None
+
         if request.headers:
-
             if all((i in request.headers) for i in ["username", "password"]):
-                print("using user pass")
+                username=request.headers["username"]
+                password=request.headers["password"]
 
-                is_validated = login(
-                    request.headers["username"],
-                    request.headers["password"]
-                )["ok"]
-
-            elif all((i in request.headers) for i in
+            if all((i in request.headers) for i in
                      ["access-token", "refresh-token"]):
-                print("using tokens")
 
-                is_validated = check_tokens(
-                    request.headers["access-token"],
-                    request.headers["refresh-token"]
-                )["is_valid"]
+                access_token = request.headers["access-token"]
+                refresh_token = request.headers["refresh-token"]
+
+        if request.body:
+            body_content = json.loads(request.body.decode("utf-8"))
+
+
+            if all((i in body_content) for i in ["username", "password"]):
+                username = body_content["username"]
+                password = body_content["password"]
+
+            if all((i in body_content) for i in
+                   ["access-token", "refresh-token"]):
+                access_token = body_content["access-token"]
+                refresh_token = body_content["refresh-token"]
+
+        print(f"{username=}")
+        print(f"{password=}")
+        print(f"{access_token=}")
+        print(f"{refresh_token=}")
+
+
+        is_validated = None
+        # if request.headers:
+
+        if username and password:
+            print("using user pass")
+
+            res = login(username, password)
+
+            is_validated = res["ok"]
+
+        elif access_token and refresh_token:
+
+            print("using tokens")
+
+            res = check_tokens(
+                access_token,
+                refresh_token
+            )
+
+            is_validated = res["is_valid"]
 
         if not is_validated:
+            rejection["debug"] = "not is_validated"
             return JsonResponse(rejection)
 
         else:
+            # todo
+            access_token = res["access_token"]
+            refresh_token = res["refresh_token"]
+
             auth_user(ip)
+            request.access_token = access_token
+            request.refresh_token = refresh_token
             return self.get_response(request)
