526f5cd206cf3f495c3c4bfcf252a6ca7c2bf5ee
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-27 19:06:00 +0200

    src page2 Page2.vue - ref

--- src/page2/Page2.vue
+++ src/page2/Page2.vue
@@ -2,7 +2,83 @@
     <div>
         <p>page 2</p>
     </div>
+
+    <nav class="navbar navbar-light bg-light">
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="type location" aria-label="Search">
+            <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+        </form>
+    </nav>
+
+    <div class="row">
+        <div class="col p-3 bg-primary text-white">search
+
+
+        </div>
+        <div class="col p-3 bg-dark text-white">map placeholder</div>
+        <div class="col p-3 bg-primary text-white">select options</div>
+    </div>
+
+
+
 </template>
 
 <script>
+
+
+// export default {
+//     data: () => ({
+//     }),
+//     mounted() {
+//         let recaptchaScript = document.createElement('script')
+//         recaptchaScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js')
+//         document.head.appendChild(recaptchaScript)
+//     },
+//     methods: {
+//     }
+// }
+
+
+
 </script>
+
+<style>
+/* .row {
+    margin-top: 40px;
+    padding: 0 10px;
+}
+
+.clickable {
+    cursor: pointer;
+}
+
+.panel-heading span {
+    margin-top: -20px;
+    font-size: 15px;
+}
+
+.table>thead>tr>th {
+    border-right: 1px solid #ddd;
+    border-bottom: 0;
+}
+
+.table>tbody>tr>td {
+    border-right: 1px solid #ddd;
+}
+
+.table>thead>tr>th:last-of-type {
+    border-right: 0px;
+}
+
+.table>tbody>tr>td:last-of-type {
+    border-right: 0px;
+}
+
+.panel-body {
+    padding: 0;
+}
+
+.panel-body>.table {
+    margin-bottom: 0px;
+} */
+</style>
