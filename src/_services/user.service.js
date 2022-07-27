bc1e42a8a79b7883437dcce8e59c4789693557d0
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-27 15:36:21 +0200

    src _services user.service.js - fix bug

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -3,7 +3,7 @@ import api from './api';
 export const userService = {
     login,
     logout,
-
+    testCRUD
 };
 
 
@@ -42,6 +42,23 @@ function logout() {
 
 
 }
+
+async function testCRUD(id, newValue) {
+    if (localStorage.getItem("user") !== null) {
+        let user = JSON.parse(window.localStorage.getItem('user'));
+        let access_token = user["auth"]["access-token"]
+        const refresh_token = user["auth"]["refresh-token"]
+
+
+        const response = await api.put(`testcrud/`, JSON.stringify({ access_token, refresh_token, id, newValue }));
+        const r = await handleNewResponse(response);
+        return r;
+
+    }
+
+
+}
+
 
 function handleNewResponse(response) {
 
