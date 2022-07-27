257d9fa0555a81eb9252576e67de91bc8714e040
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-27 19:06:00 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,16 +1,51 @@
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/views.py
-git commit -m "backend api views.py - fix login"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components
+git commit -m "src components  - add components"
+git add /home/kim/Documents/github/kod-rs/beyond/src/index.html
+git commit -m "src index.html - bootstrap 5"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/device.py
+git commit -m "backend api model device.py - add "
+git add /home/kim/Documents/github/kod-rs/beyond/src/page1/Page1.vue
+git commit -m "src page1 Page1.vue - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0010_delete_message_delete_user.py
+git commit -m "backend api migrations 0010_delete_message_delete_user.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/comm/comm.py
+git commit -m "backend api comm comm.py - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0012_device.py
+git commit -m "backend api migrations 0012_device.py - "
+
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/view/deviceView.py
+git commit -m "backend api view deviceView.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/src/App.vue
+git commit -m "src App.vue - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/building.py
+git commit -m "backend api cqrs_q building.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/device.py
+git commit -m "backend api cqrs_q device.py - add"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
-git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/vue-django-interface.txt
-git commit -m "playground marin-jovanovic vue-django-interface.txt - add comm interface"
-git add /home/kim/Documents/github/kod-rs/beyond/src/login/LoginPage.vue
-git commit -m "src login LoginPage.vue - fix login interface"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/urls.py
-git commit -m "backend urls.py - fix login path"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0013_remove_device_device_id.py
+git commit -m "backend api migrations 0013_remove_device_device_id.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/test/django_device.py
+git commit -m "test django_device.py - test"
 git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/ipCheckMiddleware.py
-git commit -m "backend api middleware ipCheckMiddleware.py - add support for data and headers credentials"
-git add /home/kim/Documents/github/kod-rs/beyond/test/django.py
-git commit -m "test django.py - wip"
+git commit -m "backend api middleware ipCheckMiddleware.py - fix"
+git add /home/kim/Documents/github/kod-rs/beyond/src/page2/Page2.vue
+git commit -m "src page2 Page2.vue - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/building.py
+git commit -m "backend api cqrs_c building.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/router.js
+git commit -m "src _helpers router.js - add routes"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/device.py
+git commit -m "backend api cqrs_c device.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/building.py
+git commit -m "backend api model building.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/src/debug
+git commit -m "src debug  - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/urls.py
+git commit -m "backend urls.py - add paths"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0011_testcrud.py
+git commit -m "backend api migrations 0011_testcrud.py - "
 git add /home/kim/Documents/github/kod-rs/beyond/src/_services/user.service.js
-git commit -m "src _services user.service.js - fix login"
+git commit -m "src _services user.service.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/testCRUD.py
+git commit -m "backend api model testCRUD.py - wip"
