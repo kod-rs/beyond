0b7a2fccae847ece5630dc64f46e5c9f179bf75b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-27 19:06:00 +0200

    backend api model building.py - add

---
+++ backend/api/model/building.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Building(models.Model):
+    building_id = models.AutoField()
+    # todo check if enum
+    building_type =models.CharField(max_length=200)
+    # todo check if enum
+    section =models.CharField(max_length=200)
+    # todo check if geotag
+    # geo =
+
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
