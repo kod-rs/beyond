a82225325f93fcbd50371921a6e1bf8cd70f0853
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-28 14:44:20 +0200

    backend api model location.py - init

--- backend/api/model/location.py
+++ backend/api/model/location.py
@@ -1,15 +1,8 @@
 from django.db import models
 
 
-# changed from building to location
 class Location(models.Model):
-    # building_id = models.AutoField()
-    # todo check if enum
-    building_type =models.CharField(max_length=200)
-    # todo check if enum
-    section =models.CharField(max_length=200)
-    # todo check if geotag
-    # geo =
-
+    section = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
     latitude = models.CharField(max_length=200)
     longitude = models.CharField(max_length=200)
