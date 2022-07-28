a08854f7a7504a166cfc0b2d03685ce1d17c5fb6
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-28 14:45:50 +0200

    backend settings prod.py - merge conf

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,31 +1,8 @@
+
 """ Production Settings """
 
-import os
-import dj_database_url
-# from .dev import *
 from .dev import *  # NOQA
 
-############
-# DATABASE #
-############
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
-
-############
-# SECURITY #
-############
-
-DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
-
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
-
-# Set to your Domain here (eg. 'django-vue-template-demo.herokuapp.com')
-ALLOWED_HOSTS = ['*']
-
-from .dev import *  # NOQA
 DEBUG = False
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
