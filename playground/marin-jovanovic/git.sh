eab4adfd626b848a889b5e9c2f82074c6fd5b56b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-28 14:44:20 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,51 +1,92 @@
-git add /home/kim/Documents/github/kod-rs/beyond/src/components
-git commit -m "src components  - add components"
+git add /home/kim/Documents/github/kod-rs/beyond/webpack.config.js
+git commit -m "webpack.config.js - wip, todo fix css"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/location.py
+git commit -m "backend api cqrs_q location.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/urls.py
+git commit -m "backend urls.py - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/package.json
+git commit -m "package.json - add map"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/device_type.py
+git commit -m "backend api cqrs_c device_type.py - init"
 git add /home/kim/Documents/github/kod-rs/beyond/src/index.html
-git commit -m "src index.html - bootstrap 5"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/device.py
-git commit -m "backend api model device.py - add "
-git add /home/kim/Documents/github/kod-rs/beyond/src/page1/Page1.vue
-git commit -m "src page1 Page1.vue - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0010_delete_message_delete_user.py
-git commit -m "backend api migrations 0010_delete_message_delete_user.py - "
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/comm/comm.py
-git commit -m "backend api comm comm.py - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0012_device.py
-git commit -m "backend api migrations 0012_device.py - "
-
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/view/deviceView.py
-git commit -m "backend api view deviceView.py - add"
-git add /home/kim/Documents/github/kod-rs/beyond/src/App.vue
-git commit -m "src App.vue - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/building.py
-git commit -m "backend api cqrs_q building.py - add"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/device.py
-git commit -m "backend api cqrs_q device.py - add"
+git commit -m "src index.html - remove old bootstrap"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/testCRUD.py
+git commit -m "backend api cqrs_q testCRUD.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/MapContainer.vue
+git commit -m "src components MapContainer.vue - init"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/tmp.py
+git commit -m "playground marin-jovanovic tmp.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/temperature.py
+git commit -m "backend api cqrs_c temperature.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/temperature.py
+git commit -m "backend api cqrs_q temperature.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/Inspect.vue
+git commit -m "src components Inspect.vue - init"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/vue-django-interface.txt
+git commit -m "playground marin-jovanovic vue-django-interface.txt - "
+git add /home/kim/Documents/github/kod-rs/beyond/yarn.lock
+git commit -m "yarn.lock - map"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/testCRUD.py
+git commit -m "backend api cqrs_c testCRUD.py - init"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0013_remove_device_device_id.py
-git commit -m "backend api migrations 0013_remove_device_device_id.py - "
-git add /home/kim/Documents/github/kod-rs/beyond/test/django_device.py
-git commit -m "test django_device.py - test"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/ipCheckMiddleware.py
-git commit -m "backend api middleware ipCheckMiddleware.py - fix"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/django
+git commit -m "playground marin-jovanovic django  - "
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/sqlite_manager.py
+git commit -m "playground marin-jovanovic sqlite_manager.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/vue-django-interface.txt
+git commit -m "vue-django-interface.txt - init"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/seed.py
+git commit -m "playground marin-jovanovic seed.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/Edit.vue
+git commit -m "src components Edit.vue - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/location.py
+git commit -m "backend api model location.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/maps
+git commit -m "playground marin-jovanovic maps  - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/data.py
+git commit -m "backend api cqrs_c data.py - init"
 git add /home/kim/Documents/github/kod-rs/beyond/src/page2/Page2.vue
-git commit -m "src page2 Page2.vue - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/building.py
-git commit -m "backend api cqrs_c building.py - add"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/router.js
-git commit -m "src _helpers router.js - add routes"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/device.py
-git commit -m "backend api cqrs_c device.py - add"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/building.py
-git commit -m "backend api model building.py - add"
-git add /home/kim/Documents/github/kod-rs/beyond/src/debug
-git commit -m "src debug  - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/urls.py
-git commit -m "backend urls.py - add paths"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0011_testcrud.py
-git commit -m "backend api migrations 0011_testcrud.py - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/_services/user.service.js
-git commit -m "src _services user.service.js - wip"
+git commit -m "src page2 Page2.vue - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/tab_name_changer.js
+git commit -m "src _helpers tab_name_changer.js - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/device_type.py
+git commit -m "backend api cqrs_q device_type.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/ipCheckMiddleware.py
+git commit -m "backend api middleware ipCheckMiddleware.py - add check env for https"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/div
+git commit -m "playground marin-jovanovic div  - "
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/models.py
+git commit -m "playground marin-jovanovic models.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/data.py
+git commit -m "backend api model data.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/README.md
+git commit -m "README.md - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/location.py
+git commit -m "backend api cqrs_c location.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/src/scripts
+git commit -m "src scripts  - "
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/db
+git commit -m "playground marin-jovanovic db  - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/device_type.py
+git commit -m "backend api model device_type.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/building.py
+git commit -m "backend api cqrs_q building.py - init"
 git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/testCRUD.py
-git commit -m "backend api model testCRUD.py - wip"
+git commit -m "backend api model testCRUD.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/device.py
+git commit -m "backend api model device.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/view/deviceView.py
+git commit -m "backend api view deviceView.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/data.py
+git commit -m "backend api cqrs_q data.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/src/App.vue
+git commit -m "src App.vue - remove old bootstrap"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/device.py
+git commit -m "backend api cqrs_q device.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/building.py
+git commit -m "backend api cqrs_c building.py - init"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/common.sh
+git commit -m "playground marin-jovanovic common.sh - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/model/temperature.py
+git commit -m "backend api model temperature.py - init"
