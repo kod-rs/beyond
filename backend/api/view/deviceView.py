5bf70978a2dd2c1b24b5e3f77258001b2017a326
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-28 14:44:20 +0200

    backend api view deviceView.py - init

--- backend/api/view/deviceView.py
+++ backend/api/view/deviceView.py
@@ -3,26 +3,16 @@ import json
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from backend.api.comm.comm import decode_data
-from rest_framework.decorators import action
-from rest_framework.viewsets import ModelViewSet
+
 from backend.api.cqrs_c.device import create
 from backend.api.cqrs_q.device import get_all, get_by_id
 
-#
-#
-# class DeviceView(ModelViewSet):
-#
-#     @action(methods=['post'], detail=True)
-#     def set_password(self, request, pk=None):
-#         print("llll")
-#
-#     @classmethod
-#     def get_extra_actions(cls):
-#         return []
+
 
 class DeviceView(APIView):
+    # create rename update delete
 
-    # def post(self, request, pk):
+    # create
     def post(self, request):
         print("todo checking role")
 
@@ -51,7 +41,7 @@ class DeviceView(APIView):
                 "consumption = ":body_content["consumption"][0],
                 "data_id = ":body_content["data_id"][0]
 
-        }
+            }
         }
 
         return JsonResponse(response)
@@ -62,23 +52,18 @@ class DeviceView(APIView):
         if pk:
             print("pk present ------------------")
             print("pk,", pk)
+
+            b = get_by_id(pk)
+            print(b)
+
         else:
             print("no pk -------------")
-
+            b = get_all()
+            [print(i) for i in b]
 
         if request.body:
             body_content = decode_data(request.body)
             print(f"{body_content=}")
-
-
-            # create(
-                # device_id=pk,
-            #     device_type=body_content["device_type"][0],
-            #     consumption=body_content["consumption"][0],
-            #     data_id=body_content["data_id"][0]
-            # )
-
-        print("get")
 
         response = {
             "auth": {
@@ -93,23 +78,3 @@ class DeviceView(APIView):
 
         return JsonResponse(response)
 
-
-    # def post(self, request):
-    #     print("logout post")
-    #
-    #     print("try logout")
-    #     logout(request.refresh_token)
-    #     print("logout done")
-    #
-    #     response = {
-    #         "auth": {
-    #             "status": True,
-    #             "access-token": None,
-    #             "refresh-token": None
-    #         },
-    #         "payload": {
-    #             "page": "logout",
-    #         }
-    #     }
-    #
-    #     return JsonResponse(response)
