809921851097f92f49cb7f22f145df7d04756a86
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-29 19:32:59 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,28 +1,20 @@
-git add /home/kim/Documents/github/kod-rs/beyond/src/App.vue
-git commit -m "src App.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/styles/page2.css
-git commit -m "src styles page2.css - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/MapContainer.vue
-git commit -m "src components MapContainer.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp
-git commit -m "src tmp  - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/page2/Page2.vue
-git commit -m "src page2 Page2.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/keycloak.sh
-git commit -m "playground marin-jovanovic keycloak.sh - run"
-git add /home/kim/Documents/github/kod-rs/beyond/package.json
-git commit -m "package.json - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/ipCheckMiddleware.py
-git commit -m "backend api middleware ipCheckMiddleware.py - fix"
-git add /home/kim/Documents/github/kod-rs/beyond/yarn.lock
-git commit -m "yarn.lock - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/page2/page2.css
-git commit -m "src page2 page2.css - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/logo.png
+git commit -m "src tmp logo.png - init"
+git add /home/kim/Documents/github/kod-rs/beyond/favicon.ico
+git commit -m "favicon.ico - init"
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/login.html
+git commit -m "src tmp login.html - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/electricity-3137843.jpg
+git commit -m "src tmp electricity-3137843.jpg - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/cable-217169.jpg
+git commit -m "src tmp cable-217169.jpg - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/index.html
+git commit -m "src tmp index.html - "
+git add /home/kim/Documents/github/kod-rs/beyond/public/index.html
+git commit -m "public index.html - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/page1.html
+git commit -m "src tmp page1.html - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/template/favicon.ico
+git commit -m "src template favicon.ico - init"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
-git commit -m "playground marin-jovanovic git.sh - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/index.html
-git commit -m "src index.html - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/index.css
-git commit -m "src index.css - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/webpack.config.js
-git commit -m "webpack.config.js - wip"
+git commit -m "playground marin-jovanovic git.sh - "
