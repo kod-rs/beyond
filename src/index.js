b229f4540208a5891ffbe1e0235180260329e2fb
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-29 14:50:35 +0200

    src index.js - fix import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
-import App from './App.vue'
+import App from './template/App.vue'
 import { router } from './_helpers';
 
 const app = createApp(App);
 app.use(router).mount('#app');
+
+// webpack
