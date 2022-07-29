d3243c5838d2e29e3e28c9dd1d8af7f71d327047
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-07-29 12:01:01 +0200

    playground marin-jovanovic keycloak.sh - run

--- playground/marin-jovanovic/keycloak.sh
+++ playground/marin-jovanovic/keycloak.sh
@@ -1,17 +1,17 @@
 # keycloak-18.0.1
 
 # start server
-./bin/kc.sh start-dev
+../../../../../keycloak-18.0.1/bin/kc.sh start-dev
 
-# export configuration
-./bin/kc.sh export --dir export_dir_name
-
-# runs on
-http://localhost:8080/
-
-# todo for prod
-https://www.keycloak.org/docs/latest/server_installation/
-ssl
-
-curl -L -X GET 'http://localhost:8080/realms/beyond/protocol/openid-connect/certs'
-
+## export configuration
+#./bin/kc.sh export --dir export_dir_name
+#
+## runs on
+#http://localhost:8080/
+#
+## todo for prod
+#https://www.keycloak.org/docs/latest/server_installation/
+#ssl
+#
+#curl -L -X GET 'http://localhost:8080/realms/beyond/protocol/openid-connect/certs'
+#
