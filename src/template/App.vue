7b457dc7ed4a1c8d878ed5dc0bdbf64c5c155640
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 13:38:28 +0200

    src template App.vue - wip bootstrap

--- src/template/App.vue
+++ src/template/App.vue
@@ -3,6 +3,7 @@
 
     <div>
         <div>
+            for debug |
             <router-link :to="{ name: 'page1' }">page 1</router-link> |
             <router-link :to="{ name: 'page2' }">map</router-link> |
         </div>
@@ -12,6 +13,7 @@
         </router-view>
 
         <p>
+            for debug |
             <router-link to="/login">Logout</router-link>
         </p>
 
