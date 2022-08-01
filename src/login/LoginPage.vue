6452d609c4b6f4404476e2c26fa142a9b02ae529
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 19:59:41 +0200

    src login LoginPage.vue - fix login button

--- src/login/LoginPage.vue
+++ src/login/LoginPage.vue
@@ -106,6 +106,7 @@ export default {
                                                     <div v-show="submitted && !username" class="invalid-feedback">
                                                         Username is required</div>
                                                 </div>
+                                                <br>
                                                 <div class="form-group">
                                                     <label htmlFor="password">Password</label>
                                                     <input type="password" v-model="password" name="password"
@@ -114,14 +115,16 @@ export default {
                                                     <div v-show="submitted && !password" class="invalid-feedback">
                                                         Password is required</div>
                                                 </div>
+
+                                                <br>
                                                 <div class="form-group">
                                                     <button class="btn btn-primary btn-dark btn-lg btn-block"
                                                         :disabled="loading">Login</button>
 
-
                                                     <img v-show="loading"
                                                         src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                                                 </div>
+                                                <br>
                                                 <div v-if="error" class="alert alert-danger">{{ error }}</div>
                                             </form>
 
