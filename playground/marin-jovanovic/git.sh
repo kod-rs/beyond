52de1342925ea86c7ba2138eebdb0bd274d1282b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 11:10:34 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,20 +1,7 @@
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/logo.png
-git commit -m "src tmp logo.png - init"
-git add /home/kim/Documents/github/kod-rs/beyond/favicon.ico
-git commit -m "favicon.ico - init"
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/login.html
-git commit -m "src tmp login.html - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/electricity-3137843.jpg
-git commit -m "src tmp electricity-3137843.jpg - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/cable-217169.jpg
-git commit -m "src tmp cable-217169.jpg - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/index.html
-git commit -m "src tmp index.html - "
-git add /home/kim/Documents/github/kod-rs/beyond/public/index.html
-git commit -m "public index.html - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/page1.html
-git commit -m "src tmp page1.html - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/template/favicon.ico
-git commit -m "src template favicon.ico - init"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
+git add /home/kim/Documents/github/kod-rs/beyond/package.json
+git commit -m "package.json - update"
+git add /home/kim/Documents/github/kod-rs/beyond/yarn.lock
+git commit -m "yarn.lock - "
+
