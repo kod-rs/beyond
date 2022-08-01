c6799f5a1534f694f80a737311792442704f61ab
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 19:59:41 +0200

    src index.js - wip

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import { router } from './_helpers';
 const app = createApp(App);
 app.use(router).mount('#app');
 
+// import MyComponent from './App.vue'
+import TopNavigationBar from './components/TopNavigationBar.vue'
+import ContentNavigationBar from './components/ContentNavigationBar.vue'
+
+app
+    .component('TopNavigationBar', TopNavigationBar)
+    .component('ContentNavigationBar', ContentNavigationBar)
+
+
 // webpack
 
 console.log("src index")
