2f1999eeecb4815bc8612258d0fcfbf38a6cef67
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 22:32:04 +0200

    src index IndexPage.vue - ref

--- src/index/IndexPage.vue
+++ src/index/IndexPage.vue
@@ -1,103 +1,145 @@
 <template>
 
     <div id="root">
-        sacemo
 
 
+        <TopNavigationBar />
 
-        <!-- <TopNavigationBar /> -->
+        <ContentNavigationBar />
 
-        <!-- offcanvas -->
-        <!-- <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
-            <div class="offcanvas-body p-0">
-                <nav class="navbar-dark">
-                    <ul class="navbar-nav">
-                        <li>
-                            <div class="text-muted small fw-bold text-uppercase px-3">
-                                CORE
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-3 active">
-                                <span class="me-2"><i class="bi bi-speedometer2"></i></span>
-                                <span>Dashboard</span>
-                            </a>
-                        </li>
-                        <li class="my-4">
-                            <hr class="dropdown-divider bg-light" />
-                        </li>
-                        <li>
-                            <div class="text-muted small fw-bold text-uppercase px-3 mb-3">
-                                links
-                            </div>
-                        </li>
-                        <li>
-                            <a class="nav-link px-3 sidebar-link" data-bs-toggle="collapse" href="#layouts">
-                                <span class="me-2"><i class="bi bi-layout-split"></i></span>
-                                <span>Test</span>
-                                <span class="ms-auto">
-                                    <span class="right-icon">
-                                        <i class="bi bi-chevron-down"></i>
-                                    </span>
-                                </span>
-                            </a>
-                            <div class="collapse" id="layouts">
-                                <ul class="navbar-nav ps-3">
-                                    <li>
-                                        <a href="#" class="nav-link px-3">
-                                            <span class="me-2"><i class="bi bi-speedometer2"></i></span>
-                                            <span>Dashboard</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-3">
-                                <span class="me-2"><i class="bi bi-book-fill"></i></span>
-                                <span>Map</span>
-                            </a>
-                        </li>
-                        <li class="my-4">
-                            <hr class="dropdown-divider bg-light" />
-                        </li>
-                        <li>
-                            <div class="text-muted small fw-bold text-uppercase px-3 mb-3">
-                                Addons
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-3">
-                                <span class="me-2"><i class="bi bi-graph-up"></i></span>
-                                <span>Charts</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-3">
-                                <span class="me-2"><i class="bi bi-table"></i></span>
-                                <span>Tables</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div> -->
-
-        <!-- <ContentNavigationBar /> -->
-
-        <!-- offcanvas -->
         <main class="mt-5 pt-3">
             <div class="container-fluid">
 
                 <div class="row">
 
-                </div>
-                <div class="row">
+                    <form @submit.prevent="handleSubmit">
+
+                        <li v-for="item in modelFields">
+                            {{ item }}
+
+                        </li>
+
+
+                        <br>
+                        <div class="form-group">
+                            <button class="btn btn-primary btn-dark btn-lg btn-block" :disabled="loading">Login</button>
+
+                            <img v-show="loading"
+                                src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                        </div>
+                        <br>
+                        <div v-if="error" class="alert alert-danger">{{ error }}</div>
+                    </form>
+
+
+                    <!-- <Map /> -->
+
+                    <!-- <input v-model="searchInput" @keyup.enter="search" placeholder="Enter place" />
+                    <button @click="search">Search</button> -->
+
 
                 </div>
+
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <div class="card">
+                            <div class="card-header">
+                                <span><i class="bi bi-table me-2"></i></span> Data Table
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="example" class="table table-striped data-table" style="width: 100%">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <tr>
+                                                <td>Bradley Greer</td>
+                                                <td>Software Engineer</td>
+                                                <td>London</td>
+                                                <td>41</td>
+                                                <td>2012/10/13</td>
+                                                <td>$132,000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Bradley Greer</td>
+                                                <td>Software Engineer</td>
+                                                <td>London</td>
+                                                <td>41</td>
+                                                <td>2012/10/13</td>
+                                                <td>$132,000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Bradley Greer</td>
+                                                <td>Software Engineer</td>
+                                                <td>London</td>
+                                                <td>41</td>
+                                                <td>2012/10/13</td>
+                                                <td>$132,000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Bradley Greer</td>
+                                                <td>Software Engineer</td>
+                                                <td>London</td>
+                                                <td>41</td>
+                                                <td>2012/10/13</td>
+                                                <td>$132,000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Dai Rios</td>
+                                                <td>Personnel Lead</td>
+                                                <td>Edinburgh</td>
+                                                <td>35</td>
+                                                <td>2012/09/26</td>
+                                                <td>$217,500</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>Donna Snider</td>
+                                                <td>Customer Support</td>
+                                                <td>New York</td>
+                                                <td>27</td>
+                                                <td>2011/01/25</td>
+                                                <td>$112,000</td>
+                                            </tr>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 
                 <div class="row">
 
+first
                 </div>
+
+<div class="row">
+                    second
+                </div>
+                <div class="row">
+                    third
+                </div> -->
 
             </div>
         </main>
@@ -107,12 +149,29 @@
 </template>
 
 <script>
-// import ContentNavigationBar from "../components/ContentNavigationBar.vue";
+export default {
+    name: "f",
+    data() {
+        return {
+            searchInput: '',
+            modelFields: ["section", "type", "longitude", "latitude"]
+        }
+    },
+    methods: {
+        search() {
+            this.fetchData()
+        },
+        async fetchData() {
+            const apiKey = import.meta.env.tTT
+            const url = ``
+            this.searchInput = ''
+            const res = await fetch(url)
+            const jsonResponse = await res.json()
 
-// import { TopNavigationBar } from '../components/TopNavigationBar.vue'
-// const tf = require("./script")
+        }
+    }
+}
 
-console.log("inde")
 </script>
 
 <style>
