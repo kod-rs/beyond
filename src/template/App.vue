426b99aa0ce2d90a1a810e262834f01a6551ec8f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 22:32:04 +0200

    src template App.vue - cleanup debug

--- src/template/App.vue
+++ src/template/App.vue
@@ -1,22 +1,9 @@
-
 <template>
 
     <div>
-        <div>
-            for debug |
-            <router-link :to="{ name: 'page1' }">page 1</router-link> |
-            <router-link :to="{ name: 'page2' }">map</router-link> |
-        </div>
-
 
         <router-view>
         </router-view>
-
-        <p>
-            for debug |
-            <router-link to="/login">Logout</router-link>
-        </p>
-
 
     </div>
 
