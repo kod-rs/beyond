00d4a1741375e079f960e910804bb393a48a7a4e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 22:32:04 +0200

    src components ContentNavigationBar.vue - ef

--- src/components/ContentNavigationBar.vue
+++ src/components/ContentNavigationBar.vue
@@ -50,6 +50,32 @@
                             <span>Map</span>
                         </a>
                     </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="bi bi-book-fill"></i></span>
+                            <span>ff</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="bi bi-book-fill"></i></span>
+                            <span>
+
+                                <router-link :to="{ name: 'page1' }">page 1</router-link>
+                            </span>
+
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="bi bi-book-fill"></i></span>
+                            <span>
+
+                                <router-link :to="{ name: 'page2' }">map</router-link>
+                            </span>
+
+                        </a>
+                    </li>
                     <li class="my-4">
                         <hr class="dropdown-divider bg-light" />
                     </li>
@@ -70,6 +96,14 @@
                             <span>Tables</span>
                         </a>
                     </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="bi bi-table"></i></span>
+                            <span>
+                                <router-link to="/login">Logout</router-link>
+                            </span>
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </div>
@@ -84,54 +118,7 @@ export default {
         return {
             a: 0
         }
-    },
-    //     template: `
-
-
-    //     <!-- top navigation bar -->
-    //     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    //         <div class="container-fluid">
-    //             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar"
-    //                 aria-controls="offcanvasExample">
-    //                 <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
-    //             </button>
-    //             <a class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" href="#">Beyond</a>
-    //             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavBar"
-    //                 aria-controls="topNavBar" aria-expanded="false" aria-label="Toggle navigation">
-    //                 <span class="navbar-toggler-icon"></span>
-    //             </button>
-    //             <div class="collapse navbar-collapse" id="topNavBar">
-    //                 <form class="d-flex ms-auto my-3 my-lg-0">
-    //                     <div class="input-group">
-    //                         <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
-    //                         <button class="btn btn-primary" type="submit">
-    //                             <i class="bi bi-search"></i>
-    //                         </button>
-    //                     </div>
-    //                 </form>
-    //                 <ul class="navbar-nav">
-    //                     <li class="nav-item dropdown">
-    //                         <a class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown"
-    //                             aria-expanded="false">
-    //                             <i class="bi bi-person-fill"></i>
-    //                         </a>
-    //                         <ul class="dropdown-menu dropdown-menu-end">
-    //                             <li><a class="dropdown-item" href="#">Action</a></li>
-    //                             <li><a class="dropdown-item" href="#">Another action</a></li>
-    //                             <li>
-    //                                 <a class="dropdown-item" href="#">Something else here</a>
-    //                             </li>
-    //                         </ul>
-    //                     </li>
-    //                 </ul>
-    //             </div>
-    //         </div>
-    //     </nav>
-
-
-    // `
-
-
+    }
 }
 
 </script>
