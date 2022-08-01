d1cce7492d09e95512cccf43b3f5632fff26c262
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 11:38:53 +0200

    test django_device.py - fix

--- test/django_device.py
+++ test/django_device.py
@@ -46,10 +46,43 @@ def get():
     print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
 
 
+def create_test_crud():
+
+    url = "http://localhost:8000/testcrud/"
+    headers = {
+        "username": "mirko",
+        "password": "mirko"
+    }
+
+    t = requests.put(url, headers=headers, data={
+        "id": "ff",
+        "newValue": "aaaa"
+    }, verify=False)
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+
+def get_test_crud():
+
+    url = "http://localhost:8000/testcrud/"
+    headers = {
+        "username": "mirko",
+        "password": "mirko"
+    }
+
+    t = requests.get(url, headers=headers, data={
+    }, verify=False)
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+
 def main():
     # create()
-    get_one()
-    get()
+    # get_one()
+    # get()
+
+    create_test_crud()
+    # get_test_crud()
 
 if __name__ == '__main__':
     main()
