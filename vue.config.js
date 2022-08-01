6f61a68a58fa5200e7d29bcb7554969696f25691
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 11:06:36 +0200

    vue.config.js - wip

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,33 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/beyond/" : "/",
+//   // configureWebpack: config => {
+
+//   //   // if (process.env.NODE_ENV === 'production') {
+//   //   if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
+//   //     // mutate config for production...
+//   //     console.log("dev")
+//   //   } else {
+//   //     console.log("production")
+//   //     // mutate for development...
+//   //   }
+//   // }
+
+// };
+
+const path = require("path");
+
+console.log("vue config ")
+
 module.exports = {
+  chainWebpack: config => {
+    config
+      .entry("app")
+      .clear()
+      .add("./src_vue/index.js")
+      .end();
+    config.resolve.alias
+      .set("@", path.join(__dirname, "./src_vue"))
+  },
   publicPath: process.env.NODE_ENV === "production" ? "/beyond/" : "/",
+
 };
