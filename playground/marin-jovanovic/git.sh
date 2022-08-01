4d18a1473031b8c070749731a1659ab3dbbccbc2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-01 19:59:41 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,38 +1,37 @@
-git add /home/kim/Documents/github/kod-rs/beyond/src/template/App.vue
-git commit -m "src template App.vue - wip bootstrap"
-git add /home/kim/Documents/github/kod-rs/beyond/src/template/index.js
-git commit -m "src template index.js - migrate"
-git add /home/kim/Documents/github/kod-rs/beyond/webpack.config.js
-git commit -m "webpack.config.js - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/login.html
-git commit -m "src tmp login.html - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/README.md
-git commit -m "README.md - update fixme"
-git add /home/kim/Documents/github/kod-rs/beyond/docs/task/.~lock.3_Task6.5 KONCAR 27-01-2022.pptx#
-git commit -m "docs task .~lock.3_Task6.5 KONCAR 27-01-2022.pptx# - "
-git add /home/kim/Documents/github/kod-rs/beyond/vue.config.js
-git commit -m "vue.config.js - cleanup"
-git add /home/kim/Documents/github/kod-rs/beyond/src_vue/template/favicon.ico
-git commit -m "src_vue template favicon.ico - add"
-git add /home/kim/Documents/github/kod-rs/beyond/src_vue/template/index.css
-git commit -m "src_vue template index.css - wip bootstrap"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/ContentNavigationBar.vue
+git commit -m "src components ContentNavigationBar.vue - wip"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/map.html
-git commit -m "src tmp map.html - wip bootstrap"
-git add /home/kim/Documents/github/kod-rs/beyond/src_vue/template/index.js
-git commit -m "src_vue template index.js - wip bootstrap"
-git add /home/kim/Documents/github/kod-rs/beyond/src/main.js
-git commit -m "src main.js - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/Chart.vue
+git commit -m "src components Chart.vue - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/TopNavigationBar.vue
+git commit -m "src components TopNavigationBar.vue - wip"
+
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/cable-217169.jpg
+git commit -m "src tmp cable-217169.jpg - move"
+git add /home/kim/Documents/github/kod-rs/beyond/src/script.js
+git commit -m "src script.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/logo.png
+git commit -m "src tmp logo.png - move"
+git add /home/kim/Documents/github/kod-rs/beyond/src/index/IndexPage.vue
+git commit -m "src index IndexPage.vue - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/src/admin
+git commit -m "src admin  - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/README.md
+git commit -m "README.md - update fixme"
+git add /home/kim/Documents/github/kod-rs/beyond/src/login/LoginPage.vue
+git commit -m "src login LoginPage.vue - fix login button"
+git add /home/kim/Documents/github/kod-rs/beyond/src/index/script.js
+git commit -m "src index script.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/index.js
+git commit -m "src index.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/electricity-3137843.jpg
+git commit -m "src tmp electricity-3137843.jpg - move"
+git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/login.html
+git commit -m "src tmp login.html - move"
+git add /home/kim/Documents/github/kod-rs/beyond/src/home/HomePage.vue
+git commit -m "src home HomePage.vue - wip"
 git add /home/kim/Documents/github/kod-rs/beyond/src/template/index.html
-git commit -m "src template index.html - wip bootstrap"
-git add /home/kim/Documents/github/kod-rs/beyond/src_vue/index.js
-git commit -m "src_vue index.js - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/src/debug/Debug.vue
-git commit -m "src debug Debug.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src_vue/template/index.html
-git commit -m "src_vue template index.html - wip bootstrap"
-git add /home/kim/Documents/github/kod-rs/beyond/src_vue/template/App.vue
-git commit -m "src_vue template App.vue - wip bootstrap"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/router.js
-git commit -m "src _helpers router.js - cleanup"
+git commit -m "src template index.html - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/index/style.css
+git commit -m "src index style.css - ref"
