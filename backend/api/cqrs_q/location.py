d792b1b4b260c6e42606a719aea6e61bcac59f19
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-02 14:46:07 +0200

    backend api cqrs_q location.py - add

--- backend/api/cqrs_q/location.py
+++ backend/api/cqrs_q/location.py
@@ -1,6 +1,22 @@
-from backend.api.model.location import Location
+import ast
+import json
 
+from backend.api.model.location import Location
+from django.core import serializers
+from django.forms.models import model_to_dict
+import json
+from ast import parse
 
 def get_all():
-    return Location.objects.all().iterator()
+
+    t = serializers.serialize('json', Location.objects.all())
+    t = json.loads(t)
+    t = [i["fields"] for i in t]
+
+    t = [{k: ast.literal_eval(v) for k,v in i.items()} for i in t]
+
+
+    return t
+
+    # return [i  i in Location.objects.all().iterator()]
 
