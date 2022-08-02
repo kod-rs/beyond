79dce710837cb343ee789bd0cd7c7f5e31d6e7a1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-02 14:46:07 +0200

    backend urls.py - add location

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,10 @@ from backend.api.view.loginView import LoginView
 from backend.api.view.logoutView import LogoutView
 from backend.api.view.deviceView import DeviceView
 from backend.api.startup import run_startup
+from backend.api.view.dbView import pureDjangoView
+from backend.api.model.testCRUD import TestCrudView
+from backend.api.view.locationsView import LocationsView
+
 run_startup()
 
 api_router = routers.DefaultRouter()
@@ -15,12 +19,7 @@ api_router.register('device', DeviceView, basename="device")
 # router.register('snippets/<int:pk>', SnippetDetail)
 
 
-from django.http import HttpResponse
 
-
-from backend.api.view.dbView import pureDjangoView
-from backend.api.model.testCRUD import TestCrudView
-from backend.api.model.locationsView import LocationsView
 
 urlpatterns = [
     path('', IndexView.as_view()),
@@ -28,30 +27,19 @@ urlpatterns = [
     path('logout/', LogoutView.as_view()),
     path("device/<int:pk>", DeviceView.as_view()),
     path("device/", DeviceView.as_view()),
+    path('puredjango', pureDjangoView, name='home'),
+    path("testcrud/", TestCrudView.as_view()),
+    path("locations/", LocationsView.as_view()),
+    path('api/admin/', admin.site.urls),
 
     # path('accounts/', include('allauth.urls')),
-
     # path('bank', BankViewSet.as_view, name='bank'),
-
     # path('auth/', include('social_django.urls', namespace='social')),
-
-    path('puredjango', pureDjangoView, name='home'),
-
     # path('snippets/<int:pk>', views.snippet_detail),
-    path('', IndexView.as_view()),
-
-    path("testcrud/", TestCrudView.as_view()),
-
-    path("locations/", LocationsView.as_view()),
-
     # http://localhost:8000/
     # path('', IndexView, name='index'),
-
-
     # http://localhost:8000/api/<router-viewsets>
     # path('api/', include(api_router.urls)),
-
-    path('api/admin/', admin.site.urls),
 
 ]
 
