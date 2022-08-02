c9405213fa1177dd8a6e834cc024b90446f85157
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-02 14:46:07 +0200

    backend api view locationsView.py - add

--- backend/api/view/locationsView.py
+++ backend/api/view/locationsView.py
@@ -1,48 +1,96 @@
-
-
 import json
 
 from django.http import JsonResponse
 from rest_framework.views import APIView
+# from backend.api.comm.comm import decode_data
+
+# from backend.api.cqrs_c.device import create
+# from backend.api.cqrs_q.device import get_all, get_by_id
 from backend.api.comm.comm import decode_data
-
-from backend.api.cqrs_c.device import create
-from backend.api.cqrs_q.device import get_all, get_by_id
-
-
-
+from backend.api.cqrs_q.location import get_all
+from backend.api.cqrs_c.location import add
+from backend.api.config.main import ROLES
+from django.core import serializers
 class LocationsView(APIView):
     # create rename update delete
 
-    def get(self, request, pk=None):
-        print("todo checking role")
+    def put(self, request):
+        """update by editing existing"""
 
-        if pk:
-            print("pk present ------------------")
-            print("pk,", pk)
+    # def patch(self, request):
+    #     """update by editing existing"""
 
-            b = get_by_id(pk)
-            print(b)
-
-        else:
-            print("no pk -------------")
-            b = get_all()
-            [print(i) for i in b]
-
-        if request.body:
-            body_content = decode_data(request.body)
-            print(f"{body_content=}")
+    def post(self, request):
+        """create"""
 
         response = {
             "auth": {
                 "status": True,
                 "access-token": request.access_token,
                 "refresh-token": request.refresh_token
-            },
-            "payload": {
-                "page": "put device",
             }
         }
 
+        roles = request.roles
+
+        # todo check role
+        if any(i in ROLES for i in roles):
+            print("access granted")
+
+            if request.body:
+                body_content = decode_data(request.body)
+                print(f"{body_content=}")
+
+
+                d = add(**body_content)
+                d = "sace"
+
+                response["payload"] = {
+                    "status": True,
+                    "content": d
+                }
+
+                return JsonResponse(response)
+
+        # else:
+        response["payload"] = {"status": False}
+
+
         return JsonResponse(response)
 
+
+    def get(self, request):
+
+        response = {
+            "auth": {
+                "status": True,
+                "access-token": request.access_token,
+                "refresh-token": request.refresh_token
+            }
+        }
+
+        roles = request.roles
+
+        # todo check role
+        if any(i in ROLES for i in roles):
+            print("access granted")
+
+            print("getting roles")
+
+            d = get_all()
+
+            # print(d)
+            [print(i) for i in d]
+            # serialized_d = serializers.serialize('json', [d, ])
+
+            response["payload"] = {
+                "status": True,
+                "content": d
+            }
+
+
+        else:
+            response["payload"] = {"status": False}
+
+
+        return JsonResponse(response)
