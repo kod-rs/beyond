267e5a92afaf62ce68983ef9ea2cb5d86b6a1815
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-02 18:22:52 +0200

    src index IndexPage.vue - wip

--- src/index/IndexPage.vue
+++ src/index/IndexPage.vue
@@ -12,113 +12,56 @@
 
                 <div class="row">
 
-                    <form @submit.prevent="handleSubmit">
-
-                        <li v-for="item in modelFields">
-                            {{ item }}
-
-                        </li>
-
-
-                        <br>
-                        <div class="form-group">
-                            <button class="btn btn-primary btn-dark btn-lg btn-block" :disabled="loading">Login</button>
-
-                            <img v-show="loading"
-                                src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        </div>
-                        <br>
-                        <div v-if="error" class="alert alert-danger">{{ error }}</div>
-                    </form>
 
 
                     <!-- <Map /> -->
 
-                    <!-- <input v-model="searchInput" @keyup.enter="search" placeholder="Enter place" />
-                    <button @click="search">Search</button> -->
 
-
+                    <!-- <input v-model="searchInput" @keyup.enter="search" placeholder="Enter place" /> -->
+                    <button @click="refreshLocations">refresh locations, maybe someone else placed something
+                        new</button>
                 </div>
+
+                <hr>
+                <hr>
+                <hr>
 
                 <div class="row">
                     <div class="col-md-12 mb-3">
+
                         <div class="card">
                             <div class="card-header">
                                 <span><i class="bi bi-table me-2"></i></span> Data Table
+
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
                                     <table id="example" class="table table-striped data-table" style="width: 100%">
                                         <thead>
                                             <tr>
-                                                <th>Name</th>
-                                                <th>Position</th>
-                                                <th>Office</th>
-                                                <th>Age</th>
-                                                <th>Start date</th>
-                                                <th>Salary</th>
+                                                <th>id curr</th>
+                                                <th>section</th>
+                                                <th>type</th>
+                                                <th>latitude</th>
+                                                <th>longitude</th>
                                             </tr>
                                         </thead>
                                         <tbody>
 
-                                            <tr>
-                                                <td>Bradley Greer</td>
-                                                <td>Software Engineer</td>
-                                                <td>London</td>
-                                                <td>41</td>
-                                                <td>2012/10/13</td>
-                                                <td>$132,000</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Bradley Greer</td>
-                                                <td>Software Engineer</td>
-                                                <td>London</td>
-                                                <td>41</td>
-                                                <td>2012/10/13</td>
-                                                <td>$132,000</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Bradley Greer</td>
-                                                <td>Software Engineer</td>
-                                                <td>London</td>
-                                                <td>41</td>
-                                                <td>2012/10/13</td>
-                                                <td>$132,000</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Bradley Greer</td>
-                                                <td>Software Engineer</td>
-                                                <td>London</td>
-                                                <td>41</td>
-                                                <td>2012/10/13</td>
-                                                <td>$132,000</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Dai Rios</td>
-                                                <td>Personnel Lead</td>
-                                                <td>Edinburgh</td>
-                                                <td>35</td>
-                                                <td>2012/09/26</td>
-                                                <td>$217,500</td>
+                                            <tr v-for="(item, i) in modelContent" :key="item.id">
+                                                <td>{{ i }}</td>
+                                                <td v-for="(jitem, j) in item">{{ jitem }}</td>
+
                                             </tr>
 
-                                            <tr>
-                                                <td>Donna Snider</td>
-                                                <td>Customer Support</td>
-                                                <td>New York</td>
-                                                <td>27</td>
-                                                <td>2011/01/25</td>
-                                                <td>$112,000</td>
-                                            </tr>
                                         </tbody>
                                         <tfoot>
                                             <tr>
-                                                <th>Name</th>
-                                                <th>Position</th>
+                                                <th>sum</th>
+                                                <th>avg</th>
                                                 <th>Office</th>
                                                 <th>Age</th>
                                                 <th>Start date</th>
-                                                <th>Salary</th>
                                             </tr>
                                         </tfoot>
                                     </table>
@@ -128,18 +71,7 @@
                     </div>
                 </div>
 
-                <!-- 
-                <div class="row">
 
-first
-                </div>
-
-<div class="row">
-                    second
-                </div>
-                <div class="row">
-                    third
-                </div> -->
 
             </div>
         </main>
@@ -149,15 +81,44 @@ first
 </template>
 
 <script>
+import { router } from '../_helpers';
+import { userService } from '../_services';
+
 export default {
     name: "f",
     data() {
         return {
             searchInput: '',
-            modelFields: ["section", "type", "longitude", "latitude"]
+            modelFields: ["section", "type", "longitude", "latitude"],
+            modelContent: [["a", 'b', "c", "d"], ['f', 'f', "g", 'i']],
+            error: '',
+            loading: false
         }
     },
+    // created: {
+    //     this.modelContent = 
+    // },
+    // created: {
+    //     // searchInput = "fff"
+    //     // console.log("index page created");
+    // },
     methods: {
+        refreshLocations() {
+            console.log("refreshing locations")
+
+
+            userService.getAllLocations('username', "password").then(
+                user => { console.log("new locations") },
+                error => {
+                    console.log("err")
+                    this.error = "invalid credentials"
+                    // this.error = error;
+                    this.loading = false;
+                }
+            );
+
+        },
+
         search() {
             this.fetchData()
         },
