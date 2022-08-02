953630cbc8623a5431dee182ab48b34f4653411f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 00:33:08 +0200

    backend api cqrs_q location.py - fix getall

--- backend/api/cqrs_q/location.py
+++ backend/api/cqrs_q/location.py
@@ -11,9 +11,35 @@ def get_all():
 
     t = serializers.serialize('json', Location.objects.all())
     t = json.loads(t)
-    t = [i["fields"] for i in t]
+    print(t)
 
-    t = [{k: ast.literal_eval(v) for k,v in i.items()} for i in t]
+    n = []
+    for i in t:
+        f = {}
+        p = i["pk"]
+        f["pk"] = p
+        f.update(i["fields"])
+        # print(f)
+        n.append(f)
+
+    print("fffffff")
+    for i in n:
+        print(i)
+    t = n
+    # print("aaaaaaaaaaaa")
+    # t = [i["fields"].update({"pk": i["pk"]}) for i in t]
+    # # b = [i["pk"] for i in t]
+    # print("bbbbbbbbbbb")
+    # print(t)
+    # t = []
+
+    try:
+        t = [{k: ast.literal_eval(v)[0] for k,v in i.items()} for i in t]
+    except ValueError:
+        try:
+            t = [{k: ast.literal_eval(v) for k,v in i.items()} for i in t]
+        except ValueError:
+            t = [{k: v for k,v in i.items()} for i in t]
 
 
     return t
