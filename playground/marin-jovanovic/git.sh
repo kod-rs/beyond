50265504b4c0d443a24ebddc3632325171b99db3
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-02 14:46:07 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,37 +1,37 @@
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/ContentNavigationBar.vue
-git commit -m "src components ContentNavigationBar.vue - wip"
+
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/migrations/0015_location.py
+git commit -m "backend api migrations 0015_location.py - "
+git add /home/kim/Documents/github/kod-rs/beyond/env-scheme.txt
+git commit -m "env-scheme.txt - update"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/keycloak/keycloak_manager.py
+git commit -m "backend api keycloak keycloak_manager.py - fix"
+git add /home/kim/Documents/github/kod-rs/beyond/test/device.py
+git commit -m "test device.py - update"
+git add /home/kim/Documents/github/kod-rs/beyond/public/static/favicon.ico
+git commit -m "public static favicon.ico - "
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/view/locationsView.py
+git commit -m "backend api view locationsView.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/docs/vue_django_template/readme.md
+git commit -m "docs vue_django_template readme.md - update todo"
+git add /home/kim/Documents/github/kod-rs/beyond/requirements_builder
+git commit -m "requirements_builder  - add"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/location.py
+git commit -m "backend api cqrs_c location.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/a.txt
+git commit -m "a.txt - wip"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/Chart.vue
-git commit -m "src components Chart.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/TopNavigationBar.vue
-git commit -m "src components TopNavigationBar.vue - wip"
-
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/cable-217169.jpg
-git commit -m "src tmp cable-217169.jpg - move"
-git add /home/kim/Documents/github/kod-rs/beyond/src/script.js
-git commit -m "src script.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/logo.png
-git commit -m "src tmp logo.png - move"
-git add /home/kim/Documents/github/kod-rs/beyond/src/index/IndexPage.vue
-git commit -m "src index IndexPage.vue - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/src/admin
-git commit -m "src admin  - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/README.md
-git commit -m "README.md - update fixme"
-git add /home/kim/Documents/github/kod-rs/beyond/src/login/LoginPage.vue
-git commit -m "src login LoginPage.vue - fix login button"
-git add /home/kim/Documents/github/kod-rs/beyond/src/index/script.js
-git commit -m "src index script.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/index.js
-git commit -m "src index.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/electricity-3137843.jpg
-git commit -m "src tmp electricity-3137843.jpg - move"
-git add /home/kim/Documents/github/kod-rs/beyond/src/tmp/login.html
-git commit -m "src tmp login.html - move"
-git add /home/kim/Documents/github/kod-rs/beyond/src/home/HomePage.vue
-git commit -m "src home HomePage.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/template/index.html
-git commit -m "src template index.html - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/index/style.css
-git commit -m "src index style.css - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/public/index.html
+git commit -m "public index.html - del"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/config
+git commit -m "backend api config  - add config"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/settings/dev.py
+git commit -m "backend settings dev.py - remove kc middleware"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/ipCheckMiddleware.py
+git commit -m "backend api middleware ipCheckMiddleware.py - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/location.py
+git commit -m "backend api cqrs_q location.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/test/location.py
+git commit -m "test location.py - add"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/urls.py
+git commit -m "backend urls.py - add location"
