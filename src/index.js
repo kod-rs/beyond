fffbfd8ce32c45f8438b9b7c3f9e31dcb3a7749d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 00:33:08 +0200

    src index.js - add location component

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ app.use(router).mount('#app');
 import TopNavigationBar from './components/TopNavigationBar.vue'
 import ContentNavigationBar from './components/ContentNavigationBar.vue'
 import Map from './components/Map.vue'
+import LocationForm from './components/LocationForm.vue'
 
 app
     .component('TopNavigationBar', TopNavigationBar)
     .component('ContentNavigationBar', ContentNavigationBar)
     .component('Map', Map)
+    .component('LocationForm', LocationForm)
 
 // webpack
 
