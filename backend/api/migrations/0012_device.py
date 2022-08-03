3999a8c14a76539493d12f0ecbaea6b22a822ec1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 09:52:29 +0200

    backend api migrations 0012_device.py - ref

--- backend/api/migrations/0012_device.py
+++ backend/api/migrations/0012_device.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('api', '0011_testcrud'),
     ]
@@ -13,7 +12,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Device',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                                        serialize=False, verbose_name='ID')),
                 ('device_id', models.CharField(max_length=200)),
                 ('data_id', models.CharField(max_length=200)),
                 ('device_type', models.CharField(max_length=200)),
