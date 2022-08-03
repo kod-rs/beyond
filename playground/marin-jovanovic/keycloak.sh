c8073c6d2e4d4e72f574fce9381ace91dbd9f30d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 15:31:47 +0200

    playground marin-jovanovic keycloak.sh - add kc config

--- playground/marin-jovanovic/keycloak.sh
+++ playground/marin-jovanovic/keycloak.sh
@@ -2,6 +2,7 @@
 
 # start server
 ../../../../../keycloak-18.0.1/bin/kc.sh start-dev
+#../../../../../keycloak-18.0.1/bin/kc.sh export --dir /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic
 
 ## export configuration
 #./bin/kc.sh export --dir export_dir_name
