bb4cc5fe509080c9e96b5d40a185399f44c48d54
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 09:52:29 +0200

    backend urls.py - ref

--- backend/urls.py
+++ backend/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from rest_framework import routers
-from backend.api.views import IndexView
-from backend.api.view.loginView import LoginView
-from backend.api.view.logoutView import LogoutView
-from backend.api.view.deviceView import DeviceView
+
+from backend.api.model.testCRUD import TestCrudView
 from backend.api.startup import run_startup
 from backend.api.view.dbView import pureDjangoView
-from backend.api.model.testCRUD import TestCrudView
+from backend.api.view.deviceView import DeviceView
 from backend.api.view.locationsView import LocationsView
+from backend.api.view.loginView import LoginView
+from backend.api.view.logoutView import LogoutView
+from backend.api.views import IndexView
 
 run_startup()
 
@@ -17,9 +18,6 @@ api_router.register('device', DeviceView, basename="device")
 
 # router.register('tmp', TmpViewSet)
 # router.register('snippets/<int:pk>', SnippetDetail)
-
-
-
 
 urlpatterns = [
     path('', IndexView.as_view()),
@@ -49,5 +47,3 @@ urlpatterns = [
     # path('api/', include(api_router.urls)),
 
 ]
-
-
