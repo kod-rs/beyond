59f7daa6466873ed442bcccc9f8bcf97cd29277f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 15:31:47 +0200

    src components MapContainer.vue - ref ol

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -82,3 +82,38 @@ export default {
     }
 }
 </script>
+
+<!-- <template>
+    <div id="map">
+    </div>
+</template>
+
+<script>
+import { Map, View } from 'ol';
+import { Tile as TileLayer } from 'ol/layer';
+import { OSM } from 'ol/source';
+
+export default {
+    name: 'Map',
+    mounted() {
+        this.map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                })
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 2
+            })
+        })
+    }
+}
+</script>
+
+<style>
+#map {
+    height: 500vh;
+}
+</style> -->
