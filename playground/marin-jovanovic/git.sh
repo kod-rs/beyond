7e4f7022c9cb273da3793a3c3dc1a28dcbd9a7e4
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 10:57:57 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,22 +1,12 @@
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_c/ip.py
-git commit -m "backend api cqrs_c ip.py - fix ip check"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/cqrs_q/ip.py
-git commit -m "backend api cqrs_q ip.py - fix ip check"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/ipCheckMiddleware.py
-git commit -m "backend api middleware ipCheckMiddleware.py - fix ip check, ref"
-git add /home/kim/Documents/github/kod-rs/beyond/README.md
-git commit -m "README.md - add todo"
+git add /home/kim/Documents/github/kod-rs/beyond/docs/task/.~lock.3_Task6.5 KONCAR 27-01-2022.pptx#
+git commit -m "docs task .~lock.3_Task6.5 KONCAR 27-01-2022.pptx# - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/router.js
+git commit -m "src _helpers router.js - add routes"
+git add /home/kim/Documents/github/kod-rs/beyond/src/views
+git commit -m "src views  - add view folder"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/ContentNavigationBar.vue
+git commit -m "src components ContentNavigationBar.vue - fix href"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/TopNavigationBar.vue
-git commit -m "src components TopNavigationBar.vue - remove search"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/authCheckMiddleware.py
-git commit -m "backend api middleware authCheckMiddleware.py - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/roleCheckMiddleware.py
-git commit -m "backend api middleware roleCheckMiddleware.py - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/ContentNavigationBar.vue
-git commit -m "src components ContentNavigationBar.vue - ref"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/settings/dev.py
-git commit -m "backend settings dev.py - add mw"
-git add /home/kim/Documents/github/kod-rs/beyond/backend/api/middleware/httpsCheckMiddleware.py
-git commit -m "backend api middleware httpsCheckMiddleware.py - ref"
+git add /home/kim/Documents/github/kod-rs/beyond/src/index/IndexPage.vue
+git commit -m "src index IndexPage.vue - ref"
