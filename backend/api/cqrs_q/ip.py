687ac7f68c402d183405ca58f13c9e4f29ec75db
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-03 09:49:13 +0200

    backend api cqrs_q ip.py - fix ip check

--- backend/api/cqrs_q/ip.py
+++ backend/api/cqrs_q/ip.py
@@ -3,7 +3,9 @@ from backend.api.model.ip import IpCounter
 
 def check_max_count(ip, max_count):
     try:
+
         ip_obj = IpCounter.objects.get(ip=ip)
+        print(ip_obj, ip_obj.counter)
         return ip_obj.counter > max_count
 
     except IpCounter.DoesNotExist:
