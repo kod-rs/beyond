aed9e7dd2682bedde7ffb088ad601deb591e0f08
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-06 23:20:03 +0200

    backend api middleware roleCheckMiddleware.py - ref

--- backend/api/middleware/roleCheckMiddleware.py
+++ backend/api/middleware/roleCheckMiddleware.py
@@ -10,16 +10,14 @@ class RoleCheckMiddleware:
         self.debug = config("DEBUG") != "0"
 
     def __call__(self, request):
-        print(80 * "-")
-        print("role check")
+        # print(80 * "-")
+        # print("role check")
 
         access_token = request.access_token
-        print(access_token)
 
         request.roles = get_roles(access_token)
-        print(request.roles)
 
-        if self.debug:
-            print("returning")
+        # if self.debug:
+        #     print("returning")
 
         return self.get_response(request)
