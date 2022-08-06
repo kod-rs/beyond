e27b104d0bf4e11753e9e0a2c39d3283e9026095
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-06 23:20:03 +0200

    backend api middleware authCheckMiddleware.py - ref

--- backend/api/middleware/authCheckMiddleware.py
+++ backend/api/middleware/authCheckMiddleware.py
@@ -15,7 +15,6 @@ class AuthCheckMiddleware:
         self.debug = config("DEBUG") != "0"
 
     def __call__(self, request):
-        print(80 * "-")
 
         rejection = {
             "auth": {
@@ -43,15 +42,11 @@ class AuthCheckMiddleware:
                     auth_credentials[k] = request.headers[k]
 
         if request.body:
-            print("decoding", request.body)
             body_content = decode_data(request.body)
 
             for k, v in auth_credentials.items():
                 if k in body_content:
                     auth_credentials[k] = body_content[k]
-
-        for i in auth_credentials.items():
-            print(i)
 
         username = auth_credentials["username"]
         password = auth_credentials["password"]
@@ -66,31 +61,19 @@ class AuthCheckMiddleware:
                 action = request.headers["action"]
 
         if request.body:
-            print("decoding", request.body)
             body_content = decode_data(request.body)
             if "action" in body_content:
                 action = body_content["action"]
 
-        if self.debug:
-            print(f"{username=}")
-            print(f"{password=}")
-            print(f"{str(access_token)[:15]=}")
-            print(f"{str(refresh_token)[:15]=}")
-
         is_validated = None
 
         if username and password:
-            if self.debug:
-                print("using user pass")
 
             res = login(username, password)
 
             is_validated = res["ok"]
 
         elif access_token and refresh_token:
-
-            if self.debug:
-                print("using tokens")
 
             res = check_tokens(
                 access_token,
@@ -100,14 +83,9 @@ class AuthCheckMiddleware:
             is_validated = res["is_valid"]
 
         if not is_validated:
-            if self.debug:
-                print("not valid")
 
             rejection["debug"] = "not is_validated"
             return JsonResponse(rejection)
-
-        if self.debug:
-            print("returning correct")
 
         access_token = res["access_token"]
         refresh_token = res["refresh_token"]
@@ -118,8 +96,5 @@ class AuthCheckMiddleware:
         request.action = action
         request.access_token = access_token
         request.refresh_token = refresh_token
-
-        if self.debug:
-            print("returning")
 
         return self.get_response(request)
