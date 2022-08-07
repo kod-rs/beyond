f484548d7e88a73f587e0fb441700e27827051f1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-07 22:38:41 +0200

    src components navigation  - wip

---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <TopNavigationBar />
+    <ContentNavigationBar />
+
+</template>
+
+<script>
+import ContentNavigationBar from './ContentNavigationBar.vue';
+import TopNavigationBar from './TopNavigationBar.vue';
+export default {
+    components: { ContentNavigationBar, TopNavigationBar }
+}
+</script>
+
