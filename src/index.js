fdcaba86ebdd1f7603fd766e2bfa2a735cbee474
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-07 22:38:41 +0200

    src index.js - wip

--- src/index.js
+++ src/index.js
@@ -1,57 +1,17 @@
 import { createApp } from 'vue';
 import App from './template/App.vue'
-import { router } from './_helpers';
-
-
-
-
-
-// import { createApp } from 'vue'
-import { createStore } from 'vuex'
-
-// Create a new store instance.
-const store = createStore({
-    state() {
-        return {
-            count: 0,
-            synchronizer_token: ""
-        }
-    },
-    mutations: {
-        increment(state) {
-            state.count++
-        },
-        setSynchronizerToken(synchronizer_token, newValue) {
-            synchronizer_token = newValue;
-        }
-    }
-})
-
-
-
-
-
-
+import { router } from './scripts/router';
+import { store } from "./scripts/store"
 
 const app = createApp(App);
 
-
-
-
 app.use(store)
-
-
-
-
 
 app.use(router).mount('#app');
 
-import TopNavigationBar from './components/TopNavigationBar.vue'
-import ContentNavigationBar from './components/ContentNavigationBar.vue'
-import Map from './components/Map.vue'
-import LocationForm from './components/LocationForm.vue'
-import CSRFToken from './components/CSRFToken.vue'
-
+import Map from './components/map/Map.vue'
+import CSRFToken from './components/form/CSRFToken.vue'
+import Navigation from './components/navigation/Navigation.vue'
 import OpenLayersMap from 'vue3-openlayers'
 import 'vue3-openlayers/dist/vue3-openlayers.css'
 
@@ -61,11 +21,9 @@ const globalSTS = sts.getSTS({ 'max-age': { 'days': 10 }, 'includeSubDomains': t
 app.use(OpenLayersMap, globalSTS);
 
 app
-    .component('TopNavigationBar', TopNavigationBar)
-    .component('ContentNavigationBar', ContentNavigationBar)
     .component('Map', Map)
-    .component('LocationForm', LocationForm)
     .component('CSRFToken', CSRFToken)
+    .component('Navigation', Navigation)
 
 // webpack
 
