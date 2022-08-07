c1d80c316a7ece750666c5d3ab24119c7f3be1d0
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-07 23:58:09 +0200

    playground marin-jovanovic git.sh - wip

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,64 +1,18 @@
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/navigation
-git commit -m "src components navigation  - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/router.js
-git commit -m "src _helpers router.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/tab_name_changer.js
-git commit -m "src scripts tab_name_changer.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/views/login/LoginPage.vue
-git commit -m "src views login LoginPage.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/api.js
-git commit -m "src scripts api.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/store.js
-git commit -m "src scripts store.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/TopNavigationBar.vue
-git commit -m "src components TopNavigationBar.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/template/App.vue
-git commit -m "src template App.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/Map.vue
-git commit -m "src components Map.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/store/main.js
-git commit -m "src store main.js - wip"
 git add /home/kim/Documents/github/kod-rs/beyond/src/template/index.html
 git commit -m "src template index.html - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_services/api.js
-git commit -m "src _services api.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/todo_map.js
-git commit -m "src scripts todo_map.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
-git commit -m "playground marin-jovanovic git.sh - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/LocationForm.vue
-git commit -m "src components LocationForm.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/tab_name_changer.js
-git commit -m "src _helpers tab_name_changer.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/CSRFToken.vue
-git commit -m "src components CSRFToken.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/MapContainer.vue
-git commit -m "src components MapContainer.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/router.js
-git commit -m "src scripts router.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/map
-git commit -m "src components map  - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/constants.js
-git commit -m "src scripts constants.js - wip"
 git add /home/kim/Documents/github/kod-rs/beyond/src/index.js
 git commit -m "src index.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/views/location/View.vue
-git commit -m "src views location View.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/ContentNavigationBar.vue
-git commit -m "src components ContentNavigationBar.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/views/index/IndexPage.vue
-git commit -m "src views index IndexPage.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/constants.js
-git commit -m "src _helpers constants.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/views/location/Add.vue
-git commit -m "src views location Add.vue - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/form
-git commit -m "src components form  - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_services/index.js
-git commit -m "src _services index.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_services/user.service.js
-git commit -m "src _services user.service.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/index.js
-git commit -m "src _helpers index.js - wip"
-git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/map/index.js
-git commit -m "src scripts map index.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/package.json
+git commit -m "package.json - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
+git commit -m "playground marin-jovanovic git.sh - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/map/Map.vue
+git commit -m "src components map Map.vue - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/store.js
+git commit -m "src scripts store.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/scripts/api.js
+git commit -m "src scripts api.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/backend/api/view/locationsView.py
+git commit -m "backend api view locationsView.py - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/yarn.lock
+git commit -m "yarn.lock - wip"
