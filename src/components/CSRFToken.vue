4817230382c6cf6b74882885a6fa31588994ca5d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-07 18:07:37 +0200

    src components CSRFToken.vue - wip

--- src/components/CSRFToken.vue
+++ src/components/CSRFToken.vue
@@ -1,6 +1,10 @@
 <template>
     <!-- <input type="hidden" name="csrf-token" value={{}} /> -->
+
+    <!-- one way binding -->
     <!-- <input name="csrf-token" :value=content /> -->
+
+    <!-- two way binding -->
     <input name="csrf-token" v-model=content />
     <br>
 
@@ -16,13 +20,16 @@ export default {
         }
     },
     async mounted() {
+        this.$store.commit('increment')
+        console.log(this.$store.state.count)
+        console.log("-")
         const CSRFPayload = await userService.getCSRFAuthData();
 
         if (CSRFPayload["status"]) {
             this.content = CSRFPayload["synchronizer_token"]
 
         } else {
-            // error generating
+            // error generating, user will not be able to submit form
             this.content = ""
         }
     }
