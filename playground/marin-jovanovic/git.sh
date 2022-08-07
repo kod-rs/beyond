b3fa86e04cf02a478c1e6724a0326b0cdc67223b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-07 18:07:37 +0200

    playground marin-jovanovic git.sh -

--- playground/marin-jovanovic/git.sh
+++ playground/marin-jovanovic/git.sh
@@ -1,12 +1,25 @@
-git add /home/kim/Documents/github/kod-rs/beyond/docs/task/.~lock.3_Task6.5 KONCAR 27-01-2022.pptx#
-git commit -m "docs task .~lock.3_Task6.5 KONCAR 27-01-2022.pptx# - "
-git add /home/kim/Documents/github/kod-rs/beyond/src/_helpers/router.js
-git commit -m "src _helpers router.js - add routes"
-git add /home/kim/Documents/github/kod-rs/beyond/src/views
-git commit -m "src views  - add view folder"
-git add /home/kim/Documents/github/kod-rs/beyond/src/components/ContentNavigationBar.vue
-git commit -m "src components ContentNavigationBar.vue - fix href"
+
+git add /home/kim/Documents/github/kod-rs/beyond/src/index.js
+git commit -m "src index.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/_services/user.service.js
+git commit -m "src _services user.service.js - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/views/location/LocationAdd.vue
+git commit -m "src views location LocationAdd.vue - wip"
 git add /home/kim/Documents/github/kod-rs/beyond/playground/marin-jovanovic/git.sh
 git commit -m "playground marin-jovanovic git.sh - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/ContentNavigationBar.vue
+git commit -m "src components ContentNavigationBar.vue - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/LocationForm.vue
+git commit -m "src components LocationForm.vue - wip"
 git add /home/kim/Documents/github/kod-rs/beyond/src/index/IndexPage.vue
-git commit -m "src index IndexPage.vue - ref"
+git commit -m "src index IndexPage.vue - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/template/index.html
+git commit -m "src template index.html - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/components/CSRFToken.vue
+git commit -m "src components CSRFToken.vue - wip"
+git add /home/kim/Documents/github/kod-rs/beyond/src/store
+git commit -m "src store  - init"
+git add /home/kim/Documents/github/kod-rs/beyond/a.txt
+git commit -m "a.txt - "
+git add /home/kim/Documents/github/kod-rs/beyond/src/template/App.vue
+git commit -m "src template App.vue - wip"
