48f1323fc7ebc06508dfa5f1c339a560171ded8e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-07 18:07:36 +0200

    src index.js - wip

--- src/index.js
+++ src/index.js
@@ -2,7 +2,48 @@ import { createApp } from 'vue';
 import App from './template/App.vue'
 import { router } from './_helpers';
 
+
+
+
+
+// import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
+// Create a new store instance.
+const store = createStore({
+    state() {
+        return {
+            count: 0,
+            synchronizer_token: ""
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        },
+        setSynchronizerToken(synchronizer_token, newValue) {
+            synchronizer_token = newValue;
+        }
+    }
+})
+
+
+
+
+
+
+
 const app = createApp(App);
+
+
+
+
+app.use(store)
+
+
+
+
+
 app.use(router).mount('#app');
 
 import TopNavigationBar from './components/TopNavigationBar.vue'
@@ -13,7 +54,6 @@ import CSRFToken from './components/CSRFToken.vue'
 
 import OpenLayersMap from 'vue3-openlayers'
 import 'vue3-openlayers/dist/vue3-openlayers.css'
-
 
 const sts = require('strict-transport-security');
 const globalSTS = sts.getSTS({ 'max-age': { 'days': 10 }, 'includeSubDomains': true });
