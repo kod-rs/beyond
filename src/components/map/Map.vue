7a99e7da5ce6cc7419fae4ad92756d72c29ca16b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-10 00:34:13 +0200

    src components map Map.vue - wip; make checkpoint for migration to bare ol

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -1,61 +1,75 @@
+
+
 <template>
+    <img src='marker-blue.png' />
+    <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:400px" ref="map">
 
-    <UserCoordinates></UserCoordinates>
-
-    <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:400px">
         <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection" />
 
         <ol-tile-layer>
             <ol-source-osm />
         </ol-tile-layer>
 
-        <ol-vector-layer>
-            <ol-source-vector ref="vectors">
-                <ol-interaction-draw @drawstart="drawstart" :type="drawType">
-                </ol-interaction-draw>
-            </ol-source-vector>
+        <ol-geolocation :projection="projection" @positionChanged="geoLocChange">
+            <template v-slot="slotProps">
+                <ol-vector-layer :zIndex="2">
+                    <ol-source-vector>
+                        <ol-feature ref="positionFeature">
+                            <ol-geom-point :coordinates="slotProps.position"></ol-geom-point>
+                            <ol-style>
+                                <ol-style-icon :src="hereIcon" :scale="0.1"></ol-style-icon>
+                            </ol-style>
+                        </ol-feature>
+                    </ol-source-vector>
 
-            <ol-style>
-                <ol-style-icon :src="markerIcon" :scale="2"></ol-style-icon>
-            </ol-style>
-        </ol-vector-layer>
+                </ol-vector-layer>
+            </template>
+        </ol-geolocation>
 
     </ol-map>
 </template>
 
 <script>
-import markerIcon from './../../assets/login_logo.png'
+import hereIcon from './marker-blue.png'
+// import hereIcon from './../../assets/marker-blue.png'
 
-import { ref } from "vue";
-import UserCoordinates from "./UserCoordinates.vue"
+// import hereIcon from '@/assets/here.png'
+import {
+    ref
+} from 'vue'
 export default {
+    data() {
+        imageF: "marker-blue.png"
+
+    },
 
     setup() {
-        const center = ref([54.1966794, 31.8797732]);
-        const projection = ref("EPSG:4326");
-        const zoom = ref(6);
-        const rotation = ref(0);
-        const markers = ref(null);
-        const drawType = ref("Point");
-        const drawedMarker = ref();
-        const vectors = ref(null);
-        const drawstart = (event) => {
-            vectors.value.source.removeFeature(drawedMarker.value);
-            drawedMarker.value = event.feature;
-            console.log(vectors.value.source);
-        };
+        const center = ref([40, 40])
+        const projection = ref('EPSG:4326')
+        const zoom = ref(8)
+        const rotation = ref(0)
+
+        const view = ref(null)
+        const map = ref(null)
+
+        const geoLocChange = (loc) => {
+            console.log(loc);
+            view.value.fit([loc[0], loc[1], loc[0], loc[1]], {
+                maxZoom: 14
+            })
+        }
+
         return {
-            vectors,
-            drawstart,
             center,
             projection,
             zoom,
             rotation,
-            markerIcon,
-            markers,
-            drawType
-        };
+            hereIcon,
+            view,
+            map,
+            geoLocChange
+        }
     },
-    components: { UserCoordinates }
 }
+
 </script>
