4e10847448522da9c71ae2734a64ee817eace227
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-09 15:13:07 +0200

    src index.js - fix show user geo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
+// webpack dev version
+
 import { createApp } from 'vue';
 import App from './template/App.vue'
 import { router } from './scripts/router';
 import { store } from "./scripts/store"
-
-const app = createApp(App);
-
-// app.use(store)
-
-app.use(router, store).mount('#app');
-
-store.commit('setLatitude', 2);
-console.log("store lat", store.state.latitude)
-
 import Map from './components/map/Map.vue'
 import CSRFToken from './components/form/CSRFToken.vue'
 import Navigation from './components/navigation/Navigation.vue'
 import OpenLayersMap from 'vue3-openlayers'
 import 'vue3-openlayers/dist/vue3-openlayers.css'
-
 const sts = require('strict-transport-security');
+
+const app = createApp(App);
+
+// app.use(store)
+
+app.use(router).use(store).mount('#app');
+
+store.commit('setLatitude', 12);
+console.log("store lat", store.state.latitude)
 const globalSTS = sts.getSTS({ 'max-age': { 'days': 10 }, 'includeSubDomains': true });
 
 // import VueGeolocation from 'vue-browser-geolocation';
