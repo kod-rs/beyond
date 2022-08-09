913866cd7dbeae6b741a1a6a911240af11ba27b6
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-09 15:13:07 +0200

    src main.js - fix show user geo

--- src/main.js
+++ src/main.js
@@ -1,32 +1,4 @@
-// import { createApp } from 'vue';
-// import App from './template/App.vue'
-// import { router } from './_helpers';
-
-// const app = createApp(App);
-// app.use(router).mount('#app');
-
-// import TopNavigationBar from './components/TopNavigationBar.vue'
-// import ContentNavigationBar from './components/ContentNavigationBar.vue'
-// import Map from './components/Map.vue'
-// import LocationForm from './components/LocationForm.vue'
-
-// import OpenLayersMap from 'vue3-openlayers'
-// import 'vue3-openlayers/dist/vue3-openlayers.css'
-
-// const sts = require('strict-transport-security');
-// const globalSTS = sts.getSTS({'max-age':{'days': 10}, 'includeSubDomains': true});
-
-// app.use(OpenLayersMap, globalSTS);
-
-// app
-//     .component('TopNavigationBar', TopNavigationBar)
-//     .component('ContentNavigationBar', ContentNavigationBar)
-//     .component('Map', Map)
-//     .component('LocationForm', LocationForm)
-
-// // webpack
-
-// console.log("src index")
+// webpack dev version
 
 import { createApp } from 'vue';
 import App from './template/App.vue'
@@ -64,4 +36,4 @@ app
 
 // webpack
 
-console.log("src index")
+console.log("src main")
