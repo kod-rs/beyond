3ec5f9f265e68242968db1bb36358ec639f92a11
Ivan <ivan.cindric95@gmail.com>
2022-08-10 13:30:04 +0200

    backend api model location - change x and y types

--- backend/api/model/location.py
+++ backend/api/model/location.py
@@ -4,5 +4,5 @@ from django.db import models
 class Location(models.Model):
     section = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
-    latitude = models.CharField(max_length=200)
-    longitude = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
