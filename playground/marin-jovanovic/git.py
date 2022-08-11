6e90b5e587948ef2aec364043081e0194274b844
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-12 00:47:35 +0200

    playground marin-jovanovic git.py - add zoom to user location

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "vue"
+    comment = "add zoom to user location"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
