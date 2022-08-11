5628ddadd3015c1af3841faa9234b708dc848619
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-11 19:58:44 +0200

    playground marin-jovanovic git.py - move img

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "fix image loading"
+    comment = "move img"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
