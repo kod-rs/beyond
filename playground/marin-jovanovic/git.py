439adb43e130b5ed88bd08a6536889e54c6146db
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 22:55:32 +0200

    playground marin-jovanovic git.py - fix config

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "wip: fix webpack config to babel"
+    comment = "fix config"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
