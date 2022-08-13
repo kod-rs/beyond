bfdc6f13ff3f0490e4db54f7f5ab4c933b91ba57
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 18:47:57 +0200

    src main.js - wip: restore point for webpack

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// webpack dev version
-
 import { createApp } from 'vue';
 import App from './template/App.vue'
 import { router } from './scripts/router';
@@ -7,7 +5,7 @@ import { store } from "./scripts/store"
 import Map from './components/map/Map.vue'
 import CSRFToken from './components/form/CSRFToken.vue'
 import InputAutocomplete from './components/form/InputAutocomplete.vue';
-
+import TestNavigation from './components/navigation/TestNavigation.vue'
 import Navigation from './components/navigation/Navigation.vue'
 import OpenLayersMap from 'vue3-openlayers'
 import 'vue3-openlayers/dist/vue3-openlayers.css'
@@ -16,28 +14,22 @@ import TopNavigationBar from "./components/navigation/TopNavigationBar.vue";
 const app = createApp(App);
 import UserCoordinates from "./components/map/UserCoordinates.vue"
 
-// app.use(store)
-
-app.use(router).use(store).mount('#app');
 
 const globalSTS = sts.getSTS({ 'max-age': { 'days': 10 }, 'includeSubDomains': true });
 
-// import VueGeolocation from 'vue-browser-geolocation';
-// app.use(VueGeolocation);
-
-
-app.use(OpenLayersMap, globalSTS);
+app.use(router).use(store).mount('#app')
 
 app
+    .use(OpenLayersMap, globalSTS)
     .component('Map', Map)
     .component('CSRFToken', CSRFToken)
     .component("InputAutocomplete", InputAutocomplete)
     .component('Navigation', Navigation)
+    .component("TestNavigation", TestNavigation)
     .component("TopNavigationBar", TopNavigationBar)
     .component("UserCoordinates", UserCoordinates)
-// webpack
 
-console.log("src index")
+console.log("production entry point")
 
 /////////////////////////
 // production entry point
