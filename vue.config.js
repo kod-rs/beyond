99ec18bcf3283c4aab3dca9c793f985c89a6d35b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 22:12:53 +0200

    vue.config.js - wip: fix webpack config to babel

--- vue.config.js
+++ vue.config.js
@@ -1,51 +1,113 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === "production" ? "/beyond/" : "/",
-//   // configureWebpack: config => {
-
-//   //   // if (process.env.NODE_ENV === 'production') {
-//   //   if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
-//   //     // mutate config for production...
-//   //     console.log("dev")
-//   //   } else {
-//   //     console.log("production")
-//   //     // mutate for development...
-//   //   }
-//   // }
-
-// };
-
-const path = require("path");
-
-console.log("vue config ")
-
-module.exports = {
-  // assetsDir: "./src_vue",
-
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: 'http://localhost:8000/'
+  },
   // chainWebpack: config => {
-  //   config
-  //     .entry("app")
-  //     .clear()
-  //     .add("./src_vue/index.js")
-  //     .end();
-  //   config.resolve.alias
-  //     .set("@", path.join(__dirname, "./src_vue"));
-  //   config
-  //     .plugin('html')
-  //     .tap(args => {
-  //       args[0].template = '/src_vue/template/index.html'
-  //       return args
-  //     })
-  // },
+  // config.module
+  //   .rule('img')
+  //   .test(/\.(png|svg|jpg|jpeg|gif|json)$/i)
+  //   .type('asset/resource')
+  //   .end()
 
-  // chainWebpack: config => {
-  //   config
-  //     .plugin('html')
-  //     .tap(args => {
-  //       args[0].template = '/Users/username/proj/app/templates/index.html'
-  //       return args
-  //     })
   // }
+})
 
-  publicPath: process.env.NODE_ENV === "production" ? "/beyond/" : "/",
 
-};
+// ////////////////////////////////////////////////////
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { VueLoaderPlugin } = require('vue-loader')
+// const path = require('path');
+// const fs = require('fs');
+// const webpack = require('webpack');
+
+
+// //const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
+// // const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
+
+// module.exports = {
+//   mode: 'development',
+//   resolve: {
+//     extensions: ['.js', '.vue', '.css']
+//   },
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.vue$/,
+//         exclude: /node_modules/,
+//         loader: 'vue-loader'
+//       },
+//       {
+//         test: /\.js?$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader'
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           { loader: "style-loader" },
+//           { loader: "css-loader" },
+//         ],
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//         type: 'asset/resource',
+//       },
+//       // {
+//       //     test: /\.(ico)$/i,
+//       //     type: 'asset/resource',
+//       // },
+//       {
+//         test: /\.(json)$/i,
+//         type: 'asset/resource',
+//       },
+//     ]
+//   },
+//   plugins: [
+//     new VueLoaderPlugin(),
+//     // new HtmlWebpackPlugin({
+//     //     template: 'src/template/index.html',
+//     //     title: 'Beyond',
+//     //     BASE_URL: "localhost, not working"
+//     // }),
+//     // new webpack.ProvidePlugin({
+//     //     // Make a global `process` variable that points to the `process` package,
+//     //     // because the `util` package expects there to be a global variable named `process`.
+//     //     // Thanks to https://stackoverflow.com/a/65018686/14239942
+//     //     process: 'process/browser'
+//     // })
+//     // new webpack.DefinePlugin({
+//     //     'process.env': JSON.stringify(dotenv.parsed),
+//     // }),
+//     //        new webpack.DefinePlugin({
+//     //      'process.env.NODE_ENV': JSON.stringify(dotenv.NODE_ENV),
+//     //      'process.env.MY_ENV': JSON.stringify(dotenv.MY_ENV),
+//     //    })
+//   ],
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'src', 'assets'),
+//     },
+//     historyApiFallback: true,
+//     headers: {
+//       "X-Powered-By": 'Beyond',
+//       "Cache-Control": "no-cache, no-store, must-revalidate",
+//       "Pragma": "no-cache",
+//       "Strict-Transport-Security": "max-age=31536000; includeSubDomains"
+//     },
+//     proxy: {
+//       '/api*': {
+//         target: 'http://localhost:8000/',
+//       },
+
+//     },
+
+//   },
+//   externals: {
+//     config: JSON.stringify({
+//       djangoApi: 'http://localhost:8000'
+//     })
+//   }
+// }
