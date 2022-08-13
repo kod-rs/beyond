ec0688ee1ef1954e7e3168f7d8aa6c2f3eea9e00
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 18:47:57 +0200

    webpack.config.js - wip: restore point for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader')
 const path = require('path');
 const fs = require('fs');
-//const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 const webpack = require('webpack');
+
+
+//const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 // const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 
 module.exports = {
@@ -35,6 +37,10 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
+            // {
+            //     test: /\.(ico)$/i,
+            //     type: 'asset/resource',
+            // },
             {
                 test: /\.(json)$/i,
                 type: 'asset/resource',
@@ -48,12 +54,12 @@ module.exports = {
             title: 'Beyond',
             BASE_URL: "localhost, not working"
         }),
-        new webpack.ProvidePlugin({
-            // Make a global `process` variable that points to the `process` package,
-            // because the `util` package expects there to be a global variable named `process`.
-            // Thanks to https://stackoverflow.com/a/65018686/14239942
-            process: 'process/browser'
-        })
+        // new webpack.ProvidePlugin({
+        //     // Make a global `process` variable that points to the `process` package,
+        //     // because the `util` package expects there to be a global variable named `process`.
+        //     // Thanks to https://stackoverflow.com/a/65018686/14239942
+        //     process: 'process/browser'
+        // })
         // new webpack.DefinePlugin({
         //     'process.env': JSON.stringify(dotenv.parsed),
         // }),
