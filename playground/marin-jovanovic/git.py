93c3ef41d25202fcb8a676a6ff7503dd5560f090
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 23:33:38 +0200

    playground marin-jovanovic git.py - wip

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "fix config"
+    comment = "wip"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
