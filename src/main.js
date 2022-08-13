05c5103ccb828399a36532f22d44303ca1c3b7d1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 15:37:12 +0200

    src main.js - add: track mouse on map and log country

--- src/main.js
+++ src/main.js
@@ -4,23 +4,22 @@ import { createApp } from 'vue';
 import App from './template/App.vue'
 import { router } from './scripts/router';
 import { store } from "./scripts/store"
-
-const app = createApp(App);
-
-// app.use(store)
-
-app.use(router, store).mount('#app');
-
-store.commit('setLatitude', 2);
-console.log("store lat", store.state.latitude)
-
 import Map from './components/map/Map.vue'
 import CSRFToken from './components/form/CSRFToken.vue'
+import InputAutocomplete from './components/form/InputAutocomplete.vue';
+
 import Navigation from './components/navigation/Navigation.vue'
 import OpenLayersMap from 'vue3-openlayers'
 import 'vue3-openlayers/dist/vue3-openlayers.css'
-
 const sts = require('strict-transport-security');
+import TopNavigationBar from "./components/navigation/TopNavigationBar.vue";
+const app = createApp(App);
+import UserCoordinates from "./components/map/UserCoordinates.vue"
+
+// app.use(store)
+
+app.use(router).use(store).mount('#app');
+
 const globalSTS = sts.getSTS({ 'max-age': { 'days': 10 }, 'includeSubDomains': true });
 
 // import VueGeolocation from 'vue-browser-geolocation';
@@ -32,8 +31,15 @@ app.use(OpenLayersMap, globalSTS);
 app
     .component('Map', Map)
     .component('CSRFToken', CSRFToken)
+    .component("InputAutocomplete", InputAutocomplete)
     .component('Navigation', Navigation)
-
+    .component("TopNavigationBar", TopNavigationBar)
+    .component("UserCoordinates", UserCoordinates)
 // webpack
 
-console.log("src main")
+console.log("src index")
+
+/////////////////////////
+// production entry point
+/////////////////////////
+
