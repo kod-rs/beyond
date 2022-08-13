93bab52754c0b83aae253fdfdb2de465e4222245
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 23:03:56 +0200

    playground marin-jovanovic common.sh - add command for deleting branches

--- playground/marin-jovanovic/common.sh
+++ playground/marin-jovanovic/common.sh
@@ -2,3 +2,7 @@
 # not intended to be run as a script
 
 pip3 freeze > requirements.txt
+
+
+# delete all non existing branches
+git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
