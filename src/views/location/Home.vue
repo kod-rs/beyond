a59a3e6dc78ca2e697fbe263e0f21a2772e003c0
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-13 22:55:31 +0200

    src views location Home.vue - fix config

--- src/views/location/Home.vue
+++ src/views/location/Home.vue
@@ -2,7 +2,17 @@
 
 
     home where are you?
-
+    <br>
+    <hr>
+    <button>add new location</button>
+    <hr>
+    <button>delete existing</button>
+    <hr>
+    <button>edit existing</button>
+    <br>
+    change group?
+    <br>
+    change setting?
 
 </template>
 
