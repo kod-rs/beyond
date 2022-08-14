9ad2637e54ab774f04e637de01efed076ab076fc
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-14 18:41:05 +0200

    playground marin-jovanovic git.py - ref: rewrite msgBodyCheckMiddleware

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "wip: role check rewrite"
+    comment = "ref: rewrite msgBodyCheckMiddleware"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
