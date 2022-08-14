0dfcee0f0847b5af5f48965ee9d429759b94169c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 00:59:12 +0200

    playground marin-jovanovic git.py - fix: show only user locations in view & fix role checking

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "fix: add location"
+    comment = "fix: show only user locations in view & fix role checking"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
