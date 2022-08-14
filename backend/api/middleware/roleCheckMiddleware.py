006491dff7b17f161cca94cc9439df6d02ce4ea2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-14 13:33:35 +0200

    backend api middleware roleCheckMiddleware.py - wip: role check rewrite

--- backend/api/middleware/roleCheckMiddleware.py
+++ backend/api/middleware/roleCheckMiddleware.py
@@ -1,6 +1,8 @@
 from decouple import config
 
 from backend.api.keycloak.keycloak_manager import get_roles
+from backend.api.comm.role_validator import SchemeValidator
+from backend.api.config.main import MIDDLEWARE_NO_ACTION, INTERNAL_SERVER_ERROR_MESSAGE
 
 
 class RoleCheckMiddleware:
@@ -8,16 +10,30 @@ class RoleCheckMiddleware:
         self.get_response = get_response
 
         self.debug = config("DEBUG") != "0"
+        self.scheme_validator = SchemeValidator()
 
     def __call__(self, request):
-        # print(80 * "-")
-        # print("role check")
 
         access_token = request.access_token
 
-        request.roles = get_roles(access_token)
 
-        # if self.debug:
-        #     print("returning")
+        if not hasattr(request, "action"):
+            print("internal server error, check action middleware, "
+                  "he is not adding empty action")
+            raise Exception(INTERNAL_SERVER_ERROR_MESSAGE)
+
+        if request.action == MIDDLEWARE_NO_ACTION:
+            print("action is empty")
+            request.action_checked = MIDDLEWARE_NO_ACTION
+        else:
+            print(f"has action: {request.action=}")
+            roles = get_roles(access_token)
+            action = request.action
+            r = self.scheme_validator.check_action(roles, action)
+            print("checking action against role", r)
+            request.action_checked = MIDDLEWARE_NO_ACTION
+            # print("roles", roles)
+
+        request.roles = get_roles(access_token)
 
         return self.get_response(request)
