f8dbec19bb01fa9c9f39d0ea6fe83c0b4c574b5a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 00:59:12 +0200

    backend api middleware msgBodyCheckMiddleware.py - fix: show only user locations in view & fix role checking

--- backend/api/middleware/msgBodyCheckMiddleware.py
+++ backend/api/middleware/msgBodyCheckMiddleware.py
@@ -1,23 +1,20 @@
 import json
-
-from decouple import config
 from django.http import JsonResponse
-
-from backend.api.middleware.comm import get_empty_response_template, middleware_check_params
+from backend.api.middleware.comm import middleware_check_params
+from backend.api.comm.http import get_empty_response_template
 from backend.api.comm.json_loader import vue_interface_cfg
 from backend.api.middleware.comm import DebuggableMiddleware
 
-class MsgBodyCheckMiddleware(DebuggableMiddleware):
-    def __init__(self, get_response):
 
-        # self.get_response = get_response
-        # self.debug = config("DEBUG") != "0"
+class MsgBodyCheckMiddleware(DebuggableMiddleware):
+
+    def __init__(self, get_response):
         super().__init__(get_response)
+
         self.vue_interface_cfg = vue_interface_cfg
 
     def __call__(self, request):
-
-
+        print("MsgBodyCheckMiddleware")
         rejection = get_empty_response_template()
 
         if request.environ['QUERY_STRING'] != '':
@@ -29,70 +26,19 @@ class MsgBodyCheckMiddleware(DebuggableMiddleware):
             body = json.loads(request.body)
             keys = list(body.keys())
 
-            # print("MsgBodyCheckMiddleware")
-            #
-            # print(request.action)
-            # print(f"{keys=}")
-
-            # todo
-
-            #
-            # print(str(request.get_full_path).strip("/"))
-
-            t = request.path
-            t = str(request.path).strip("/")
-
-            # print(t)
-
-            t = middleware_check_params(
-
-                vue_interface_cfg=self.vue_interface_cfg,
-                # route=request.action.split(";")[0],
-                c=request.action,
+            if not middleware_check_params(
+                action_composite=request.action,
                 given=keys
-            )
-
-            print("check params", t)
-
-            if not t:
+            ):
+                print(f"\tmissing something, {request.action=}")
+                print(f"\tprovided {keys}")
                 return JsonResponse(rejection)
 
-            # if request.path == '/csrf/':
-            #     if keys != ['access_token', 'refresh_token', 'action']:
-            #         print('rejected csrf')
-            #         return JsonResponse(rejection)
-            #
-            # elif request.path == '/locations/':
-            #     expected_keys = ['access_token', 'refresh_token', 'action']
-            #     if body['action'] == LOCATION_ACTION.DELETE_SINGLE.value:
-            #         expected_keys.append('index')
-            #     if body['action'] == LOCATION_ACTION.ADD_SINGLE.value:
-            #         expected_keys += [
-            #             "username",'type',
-            #                           'section',
-            #                           'latitude',
-            #                           'longitude',
-            #                           'synchronizer_token']
-            #
-            #     if keys != expected_keys:
-            #         print(f"{keys=}")
-            #         print(f"{expected_keys=}")
-            #         print('rejected locations')
-            #         return JsonResponse(rejection)
-            #
-            # elif request.path == '/login/':
-            #     if keys != ['username', 'password']:
-            #         print('rejected login')
-            #         return JsonResponse(rejection)
-            #
-            # elif request.path == '/logout/':
-            #     if list(body.keys()) != ['access_token', 'refresh_token']:
-            #         print('rejected logout')
-            #         return JsonResponse(rejection)
-
             return self.get_response(request)
+
         except Exception as e:
             if self.debug:
                 print(e)
-            print('rejected because of error')
-            return JsonResponse(rejection)
+                print('rejected because of error')
+
+        return JsonResponse(rejection)
