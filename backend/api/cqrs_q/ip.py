1537064db0cf8503b014e16ebc556848e5495d52
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 00:59:13 +0200

    backend api cqrs_q ip.py - fix: show only user locations in view & fix role checking

--- backend/api/cqrs_q/ip.py
+++ backend/api/cqrs_q/ip.py
@@ -5,7 +5,7 @@ def check_max_count(ip, max_count):
     try:
 
         ip_obj = IpCounter.objects.get(ip=ip)
-        print(ip_obj, ip_obj.counter)
+        # print(ip_obj, ip_obj.counter)
         return ip_obj.counter > max_count
 
     except IpCounter.DoesNotExist:
