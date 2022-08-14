721a35a27b3f293bd99ccaf76bd775b50fabc73a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-14 13:33:35 +0200

    backend api config main.py - wip: role check rewrite

--- backend/api/config/main.py
+++ backend/api/config/main.py
@@ -11,3 +11,16 @@ class ACTIONS(enum.Enum):
     ADD = 'add'
     GET_ALL = 'get all'
     DELETE = 'delete'
+
+class LOCATION_ACTION(enum.Enum):
+    ADD_SINGLE = 'location add single'
+    # ADD_MULTIPLE = "add multiple"
+    # DELETE_SINGLE = "delete single"
+    GET_BY_USERNAME = "location get username"
+    GET_ALL = "location get all"
+
+from backend.api.comm.json_loader import role_validation_cfg
+
+MIDDLEWARE_NO_ACTION = role_validation_cfg["no action"]
+MIDDLEWARE_ACTION_DENIED = False
+INTERNAL_SERVER_ERROR_MESSAGE = "internal server error"
