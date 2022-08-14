cac7ddfe4fcf62c9d56908217a0faf023c6280ae
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-14 13:33:35 +0200

    backend api model location.py - wip: role check rewrite

--- backend/api/model/location.py
+++ backend/api/model/location.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Location(models.Model):
+    username = models.CharField(max_length=200)
     section = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
     latitude = models.FloatField()
