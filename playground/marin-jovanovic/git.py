c0f43587bda50db39c007928108800e1597e40ab
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 23:41:32 +0200

    playground marin-jovanovic git.py - add: pick location on map when creating location

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "fix: add map popup isolated example"
+    comment = "add: pick location on map when creating location"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
