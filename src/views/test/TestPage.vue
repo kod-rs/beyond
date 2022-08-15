49e30aca966455a2fc2997ada76bca4f69c07154
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 23:19:03 +0200

    src views test  - fix: add map popup isolated example

---
+++ src/views/test/TestPage.vue
@@ -0,0 +1,85 @@
+/**
+https://openlayers.org/en/latest/examples/popup.html
+ */
+
+<template>
+
+
+
+    <div id="map" class="map"></div>
+    <MapPopup ref="mappopup"></MapPopup>
+
+</template>
+
+
+    <style>
+    .map {
+        width: 100%;
+        height: 800px;
+    }
+    </style>
+
+<script>
+import Map from 'ol/Map';
+
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
+import OSM from 'ol/source/OSM';
+import { toLonLat } from 'ol/proj';
+import { toStringHDMS } from 'ol/coordinate';
+
+
+
+export default {
+    data() {
+
+    },
+    async created() {
+
+
+    },
+    mounted() {
+
+
+        /**
+         * Create the map.
+         *   
+                 */
+        const map = new Map({
+            layers: [
+                new TileLayer({
+                    source: new OSM(),
+                }),
+
+            ],
+            // overlays: [overlay],
+            target: 'map',
+            view: new View({
+                center: [0, 0],
+                zoom: 2,
+            }),
+        });
+
+        map.addOverlay(this.$refs.mappopup.getOverlay())
+
+        /**
+         * Add a click handler to the map to render the popup.
+         */
+        map.on('singleclick', (evt) => {
+            // console.log("clicked", evt)
+            //     console.log("clicked", evt)
+            const coordinate = evt.coordinate;
+            const hdms = toStringHDMS(toLonLat(coordinate));
+
+            console.log("log coord", hdms)
+            console.log("set postiion,", coordinate)
+            this.$refs.mappopup.setText(hdms);
+            this.$refs.mappopup.setPosition(coordinate);
+            //     content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+            //     overlay.setPosition(coordinate);
+        });
+    }
+
+};
+</script> 
