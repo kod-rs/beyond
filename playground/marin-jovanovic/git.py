fcf2bf6d05140cb90e7c9b78c4a27be403279441
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 23:19:03 +0200

    playground marin-jovanovic git.py - fix: add map popup isolated example

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "add: add locations from db"
+    comment = "fix: add map popup isolated example"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
