1771bd1980bc10c019fac3fcff0f1d62a7478a4c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 23:41:32 +0200

    src views location AddLocation.vue - add: pick location on map when creating location

--- src/views/location/AddLocation.vue
+++ src/views/location/AddLocation.vue
@@ -40,6 +40,10 @@
 
 
                     </form>
+
+                    <div id="map" class="map"></div>
+                    <MapPopup ref="mappopup"></MapPopup>
+
                 </div>
 
             </div>
@@ -49,8 +53,26 @@
 
 </template>
 
+    <style>
+    .map {
+        width: 100%;
+        height: 100px;
+    }
+    </style>
+
+
 <script>
 import { apiCalls } from '../../scripts/api';
+import Map from 'ol/Map';
+
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
+import OSM from 'ol/source/OSM';
+import { toLonLat } from 'ol/proj';
+import { toStringHDMS } from 'ol/coordinate';
+
+
 
 export default {
     data() {
@@ -107,6 +129,46 @@ export default {
 
             this.submitted = false;
         }
+    },
+    mounted() {
+        /**
+      * Create the map.
+      *   
+     */
+
+
+        const map = new Map({
+            layers: [
+                new TileLayer({
+                    source: new OSM(),
+                }),
+
+            ],
+            target: 'map',
+            view: new View({
+                center: [0, 0],
+                zoom: 2,
+            }),
+        });
+
+        map.addOverlay(this.$refs.mappopup.getOverlay())
+
+        /**
+         * Add a click handler to the map to render the popup.
+         */
+        map.on('singleclick', (evt) => {
+
+            const coordinate = evt.coordinate;
+            const hdms = toStringHDMS(toLonLat(coordinate));
+            this.formData[2].value = toLonLat(coordinate)[0];
+            this.formData[3].value = toLonLat(coordinate)[1];
+
+            console.log("log coord", hdms)
+            console.log("set postiion,", coordinate)
+            this.$refs.mappopup.setText(hdms);
+            this.$refs.mappopup.setPosition(coordinate);
+
+        });
     }
 };
 </script> 
