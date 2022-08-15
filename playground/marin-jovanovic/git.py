3efe175a734486cde6e77a9566e60c6364e369df
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-16 00:01:41 +0200

    playground marin-jovanovic git.py - wip:

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "add: pick location on map when creating location"
+    comment = "wip:"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
