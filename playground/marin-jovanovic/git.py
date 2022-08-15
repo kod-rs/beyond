045d6078371a4d5b6c95d9d0588ef0141bfc2460
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-15 19:55:29 +0200

    playground marin-jovanovic git.py - add: add locations from db

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -10,7 +10,7 @@ def main():
 
     same_comment = True
     comment = None
-    comment = "fix: show only user locations in view & fix role checking"
+    comment = "add: add locations from db"
     if same_comment:
         if not comment:
             comment = input("comment for all commits will be same:")
