eb0810d04684c4462f7ece168c2572ab91cabf12
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-27 00:25:29 +0200

    backend api role_action_validation role_validator.py - wip: refactor role validation

--- backend/api/role_action_validation/role_validator.py
+++ backend/api/role_action_validation/role_validator.py
@@ -7,6 +7,7 @@ class SchemeValidator:
         self.scheme = role_validation_cfg
         self.serialization_connector = ";"
 
+    # pass url and action, remove url from action
     def check_action(self, role, action):
         # print(f"check action {role=} {action=}")
         if isinstance(action, str):
@@ -43,6 +44,7 @@ class SchemeValidator:
 
         return r
 
+    # todo remove
     def deserialize(self, payload):
 
         splitters = [self.serialization_connector, " "]
@@ -60,46 +62,48 @@ class SchemeValidator:
                     payload.split(" ", 1) if i.strip()]
 
 #
-# def main():
-#     scheme_validator = SchemeValidator()
-#
-#     for role_k, role_v in {"aggregator": True, "role_false": False}.items():
-#
-#         for table_k, table_v in {"locations ": True,
-#                                  "table_false": False}.items():
-#
-#             for action_k, action_v in {"create single": True,
-#                                        "action false": False}.items():
-#
-#                 for strip_test in ["", " "]:
-#                     for connector in [";", " "]:
-#
-#                         action_composite = table_k + connector + strip_test + action_k
-#
-#                         for deserialize_option in [True, False]:
-#                             if deserialize_option:
-#                                 action_composite = scheme_validator.deserialize(
-#                                     action_composite)
-#
-#                             print(
-#                                 f"pass {role_k:15} {str(action_composite):15}")
-#                             print(
-#                                 f"{role_v:10} {table_v:15} {action_v:15} {deserialize_option:15}")
-#
-#                             r = scheme_validator.check_action(role_k,
-#                                                               action_composite)
-#
-#                             if role_v and table_v and action_v:
-#                                 print(f"expecting true, got {r}")
-#                                 if not r:
-#                                     raise Exception("err")
-#                             else:
-#                                 print(f"expecting false, got {r}")
-#                                 if r:
-#                                     raise Exception("err")
-#                             print()
-#     print("test ok")
-#
-#
-# if __name__ == '__main__':
-#     main()
+def main():
+    from backend.api.comm.json_loader import role_validation_cfg
+
+    scheme_validator = SchemeValidator(role_validation_cfg)
+
+    for role_k, role_v in {"aggregator": True, "role_false": False}.items():
+
+        for table_k, table_v in {"locations ": True,
+                                 "table_false": False}.items():
+
+            for action_k, action_v in {"create single": True,
+                                       "action false": False}.items():
+
+                for strip_test in ["", " "]:
+                    for connector in [";", " "]:
+
+                        action_composite = table_k + connector + strip_test + action_k
+
+                        for deserialize_option in [True, False]:
+                            if deserialize_option:
+                                action_composite = scheme_validator.deserialize(
+                                    action_composite)
+
+                            print(
+                                f"pass {role_k:15} {str(action_composite):15}")
+                            print(
+                                f"{role_v:10} {table_v:15} {action_v:15} {deserialize_option:15}")
+
+                            r = scheme_validator.check_action(role_k,
+                                                              action_composite)
+
+                            if role_v and table_v and action_v:
+                                print(f"expecting true, got {r}")
+                                if not r:
+                                    raise Exception("err")
+                            else:
+                                print(f"expecting false, got {r}")
+                                if r:
+                                    raise Exception("err")
+                            print()
+    print("test ok")
+
+
+if __name__ == '__main__':
+    main()
