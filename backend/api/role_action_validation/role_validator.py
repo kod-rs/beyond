0117f89fb4a56ed5d643a23e1e8c6b4925eea826
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-27 09:54:37 +0200

    backend api role_action_validation role_validator.py - fix: role validation deserializer

--- backend/api/role_action_validation/role_validator.py
+++ backend/api/role_action_validation/role_validator.py
@@ -47,19 +47,22 @@ class SchemeValidator:
     # todo remove
     def deserialize(self, payload):
 
-        splitters = [self.serialization_connector, " "]
+        splitters = [self.serialization_connector, " ", ":"]
 
         if not any([payload.__contains__(i) for i in splitters]):
             """assumption: already deserialized"""
             return [payload]
 
-        if self.serialization_connector in payload:
-            return [i.strip() for i in
-                    payload.split(self.serialization_connector)]
+        # if self.serialization_connector in payload:
+        for j in splitters:
+            if payload.__contains__(j):
+                return [i.strip() for i in
+                        payload.split(j, 1)]
 
-        else:
-            return [i.strip() for i in
-                    payload.split(" ", 1) if i.strip()]
+        # else:
+        #     print("3")
+        #     return [i.strip() for i in
+        #             payload.split(" ", 1) if i.strip()]
 
 #
 def main():
@@ -67,41 +70,8 @@ def main():
 
     scheme_validator = SchemeValidator(role_validation_cfg)
 
-    for role_k, role_v in {"aggregator": True, "role_false": False}.items():
 
-        for table_k, table_v in {"locations ": True,
-                                 "table_false": False}.items():
 
-            for action_k, action_v in {"create single": True,
-                                       "action false": False}.items():
-
-                for strip_test in ["", " "]:
-                    for connector in [";", " "]:
-
-                        action_composite = table_k + connector + strip_test + action_k
-
-                        for deserialize_option in [True, False]:
-                            if deserialize_option:
-                                action_composite = scheme_validator.deserialize(
-                                    action_composite)
-
-                            print(
-                                f"pass {role_k:15} {str(action_composite):15}")
-                            print(
-                                f"{role_v:10} {table_v:15} {action_v:15} {deserialize_option:15}")
-
-                            r = scheme_validator.check_action(role_k,
-                                                              action_composite)
-
-                            if role_v and table_v and action_v:
-                                print(f"expecting true, got {r}")
-                                if not r:
-                                    raise Exception("err")
-                            else:
-                                print(f"expecting false, got {r}")
-                                if r:
-                                    raise Exception("err")
-                            print()
     print("test ok")
 
 
