d8c0c1b9bc350d9e434df975a9e1042a92f2562d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-28 12:25:46 +0200

    src views login ForgotPassword.vue - ref: public sites

---
+++ src/views/login/ForgotPassword.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <PublicBase>
+        <ForgotPasswordForm></ForgotPasswordForm>
+        <Links></Links>
+    </PublicBase>
+
+</template>
+
+
+<script>
+import PublicBase from "../../components/login/PublicBase.vue";
+import ForgotPasswordForm from "@/components/login/ForgotPasswordForm.vue";
+import Links from "@/components/login/Links.vue";
+
+export default {
+    data() {
+        return {};
+    },
+    components: { PublicBase, ForgotPasswordForm, Links }
+};
+</script> 
