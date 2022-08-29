0074a496e5c3920504c835dc78594029ae3ec32b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-29 15:31:59 +0200

    src scripts store.js - init: portfolio master

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -2,6 +2,13 @@ import { createStore } from 'vuex'
 
 // state : development / production
 
+function createEmptyPortfolio() {
+    return {
+        id: -1,
+        name: ""
+    };
+}
+
 export const store = createStore({
     state() {
         return {
@@ -10,7 +17,27 @@ export const store = createStore({
             latitude: 0,
             longitude: 0,
             username: "",
-            appMode: "development"
+            appMode: "development",
+
+            portfolios: [],
+            selectedPortfolio: createEmptyPortfolio(),
+            nextId: 1
+
+        }
+    },
+    actions: {
+        addPortfolio(context, portfolio) {
+            context.commit("ADD_PORTFOLIO", portfolio);
+        },
+        selectPortfolio(context, portfolio) {
+            context.commit("SELECT_PORTFOLIO", portfolio);
+        },
+        deletePortfolio(context, id) {
+            context.commit("DELETE_PORTFOLIO", id);
+
+        },
+        updatePortfolio(context, portfolio) {
+            context.commit("UPDATE_PORTFOLIO", portfolio);
         }
     },
     mutations: {
@@ -28,8 +55,39 @@ export const store = createStore({
         },
         setUsername(state, username) {
             state.username = username
+        },
+
+        ADD_PORTFOLIO(state, portfolio) {
+            portfolio.id = state.nextId++;
+            state.portfolios.push(portfolio);
+            state.selectedPortfolio = portfolio;
+        },
+        SELECT_PORTFOLIO(state, portfolio) {
+            state.selectedPortfolio = portfolio;
+        },
+        DELETE_PORTFOLIO(state, id) {
+            if (state.selectedPortfolio.id == id) {
+                state.selectedPortfolio = createEmptyPortfolio();
+            }
+            state.portfolios = state.portfolios.filter(p => p.id != id);
+        },
+        UPDATE_PORTFOLIO(state, portfolio) {
+            var store_p = state.portfolios.find(p => p.id == portfolio.id);
+            if (store_p != null) {
+                store_p.name = portfolio.name;
+            }
         }
 
+    },
+    getters: {
+        portfolios(state) {
+            return state.portfolios;
+        },
+        selectedPortfolio(state) {
+            return state.selectedPortfolio;
+        }
     }
+
 })
+
 
