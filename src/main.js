344b7f6378e3a329918dfd63861e1c72f02a83cc
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-30 20:28:11 +0200

    src main.js - wip: master detail portfolio

--- src/main.js
+++ src/main.js
@@ -1,34 +1,13 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-
-// createApp(App).mount('#app')
-
-
 import { createApp } from 'vue';
 import App from './App.vue'
-const app = createApp(App);
-
-
 import { router } from './scripts/router';
 import { store } from "./scripts/store"
-
-// import MapComponent from './components/map/MapComponent.vue'
-
 import OpenLayersMap from 'vue3-openlayers'
 import 'vue3-openlayers/dist/vue3-openlayers.css'
-const sts = require('strict-transport-security');
 
+const app = createApp(App);
+const sts = require('strict-transport-security');
 const globalSTS = sts.getSTS({ 'max-age': { 'days': 10 }, 'includeSubDomains': true });
 
-app.use(router).use(store).mount('#app')
-
-app
-    .use(OpenLayersMap, globalSTS)
-// .component('MapComponent', MapComponent)
-
-console.log("src index")
-
-
-/////////////////////////
-// development entry point
-/////////////////////////
+app.use(router).use(store).mount('#app');
+app.use(OpenLayersMap, globalSTS);
