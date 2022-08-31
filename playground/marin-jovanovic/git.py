e3cd854186d71f303737388c7a9586c424631da2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-31 23:45:28 +0200

    playground marin-jovanovic git.py - wip: rewrite api calls for locations

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -16,7 +16,7 @@ def main():
     comment = "init: screens"
     comment = 'ref: rewrite auth using backend'
 
-    comment = "init: basic master detail & fetch portfolios"
+    comment = "wip: rewrite api calls for locations"
 
     if same_comment:
         if not comment:
