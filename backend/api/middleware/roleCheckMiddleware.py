cbfbae41c50c3529fe75b09713bea343e26ceea8
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-08-31 22:45:44 +0200

    backend api middleware roleCheckMiddleware.py - init: basic master detail & fetch portfolios

--- backend/api/middleware/roleCheckMiddleware.py
+++ backend/api/middleware/roleCheckMiddleware.py
@@ -14,20 +14,20 @@ class RoleCheckMiddleware:
     def __call__(self, request):
         print("RoleCheckMiddleware")
 
-        access_token = request.access_token
+        # access_token = request.access_token
 
-        if not hasattr(request, "action"):
-            print("internal server error, check action middleware, "
-                  "he is not adding empty action")
-            raise Exception(INTERNAL_SERVER_ERROR_MESSAGE)
-
-        if request.action == MIDDLEWARE_NO_ACTION:
-            print("action is empty")
-            request.action_checked = MIDDLEWARE_NO_ACTION
-        else:
-            roles = get_roles(access_token)
-            action = request.action
-            print(f"{roles=} {action=} {request}")
+        # if not hasattr(request, "action"):
+        #     print("internal server error, check action middleware, "
+        #           "he is not adding empty action")
+        #     raise Exception(INTERNAL_SERVER_ERROR_MESSAGE)
+        #
+        # if request.action == MIDDLEWARE_NO_ACTION:
+        #     print("action is empty")
+        #     request.action_checked = MIDDLEWARE_NO_ACTION
+        # else:
+            # roles = get_roles(access_token)
+            # action = request.action
+            # print(f"{roles=} {action=} {request}")
 
             #     # print(request.META)
             #
@@ -57,9 +57,9 @@ class RoleCheckMiddleware:
             # path = request.META.get("PATH_INFO", default=None)
             # print(f"{method=} {path=}")
 
-            startup_configuration.get_scheme_validator().check_action(roles, action)
-            request.action_checked = MIDDLEWARE_NO_ACTION
+            # startup_configuration.get_scheme_validator().check_action(roles, action)
+            # request.action_checked = MIDDLEWARE_NO_ACTION
 
-        request.roles = get_roles(access_token)
+        # request.roles = get_roles(access_token)
 
         return self.get_response(request)
