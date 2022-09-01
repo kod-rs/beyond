2dfd883e97e2625232efeb0cf8cc7b083291e7cf
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-01 16:10:34 +0200

    src scripts api.js - impl: api colours

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -39,39 +39,6 @@ async function deleteLocation(i) {
 
 }
 
-// async function getLocationsFilterUsername() {
-
-//     const r = checkTokens()
-
-//     if (r["status"]) {
-
-//         let access_token = r["access_token"]
-//         let refresh_token = r["refresh_token"]
-
-//         const response = await api.post(`locations/`, JSON.stringify({ access_token, refresh_token, action: "locations;select username" }));
-//         return await handleNewResponse(response);
-
-//     }
-
-// }
-
-
-// async function getAllLocations() {
-
-//     const r = checkTokens()
-
-//     if (r["status"]) {
-
-//         let access_token = r["access_token"]
-//         let refresh_token = r["refresh_token"]
-
-//         const response = await api.post(`locations/`, JSON.stringify({ access_token, refresh_token, action: "locations;select_all" }));
-//         return await handleNewResponse(response);
-
-//     }
-
-// }
-
 
 async function addLocation(section, type, latitude, longitude, csrfToken) {
 
@@ -141,7 +108,6 @@ function logout() {
 
 function checkTokens() {
 
-
     if (sessionStorage.getItem("user") !== null) {
         let user = JSON.parse(window.sessionStorage.getItem('user'));
 
@@ -170,6 +136,40 @@ function checkTokens() {
 
 }
 
+async function getCoordinates() {
+
+    let response = await api.get(`https://ipapi.co/json/`)["data"];
+
+    return {
+        latitude: response["latitude"],
+        longitude: response["longitude"]
+    }
+
+}
+
+
+async function getLocationsFilterUsername() {
+
+    return await handleNewResponse(
+        await api.get(
+            "locations/",
+            get_auth_header()
+        )
+    );
+
+}
+
+async function getPortoflios() {
+
+    return await handleNewResponse(
+        await api.get(
+            "portfolio/",
+            get_auth_header()
+        )
+    );
+
+}
+
 function handleNewResponse(response) {
 
     if (!response.data.auth.status) {
@@ -185,157 +185,13 @@ function handleNewResponse(response) {
     return response.data
 }
 
-async function getCoordinates() {
 
-    let response = await api.get(`https://ipapi.co/json/`)
-    response = response["data"]
-    return {
-        latitude: response["latitude"],
-        longitude: response["longitude"]
-    }
-
-}
-
-async function makeBackendRequest({ method, url, action, params }) {
-    console.log("make backend req")
-    const r = checkTokens()
-
-    console.log("params", params)
-    console.log("action", action)
-
-    if (r["status"]) {
-
-        let access_token = r["access_token"];
-        let refresh_token = r["refresh_token"];
-
-        if (method === "get") {
-
-            return await handleNewResponse(
-                await api.get(
-                    url,
-                    JSON.stringify({
-                        access_token,
-                        refresh_token,
-                        action
-                    })
-                )
-            );
-
-        } else if (method === "post") {
-            return await handleNewResponse(
-                await api.post(
-                    url,
-                    JSON.stringify({
-                        access_token,
-                        refresh_token,
-                        action
-                    })
-                )
-            );
-
-        }
-
-    }
-
-
-}
-
-async function getLocationsFilterUsername() {
-
-    let headers = get_auth_header();
-
-    // not working, todo
-    headers["body"] = { "action": "ffffffffffff" };
-    // console.log(headers)
-    return await handleNewResponse(
-        await api.get(
-            "locations/",
-            // {},
-            headers
-        )
-    );
-
-
-    // return await handleNewResponse(
-    //     await api.get(
-    //         "locations/",
-    //         get_auth_header()
-    //     )
-    // );
-
-}
-
-async function getPortoflios() {
-    let headers = get_auth_header();
-
-    // not working, todo
-    headers["body"] = { "action": "ffffffffffff" };
-    // console.log(headers)
-    return await handleNewResponse(
-        await api.get(
-            "portfolio/",
-            headers
-        )
-    );
-
-    // return await api.get(
-    //     "portfolio/",
-    //     { action: "tmp" },
-    //     get_auth_header()
-    // );
-
-
-    // const response = await api.post(`locations/`, JSON.stringify({ access_token, refresh_token, action: "locations;delete single", index: i }));
-    // return await handleNewResponse(response);
-
-}
-
-async function testcall() {
-
-    const funRes = {
-        "status": false,
-        "synchronizer_token": undefined
-    }
-
-    let access_token = "access_token";
-    let refresh_token = "refresh_token";
-
-    let username = "a";
-    let password = "a";
-
-    const response = await api.post(
-        "csrf/",
-
-        JSON.stringify({
-            access_token,
-            refresh_token,
-            action: "csrf;select synchronizer_token"
-        })
-        , {
-            headers: {
-                'Authorization': 'Basic ' + ((encodeURIComponent(username + ':' + password)))
-            },
-        }
-
-    );
-    const responseData = await handleNewResponse(response)["payload"];
-
-    funRes["synchronizer_token"] = responseData["synchronizer_token"];
-    funRes["status"] = true;
-
-    return funRes;
-}
 
 
 export const apiCalls = {
     getPortoflios,
-    makeBackendRequest,
-    testcall,
-    // makeBackendRequest,
     logout,
     login,
-
-    // getAllLocations,
     deleteLocation,
     getCSRFAuthData,
     addLocation,
