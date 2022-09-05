e7ed2846a9a2278bc7c47c000074b54d6595599a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 18:50:46 +0200

    backend api cqrs_c portfolio.py - impl: create / update portfolio

---
+++ backend/api/cqrs_c/portfolio.py
@@ -0,0 +1,17 @@
+from backend.api.model.portfolio import Portfolio
+
+
+
+
+def create_or_update(username, current_name, new_name, colour):
+
+    print("create_or_update")
+
+    print(f"if have {username=} {current_name=}")
+    print(f"update this {new_name=} {colour=}")
+    i, _ = Portfolio.objects.update_or_create(
+        username=username, name=current_name,
+        defaults={"name": new_name, "colour": colour}
+    )
+    print(i)
+    i.save()
