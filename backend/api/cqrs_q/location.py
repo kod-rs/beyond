8b8be26bc0cc6fcf159d957865f4c9935888577d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 15:06:56 +0200

    backend api cqrs_q location.py - ref: rewrite db scheme

--- backend/api/cqrs_q/location.py
+++ backend/api/cqrs_q/location.py
@@ -6,33 +6,45 @@ from django.core import serializers
 from backend.api.model.location import Location
 
 
-def get_all():
-    t = serializers.serialize('json', Location.objects.all())
-    t = json.loads(t)
-    # print(t)
-
-    n = []
-    for i in t:
-        f = {}
-        p = i["pk"]
-        f["pk"] = p
-        f.update(i["fields"])
-        n.append(f)
-
-    t = n
-
-    try:
-        t = [{k: ast.literal_eval(v)[0] for k, v in i.items()} for i in t]
-    except ValueError:
-        try:
-            t = [{k: ast.literal_eval(v) for k, v in i.items()} for i in t]
-        except ValueError:
-            t = [{k: v for k, v in i.items()} for i in t]
-
-    return t
+# def get_all():
+#     t = serializers.serialize('json', Location.objects.all())
+#     t = json.loads(t)
+#     # print(t)
+#
+#     n = []
+#     for i in t:
+#         f = {}
+#         p = i["pk"]
+#         f["pk"] = p
+#         f.update(i["fields"])
+#         n.append(f)
+#
+#     t = n
+#
+#     try:
+#         t = [{k: ast.literal_eval(v)[0] for k, v in i.items()} for i in t]
+#     except ValueError:
+#         try:
+#             t = [{k: ast.literal_eval(v) for k, v in i.items()} for i in t]
+#         except ValueError:
+#             t = [{k: v for k, v in i.items()} for i in t]
+#
+#     return t
 
 def get_all_by_username(username):
-    t = serializers.serialize('json', Location.objects.filter(username=username))
+
+    try:
+
+        q = Location.objects.filter(portfolio__username=username)
+
+    except Location.DoesNotExist:
+        return False
+
+
+    print("q--------------------------")
+    print(f"{q=}")
+
+    t = serializers.serialize('json', q)
     t = json.loads(t)
     print(t)
 
@@ -42,7 +54,7 @@ def get_all_by_username(username):
         p = i["pk"]
         f["pk"] = p
         f.update(i["fields"])
-        del f["username"]
+        # del f["username"]
         n.append(f)
 
     t = n
