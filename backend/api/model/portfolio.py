ab167108b78f0f2a1bc5c08b1e6a227aee350d98
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 21:39:27 +0200

    backend api model portfolio.py - fix: add & save button edge cases

--- backend/api/model/portfolio.py
+++ backend/api/model/portfolio.py
@@ -1,9 +1,34 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+
 from backend.api.model.colour import Colour
+
+'''
+modify constraints
+
+sudo su postgres
+psql
+
+\c beyond
+\d api_portfolio
+\
+alter table api_portfolio drop constraint api_portfolio_pkey CASCADE;
+alter table api_portfolio add constraint table_pkey primary key (username, name);
+
+'''
 
 
 class Portfolio(models.Model):
-    name = models.CharField(max_length=200, unique=False)
-    username = models.CharField(max_length=200, unique=False)
+
+    # UniqueConstraint(fields=['username', 'name'], name='constraint_name')
+
+    username = models.CharField(max_length=200,unique=False)
+    name = models.CharField(max_length=200,unique=False)
     # colour = models.ForeignKey(Colour, on_delete=models.CASCADE)
-    colour = models.CharField(max_length=200, unique=False)
+    colour = models.CharField(max_length=200)
+
+    # class Meta:
+    #     # managed = True
+    #     db_table="portfolio"
+    #     unique_together = (('username',
+    #                         'name'),)
