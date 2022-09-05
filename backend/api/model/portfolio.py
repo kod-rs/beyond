41afd9f8b2cacbd8219edb6d096ddea7400014d1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 15:06:56 +0200

    backend api model portfolio.py - ref: rewrite db scheme

--- backend/api/model/portfolio.py
+++ backend/api/model/portfolio.py
@@ -1,6 +1,9 @@
 from django.db import models
+from backend.api.model.colour import Colour
 
 
 class Portfolio(models.Model):
     name = models.CharField(max_length=200, unique=False)
-    user_username = models.CharField(max_length=200, unique=False)
+    username = models.CharField(max_length=200, unique=False)
+    # colour = models.ForeignKey(Colour, on_delete=models.CASCADE)
+    colour = models.CharField(max_length=200, unique=False)
