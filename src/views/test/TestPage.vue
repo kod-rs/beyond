5f803fce0bf46cc00d454efd21ab27d11b3e1674
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 18:50:46 +0200

    src views test TestPage.vue - impl: create / update portfolio

--- src/views/test/TestPage.vue
+++ src/views/test/TestPage.vue
@@ -1,50 +1,11 @@
 <template>
 
-
-    <div id="app" class="container mt-2">
-
-        <!-- <SavedModal v-show="showModal" @close-modal="showModal = false" /> -->
+    <div class="container mt-2">
         <hr>
         <portfolio-list />
         <hr>
         <portfolio-details />
     </div>
-
-
-
-    <!-- <div v-if="userType === 'building_manager'">
-        building_manager
-        <br>
-        <hr>
-        portfolio, click to edit
-
-        <div class="field">
-            <span class="field-value" v-show="!showField('name')" @click="focusField('name')">{{ name }}</span>
-            <input v-model="name" v-show="showField('name')" id="user-name" type="text" class="field-value form-control"
-                @focus="focusField('name')" @blur="blurField">
-        </div>
-
-
-    </div>
-    <div v-else-if="userType === 'aggregator'">
-        aggregator
-        <br>
-        <hr>
-        <button>
-            create new portfolio
-
-        </button>
-    </div>
-    <div v-else-if="userType === 'aggregator_and_building_manager'">
-        aggregator and building_manager
-        <br>
-        <hr>
-        <button>
-            create new portfolio
-
-        </button>
-    </div> -->
-
 
 </template>
 
@@ -53,25 +14,23 @@
 
 import PortfolioList from "./PortfolioList.vue";
 import PortfolioDetails from "./PortfolioDetails.vue";
-import "bootstrap/dist/css/bootstrap.css";
-import store from '../../scripts/store';
+// import "bootstrap/dist/css/bootstrap.css";
+// import store from '../../scripts/store';
 
 export default {
     name: "TestPag",
-    store,
+    // store,
     data() {
         return {
-            // showModal: true,
         }
     },
     components: {
-        // "SavedModal": SavedModal,
         "portfolio-list": PortfolioList,
         "portfolio-details": PortfolioDetails
-    },
-    async mounted() {
-
     }
+    // ,
+    // async mounted() {
+    // }
 
 };
 
