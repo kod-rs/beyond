e34bf7389e9ace3e7956d4f3bd57f8f4602ec865
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 15:06:56 +0200

    backend api cqrs_q portfolio.py - ref: rewrite db scheme

---
+++ backend/api/cqrs_q/portfolio.py
@@ -0,0 +1,38 @@
+from backend.api.model.portfolio import Portfolio
+from backend.api.model.location import Location
+from django.core import serializers
+
+def get_portfolios(username):
+
+    try:
+
+        ip_obj = Portfolio.objects.filter(username=username)
+        print(f"{ip_obj=}")
+        # t = serializers.serialize('json', ip_obj)
+        # [print(i) for i in t]
+        # print(t)
+        return ip_obj
+
+    except Portfolio.DoesNotExist:
+        return False
+
+
+def get_portfolios_with_locations(username):
+
+    try:
+
+        # portfolios = Portfolio.objects.filter(username=username)
+
+        ip_obj = Location.objects.filter(portfolio__username=username)
+        print(f"{ip_obj=}")
+
+        t = serializers.serialize('json', ip_obj)
+        print(t)
+        print("---")
+        # for i in t:
+        #     print(i)
+        return ip_obj
+
+    except Portfolio.DoesNotExist:
+        return False
+
