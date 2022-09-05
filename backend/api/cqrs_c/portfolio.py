243c34eb25ad0df5f2c6a4fae15adfe40b848e04
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 23:44:40 +0200

    backend api cqrs_c portfolio.py - wip: create location; portfolio selection

--- backend/api/cqrs_c/portfolio.py
+++ backend/api/cqrs_c/portfolio.py
@@ -1,3 +1,5 @@
+from django.db import utils
+
 from backend.api.model.portfolio import Portfolio
 
 
@@ -7,9 +9,19 @@ def create_or_update(username, current_name, new_name, colour):
 
     print(f"if have {username=} {current_name=}")
     print(f"update this {new_name=} {colour=}")
-    i, _ = Portfolio.objects.update_or_create(
-        username=username, name=current_name,
-        defaults={"name": new_name, "colour": colour}
-    )
-    print(i)
-    i.save()
+    try:
+        i, _ = Portfolio.objects.update_or_create(
+            username=username, name=current_name,
+            defaults={"name": new_name, "colour": colour}
+        )
+        print(i)
+        i.save()
+
+        return True
+    except utils.IntegrityError:
+        print("integrity err")
+        return False
+
+
+def delete_portfolio(username, portfolio_name):
+    Portfolio.objects.filter(username=username, name=portfolio_name).delete()
