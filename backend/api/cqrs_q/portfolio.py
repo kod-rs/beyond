d0c911602b6f13e1c10ae182045289c53c254e9a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-05 23:44:40 +0200

    backend api cqrs_q portfolio.py - wip: create location; portfolio selection

--- backend/api/cqrs_q/portfolio.py
+++ backend/api/cqrs_q/portfolio.py
@@ -3,20 +3,10 @@ from backend.api.model.location import Location
 from django.core import serializers
 
 
-def delete_portfolio(username, portfolio_name):
-    Portfolio.objects.filter(username=username, name=portfolio_name).delete()
-
-
 def get_portfolios(username):
 
     try:
-
-        ip_obj = Portfolio.objects.filter(username=username)
-        print(f"{ip_obj=}")
-        # t = serializers.serialize('json', ip_obj)
-        # [print(i) for i in t]
-        # print(t)
-        return ip_obj
+        return Portfolio.objects.filter(username=username)
 
     except Portfolio.DoesNotExist:
         return False
@@ -25,8 +15,6 @@ def get_portfolios(username):
 def get_portfolios_with_locations(username):
 
     try:
-
-        # portfolios = Portfolio.objects.filter(username=username)
 
         ip_obj = Location.objects.filter(portfolio__username=username)
         print(f"{ip_obj=}")
@@ -41,3 +29,10 @@ def get_portfolios_with_locations(username):
     except Portfolio.DoesNotExist:
         return False
 
+def get_portfolio_names(username):
+
+    try:
+        return Portfolio.objects.filter(username=username).only("name")
+
+    except Portfolio.DoesNotExist:
+        return False
