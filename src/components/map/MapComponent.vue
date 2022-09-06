85b2589e7ae691bb874d2b55ed30f7d87458d163
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-07 00:08:34 +0200

    src components map MapComponent.vue - err: map marker zoom

--- src/components/map/MapComponent.vue
+++ src/components/map/MapComponent.vue
@@ -5,11 +5,10 @@
 <script>
 
 import Map from 'ol/Map';
-import OSM from 'ol/source/OSM';
-import TileLayer from 'ol/layer/Tile';
+// import OSM from 'ol/source/OSM';
+// import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 export default {
-    // name: "MapComponent",
     data() {
         return {
             map: undefined,
@@ -17,25 +16,22 @@ export default {
         }
     },
     methods: {
-        initMap() {
-            this.view = new View({
-                center: [0, 0],
-                zoom: 2,
-            });
-
-            this.map = new Map({
-                layers: [
-                    new TileLayer({
-                        source: new OSM(),
-                    }),
-                ],
-                target: 'map',
-                view: this.view,
-            });
-        },
     },
-    async mounted() {
-        this.initMap();
+    mounted() {
+        this.view = new View({
+            center: [0, 0],
+            zoom: 2,
+        });
+
+        this.map = new Map({
+            // layers: [
+            //     new TileLayer({
+            //         source: new OSM(),
+            //     }),
+            // ],
+            target: 'map',
+            view: this.view,
+        });
     }
 
 }
