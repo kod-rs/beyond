e5caa194990bf567bae07cc44a1402f153431687
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-06 21:36:45 +0200

    src views login LoginPage.vue - upd: cleanup

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -12,15 +12,11 @@
 import LoginForm from "../../components/login/LoginForm.vue";
 import PublicBase from "../../components/login/PublicBase.vue";
 import Links from "@/components/login/Links.vue";
-import { getMixedColors } from "../../scripts/common";
 
 export default {
     data() {
         return {};
     },
-    components: { LoginForm, PublicBase, Links },
-    mounted() {
-        console.log(getMixedColors(2))
-    }
+    components: { LoginForm, PublicBase, Links }
 };
 </script> 
