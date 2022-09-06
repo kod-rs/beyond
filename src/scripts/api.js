081992d91fb564f5a00f4bcac87ed221618467fb
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-06 12:04:52 +0200

    src scripts api.js - fix: show markers diff colours on map

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -23,40 +23,6 @@ async function getCSRFAuthData() {
 
 }
 
-async function deleteLocation(i) {
-
-    const r = checkTokens()
-
-    if (r["status"]) {
-
-        let access_token = r["access_token"]
-        let refresh_token = r["refresh_token"]
-
-        const response = await api.post(`locations/`, JSON.stringify({ access_token, refresh_token, action: "locations;delete single", index: i }));
-        return await handleNewResponse(response);
-
-    }
-
-}
-
-
-async function addLocation(portfolio, section, type, latitude, longitude, csrfToken) {
-
-    return await handleNewResponse(
-        await api.post(
-            "locations/",
-            JSON.stringify({
-                portfolio,
-                type, section, latitude, longitude, synchronizer_token: csrfToken
-            }),
-            get_auth_header()
-        )
-    );
-
-
-}
-
-
 async function login(username, password) {
 
     const response = await api.post(
@@ -106,36 +72,6 @@ function logout() {
 
 }
 
-function checkTokens() {
-
-    if (sessionStorage.getItem("user") !== null) {
-        let user = JSON.parse(window.sessionStorage.getItem('user'));
-
-        if (
-            ("auth" in user) &&
-            ("access-token" in user["auth"]) &&
-            ("refresh-token" in user["auth"])
-        ) {
-
-            const access_token = user["auth"]["access-token"]
-            const refresh_token = user["auth"]["refresh-token"]
-
-            return {
-                "status": true,
-                "access_token": access_token,
-                "refresh_token": refresh_token
-            }
-
-        }
-
-    }
-
-    return {
-        "status": false
-    }
-
-}
-
 async function getCoordinates() {
 
     let response = await api.get(`https://ipapi.co/json/`)["data"];
@@ -144,18 +80,6 @@ async function getCoordinates() {
         latitude: response["latitude"],
         longitude: response["longitude"]
     }
-
-}
-
-
-async function getLocationsFilterUsername() {
-
-    return await handleNewResponse(
-        await api.get(
-            "locations/",
-            get_auth_header()
-        )
-    );
 
 }
 
@@ -216,14 +140,15 @@ async function deletePortfolio(currentName,) {
 
 
 export const apiCalls = {
+    api,
+    handleNewResponse,
+    get_auth_header,
+
     deletePortfolio,
     createOrUpdatePortfolio,
     getPortoflios,
     logout,
     login,
-    deleteLocation,
     getCSRFAuthData,
-    addLocation,
     getCoordinates,
-    getLocationsFilterUsername
 }
