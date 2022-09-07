464907efcd8289289e0e57bd3f63a428c9455596
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-07 15:05:53 +0200

    src views location AddLocation.vue - upd: clear instead of refresh location creation

--- src/views/location/AddLocation.vue
+++ src/views/location/AddLocation.vue
@@ -4,16 +4,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col">
-                <!-- add new -->
                 <br>
-                <!-- <hr> -->
                 <div v-if="error" class="alert alert-danger">{{ error }}</div>
-
 
                 <form @submit.prevent="handleSubmit">
 
                     <h5>csrf</h5>
-                    <CSRFToken />
+                    <CSRFToken ref="csrf" />
 
                     <h5>input part</h5>
                     <InputFieldsForm ref="inputFields"></InputFieldsForm>
@@ -38,7 +35,7 @@
                 </form>
             </div>
             <div class="col">
-                <div id="map" class="map"></div>
+                <MapComponent ref="map" id="map"></MapComponent>
                 <MapPopup ref="mappopup"></MapPopup>
 
             </div>
@@ -56,12 +53,6 @@
 import CSRFToken from "../../components/form/CSRFToken.vue"
 import MapPopup from "../../components/map/MapPopup.vue"
 import { apiLocation } from '../../scripts/api/location';
-import Map from 'ol/Map';
-
-import TileLayer from 'ol/layer/Tile';
-import View from 'ol/View';
-
-import OSM from 'ol/source/OSM';
 import { toLonLat } from 'ol/proj';
 import { toStringHDMS } from 'ol/coordinate';
 
@@ -69,58 +60,57 @@ import { toStringHDMS } from 'ol/coordinate';
 import PortfolioSelector from '../../components/form/PortfolioSelector.vue';
 import LoadingComponent from "@/components/misc/LoadingComponent.vue";
 import InputFieldsForm from "@/components/misc/InputFieldsForm.vue";
+import MapComponent from "@/components/map/MapComponent.vue";
 
 
 export default {
 
     data() {
         return {
-            checkedNames: [],
-            hover: false,
-
             latitude: undefined,
             longitude: undefined,
 
             submitted: false,
             loading: false,
             error: undefined,
+
+            map: undefined,
+            view: undefined
         };
-    },
-    watch: {
-        // whenever question changes, this function will run
-        // question(newQuestion, oldQuestion) {
-        //     if (newQuestion.includes('?')) {
-        //         this.getAnswer()
-        //     }
-        // }
     },
     methods: {
 
 
         async handleSubmit() {
-            console.log("submit for p",)
-            // return
+            this.$refs.inputFields.fields["Section"]["reset"] = false;
+            this.$refs.inputFields.fields["Type"]["reset"] = false;
+
             this.submitted = true;
             this.error = "";
-            // let formContent = {};
-            // for (const element of this.formData) {
-            //     if (element.value) {
-            //         formContent[element.key] = element.value;
-            //     }
-            //     else {
-            //         this.error = "fields not filled";
-            //         return;
-            //     }
-            // }
-            const csrfToken = this.$store.state.synchronizerToken;
 
             let section = this.$refs.inputFields.fields["Section"]["value"];
-            let type = this.$refs.inputFields.fields["Section"]["value"];
+            let type = this.$refs.inputFields.fields["Type"]["value"];
 
-            // check if portfolio input has content and is valid
+            if (!(section && type)) {
+                this.error += "fill all fields"
+            }
+            if (!(this.latitude && this.longitude)) {
+                this.error += "\nselect location"
+            }
+
+            // todo check portfolio
+            const csrfToken = this.$store.state.synchronizerToken;
+
+            if (!csrfToken) {
+                alert("error with auth, reloading");
+                this.$router.go();
+            }
 
             let portfolio = this.$refs.portfolioSelector.getSearch();
-            console.log("port", portfolio);
+
+            if (this.error) {
+                return
+            }
 
             await apiLocation.addLocation(
                 portfolio,
@@ -128,7 +118,6 @@ export default {
                 type,
                 this.latitude,
                 this.longitude,
-
                 csrfToken
 
             ).then(r => {
@@ -143,70 +132,45 @@ export default {
                 console.log("error", error);
             });
 
-            // restart fields insted of refresh site
+            // this.$router.go();
 
-            this.$router.go();
+            // restart
+            this.$refs.inputFields.fields["Section"]["value"] = "";
+            this.$refs.inputFields.fields["Type"]["value"] = "";
+
+            this.$refs.inputFields.fields["Section"]["reset"] = true;
+            this.$refs.inputFields.fields["Type"]["reset"] = true;
+
+            await this.$refs.csrf.refresh();
+
+            this.$refs.mappopup.closePopup();
+            this.longitude = "";
+            this.latitude = "";
+
             this.submitted = false;
         }
     },
     async mounted() {
+        this.map = this.$refs.map.map;
+        this.view = this.$refs.map.view;
 
 
+        this.map.addOverlay(this.$refs.mappopup.getOverlay());
 
-        /**
-      * Create the map.
-      *
-     */
-        const map = new Map({
-            layers: [
-                new TileLayer({
-                    source: new OSM(),
-                }),
-            ],
-            target: "map",
-            view: new View({
-                center: [0, 0],
-                zoom: 2,
-            }),
-        });
-        map.addOverlay(this.$refs.mappopup.getOverlay());
         /**
          * Add a click handler to the map to render the popup.
          */
-        map.on("singleclick", (evt) => {
+        this.map.on("singleclick", (evt) => {
             const coordinate = evt.coordinate;
             const hdms = toStringHDMS(toLonLat(coordinate));
 
             this.longitude = toLonLat(coordinate)[0];
             this.latitude = toLonLat(coordinate)[1];
 
-            // console.log("log coord", hdms);
-            // console.log("set postiion,", coordinate);
             this.$refs.mappopup.setText(hdms);
             this.$refs.mappopup.setPosition(coordinate);
         });
     },
-    components: { PortfolioSelector, CSRFToken, MapPopup, LoadingComponent, InputFieldsForm }
+    components: { PortfolioSelector, CSRFToken, MapPopup, LoadingComponent, InputFieldsForm, MapComponent }
 };
 </script> 
-
-
-<style>
-.hint {
-    background: #e3e3e3;
-    border-radius: 50%;
-    color: #6e6e6e;
-    display: inline-block;
-    font-weight: bold;
-    text-align: center;
-    width: 1.2vw;
-    height: 1.2vw;
-}
-
-.hint_text {
-    background: #e3e3e3;
-    color: #6e6e6e;
-    display: inline;
-    text-align: center;
-}
-</style>
