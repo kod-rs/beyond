a6c4059fcaee54f5ed5655ea309ac9643d18cc8e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-07 18:41:15 +0200

    backend api middleware roleCheckMiddleware.py - wip: role validation

--- backend/api/middleware/roleCheckMiddleware.py
+++ backend/api/middleware/roleCheckMiddleware.py
@@ -3,6 +3,7 @@ from decouple import config
 from backend.api.keycloak.keycloak_manager import get_roles
 from backend.api.config.main import MIDDLEWARE_NO_ACTION, INTERNAL_SERVER_ERROR_MESSAGE
 from backend.api.startup import startup_configuration
+from backend.api.role_action_validation.new_role_validation import check
 
 class RoleCheckMiddleware:
     def __init__(self, get_response):
@@ -14,7 +15,7 @@ class RoleCheckMiddleware:
     def __call__(self, request):
         print("RoleCheckMiddleware")
 
-        # access_token = request.access_token
+        access_token = request.access_token
 
         # if not hasattr(request, "action"):
         #     print("internal server error, check action middleware, "
@@ -25,8 +26,32 @@ class RoleCheckMiddleware:
         #     print("action is empty")
         #     request.action_checked = MIDDLEWARE_NO_ACTION
         # else:
-            # roles = get_roles(access_token)
-            # action = request.action
+        roles = get_roles(access_token)
+        # print(f"{roles=}")
+        request.roles = roles
+        # print(request.META)
+        # print(80 * "-")
+        print()
+        # print(request.method, type(request.method))
+        # print(request.path, type(request.method))
+
+        t = check(role=roles,
+                  path=request.path,
+                  method=request.method
+                  )
+        print(t)
+
+        # if (hasattr(request.META, "REQUEST_METHOD")):
+        #     print(80 * "-")
+        #     print(getattr(request.META, "REQUEST_METHOD"))
+        # print("get locations", request.META.REQUEST_METHOD)
+
+        # print(request.META)
+
+        roles = request.roles
+        print(f"{roles=}")
+
+        # action = request.action
             # print(f"{roles=} {action=} {request}")
 
             #     # print(request.META)
