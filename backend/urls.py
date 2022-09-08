dda908f7147d16b46d931ce80680e568c6e8178f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-08 19:34:38 +0200

    backend urls.py - wip: colour log

--- backend/urls.py
+++ backend/urls.py
@@ -2,16 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 
-from backend.api.model.testCRUD import TestCrudView
-# from backend.api.view.dbView import pureDjangoView
-# from backend.api.view.deviceView import DeviceView
 from backend.api.view.locationsView import LocationsView
 from backend.api.view.loginView import LoginView
 from backend.api.view.logoutView import LogoutView
 from backend.api.view.indexView import IndexView
 from backend.api.view.CSRFView import CSRFView
-# from backend.api.view.userInfo import UserInfoView
 from backend.api.view.portfolioView import PortfolioView
+from backend.api.view.colourView import ColourView
 
 from backend.api.comm.json_loader import role_validation_cfg
 
@@ -31,26 +28,28 @@ urlpatterns = [
     path('', IndexView.as_view()),
 
     path('login/', LoginView.as_view()),
+
     path('logout/', LogoutView.as_view()),
 
-    # path("device/<int:pk>", DeviceView.as_view()),
-    # path("device/", DeviceView.as_view()),
+    path("colour/", ColourView.as_view()),
+    path("colour/<str:abc>", ColourView.as_view()),
 
-    # path('puredjango', pureDjangoView, name='home'),
-
-    path("testcrud/", TestCrudView.as_view()),
-
-    path("locations/<int:pk>", LocationsView.as_view()),
     path("locations/<str:pn>", LocationsView.as_view()),
     path("locations/", LocationsView.as_view()),
 
     path('api/admin/', admin.site.urls),
+
     path("csrf/", CSRFView.as_view()),
-    # path("userinfo/", UserInfoView.as_view()),
+
     path("portfolio/", PortfolioView.as_view()),
     path("portfolio/<str:name>", PortfolioView.as_view()),
 
-    # path('loginkc/', LoginKCView.as_view()),
+    #
+    # path("locations/<int:pk>", LocationsView.as_view()),
+    # path("device/<int:pk>", DeviceView.as_view()),
+
+    # path('puredjango', pureDjangoView, name='home'),
+
 
     # path('accounts/', include('allauth.urls')),
     # path('bank', BankViewSet.as_view, name='bank'),
