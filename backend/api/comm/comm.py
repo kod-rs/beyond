1fe380e48f9da745bb833083ee0b67e5ca8443f3
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-08 19:34:38 +0200

    backend api comm comm.py - wip: colour log

--- backend/api/comm/comm.py
+++ backend/api/comm/comm.py
@@ -1,17 +1,24 @@
 import json
-from urllib.parse import unquote, parse_qs
 
 
 def pretty_print_json(payload):
     print(json.dumps(payload, indent=4, sort_keys=True))
 
 
-def decode_data(payload):
+def bytes_to_json(content):
+    return json.loads(content.decode("utf-8"))
 
-    try:
-        data_content = json.loads(payload.decode("utf-8"))
-    except json.decoder.JSONDecodeError:
-        data_content = unquote(payload)
-        data_content = parse_qs(data_content)
 
-    return data_content
+def get_empty_response_template():
+    response = {
+        "auth": {
+            "status": False,
+            "access-token": "",
+            "refresh-token": ""
+        },
+        "payload": {
+        },
+        "debug": {
+        }
+    }
+    return response
