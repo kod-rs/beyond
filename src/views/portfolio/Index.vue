e3dc2223f16f6807f5caf55cb4dc4ed0cc67761f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-08 19:34:38 +0200

    src views portfolio Index.vue - wip: colour log

--- src/views/portfolio/Index.vue
+++ src/views/portfolio/Index.vue
@@ -7,6 +7,13 @@
         <portfolio-details />
 
         <hr>
+
+        <button @click="addColour">add colour</button>
+
+
+        <div class="col">
+            <input type="color" class="form-control form-control-color" v-model="colourHex">
+        </div>
     </div>
 
 </template>
@@ -16,16 +23,27 @@
 
 import PortfolioList from "./PortfolioList.vue";
 import PortfolioDetails from "./PortfolioDetails.vue";
+import { apiColour } from '../../scripts/api/colour';
 
 export default {
     name: "TestPag",
     data() {
         return {
+            colourHex: "#3584e4"
         }
     },
     components: {
         "portfolio-list": PortfolioList,
         "portfolio-details": PortfolioDetails
+    },
+    methods: {
+        async addColour() {
+            console.log(this.colourHex)
+            console.log("add c")
+            let r = await apiColour.addColour("a", this.colourHex);
+            console.log(r);
+
+        }
     }
 };
 
