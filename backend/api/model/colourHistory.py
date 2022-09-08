87aeff3780b0d91647274e35a2e725483f60908c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-08 19:34:38 +0200

    backend api model colourHistory.py - wip: colour log

--- backend/api/model/colourHistory.py
+++ backend/api/model/colourHistory.py
@@ -1,15 +1,6 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 
-from backend.api.model.portfolio import Portfolio
+class ColourHistory(models.Model):
 
+    colour_hex_value = models.CharField(max_length=20, blank=True, null=True)
 
-class Colour(models.Model):
-
-    # name = models.CharField(max_length=200, unique=False)
-    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, null=True)
-    hex_log =      ArrayField(
-            models.CharField(max_length=7, blank=False),
-            size=10,
-        )
-    # hex = models.CharField(max_length=200, unique=False)
