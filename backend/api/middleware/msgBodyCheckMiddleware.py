4de4b79e8ffa69c06fa1ec0e50b3bb25f4747ad0
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-08 10:25:59 +0200

    backend api middleware msgBodyCheckMiddleware.py - fix: body & headers keys check

--- backend/api/middleware/msgBodyCheckMiddleware.py
+++ backend/api/middleware/msgBodyCheckMiddleware.py
@@ -1,9 +1,15 @@
 import json
 from django.http import JsonResponse
-from backend.api.middleware.comm import middleware_check_params
+
+from backend.api.comm.comm import decode_data
 from backend.api.comm.http import get_empty_response_template
 from backend.api.comm.json_loader import vue_interface_cfg
 from backend.api.middleware.comm import DebuggableMiddleware
+from backend.api.role_action_validation.args_check import check_params
+import json
+
+def bytes_to_json(content):
+    return json.loads(content.decode("utf-8"))
 
 
 class MsgBodyCheckMiddleware(DebuggableMiddleware):
@@ -15,6 +21,40 @@ class MsgBodyCheckMiddleware(DebuggableMiddleware):
 
     def __call__(self, request):
         print("MsgBodyCheckMiddleware")
+
+        got_method = request.method
+        got_headers = {}
+        got_body = {}
+
+        # fixme use meta?
+        if request.headers:
+            # print(f"{request.headers=}")
+            got_headers = request.headers
+            # if not got_headers:
+            #     got_headers = set()
+            # else:
+            got_headers = {k for k,v in got_headers.items()}
+            # got_headers = set(got_headers)
+        if request.body:
+            got_body = bytes_to_json(request.body)
+            # if not got_body:
+            #     got_body = set()
+            # else:
+            got_body = {k for k,v in got_body.items()}
+
+            # got_body = set(got_body)
+            # body_content = decode_data(request.body)
+            # print(f"{body_content=}")
+
+        # print(f"{got_method=} {got_headers=} {got_body=}")
+        t = check_params(
+            path =                   request.path,
+            method=got_method,
+            headers_got=got_headers,
+            body_got=got_body
+
+                         )
+
         # rejection = get_empty_response_template()
         #
         # if request.environ['QUERY_STRING'] != '':
