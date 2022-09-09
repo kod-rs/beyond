874f4ad9bcb72ba7035d03291a40b0a8be7b0ef9
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-09 18:53:11 +0200

    backend api cqrs_c colour.py - wip: colour log

--- backend/api/cqrs_c/colour.py
+++ backend/api/cqrs_c/colour.py
@@ -1,7 +1,9 @@
+
 from backend.api.model.colourHistory import ColourHistory
 from backend.api.model.user import User
 from django.utils import timezone
 
+from django.core import serializers
 
 def add_colour_to_user(portfolio, username, history_colour_id,timestamp_colour_change):
     i = User.objects.create(
@@ -19,6 +21,9 @@ def add_colour_to_log(username, portfolio, colour_hex):
     """
     select history_colour_id_id from api_user where username='a' and portfolio='a' order by timestamp_colour_change desc limit 1
 
+    todo rewrite with this
+        insert into api_user (id, portfolio, username, history_colour_id_id, timestamp_colour_change) select 14, 'a', 'a', 3, '2022-09-08 18:58:09.432646+02' where (select history_colour_id_id from api_user where username='a' and portfolio='a' order by timestamp_colour_change desc limit 1) != 4;
+
     """
 
     ch, _ = ColourHistory.objects.update_or_create(
@@ -27,11 +32,48 @@ def add_colour_to_log(username, portfolio, colour_hex):
     )
     ch.save()
 
-    u = User.objects.filter(portfolio=portfolio,username=username).order_by('timestamp_colour_change').first()
-    print(u.timestamp_colour_change)
+    u = User\
+        .objects\
+        .filter(portfolio=portfolio,username=username)\
+        .order_by('-timestamp_colour_change')\
+        .first()
+
+    if ch.id == u.history_colour_id_id:
+        print("this is last entry in db, nothing is changed")
+        # todo
+        # return
+
+    print("new selected, changing")
+    u = User.objects.create(
+        portfolio=portfolio,
+        username=username,
+        history_colour_id=ch,
+        timestamp_colour_change=timezone.now()
+    )
+    u.save()
+
+    print("cleanup")
+    #
+    # def get_colour_log(user, portfolio):
+
+    count = User \
+        .objects \
+        .filter(portfolio=portfolio, username=username)\
+        .values("history_colour_id_id")\
+        .distinct()\
+        .count()
+    print(f"{count=}")
 
 
 
+    if count == 11:
+        print("deleting last")
+        # .select_related("history_colour_id")
+
+        # return t
+
+    # print("added", ch.id)
+    # print("last", u.history_colour_id_id)
     # print(u.query)
     # u = User.objects.create(
     #     portfolio=portfolio,
