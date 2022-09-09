7d17e11802ee5b81c0699faea8dd16f7dae32dce
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-09 13:34:23 +0200

    backend api model user.py - wip: colour log

--- backend/api/model/user.py
+++ backend/api/model/user.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+from backend.api.model.colourHistory import ColourHistory
 
+
+# portfolio info
 class User(models.Model):
-    portfolio = models.CharField(max_length=20, blank=True, null=True)
-    username = models.CharField(max_length=20, blank=True, null=True)
-    history_colour_id = models.CharField(max_length=200, blank=True, null=True)
+    # alternative primary key
+    portfolio = models.CharField(max_length=200, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+
+    # foreign key
+    history_colour_id = models.ForeignKey(ColourHistory, on_delete=models.CASCADE)
+
+    # value
     timestamp_colour_change = models.DateTimeField(auto_now_add=True)
