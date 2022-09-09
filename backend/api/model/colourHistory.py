bef4a187fc1a358c5b38a3fc272346249b42cafd
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-09 13:34:23 +0200

    backend api model colourHistory.py - wip: colour log

--- backend/api/model/colourHistory.py
+++ backend/api/model/colourHistory.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class ColourHistory(models.Model):
 
+class ColourHistory(models.Model):
     colour_hex_value = models.CharField(max_length=20, blank=True, null=True)
 
