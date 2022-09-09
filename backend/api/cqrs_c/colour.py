d28dafad1fe872478d26499b1d07df3388dfe081
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-09 13:34:23 +0200

    backend api cqrs_c colour.py - wip: colour log

--- backend/api/cqrs_c/colour.py
+++ backend/api/cqrs_c/colour.py
@@ -1,6 +1,6 @@
-from backend.api.cqrs_q.portfolio import get_portfolio
 from backend.api.model.colourHistory import ColourHistory
 from backend.api.model.user import User
+from django.utils import timezone
 
 
 def add_colour_to_user(portfolio, username, history_colour_id,timestamp_colour_change):
@@ -14,23 +14,65 @@ def add_colour_to_user(portfolio, username, history_colour_id,timestamp_colour_c
     print("saved")
 
 def add_colour_to_log(username, portfolio, colour_hex):
-    print(username, portfolio, colour_hex)
-    # p = get_portfolio(portfolio)
+    # select * from api_user where username='a' and portfolio='a' order by timestamp_colour_change asc
 
-    i, _ = ColourHistory.objects.update_or_create(
-         defaults={"colour_hex_value": colour_hex}
+    """
+    select history_colour_id_id from api_user where username='a' and portfolio='a' order by timestamp_colour_change desc limit 1
+
+    """
+
+    ch, _ = ColourHistory.objects.update_or_create(
+        colour_hex_value=colour_hex,
+        defaults={"colour_hex_value": colour_hex}
     )
-    i.save()
+    ch.save()
 
-    return i.id
+    u = User.objects.filter(portfolio=portfolio,username=username).order_by('timestamp_colour_change').first()
+    print(u.timestamp_colour_change)
 
-    # l = Colour.objects.create(
-    #     colour_hex_value=colour_hex
+
+
+    # print(u.query)
+    # u = User.objects.create(
+    #     portfolio=portfolio,
+    #     username=username,
+    #     history_colour_id=ch,
+    #     timestamp_colour_change=timezone.now()
     # )
-    #
-    # l.save()
+    # u.save()
 
-    return True
+    # if last
+
+    # if ColourHistory.objects.filter(colour_hex_value=colour_hex).exists():
+    #
+    #     print("object exists")
+    #
+    #     ch = ColourHistory.objects.create(
+    #         colour_hex_value=colour_hex
+    #     )
+    #
+    #     ch.save()
+    #
+    #     u = User.objects.create(
+    #        portfolio=portfolio,
+    #         username=username,
+    #         history_colour_id=ch,
+    #         timestamp_colour_change=timezone.now()
+    #     )
+    #     u.save()
+    #
+    #     return True
+    #     return {"new": True}
+    # else:
+    #     return {"new": False}
+
+    # i, _ = ColourHistory.objects.update_or_create(
+    #     colour_hex_value=colour_hex,
+    #     defaults={"colour_hex_value": colour_hex}
+    # )
+
+    # return i.id
+
 
 def get_history():
     print("return history")
