2fe6d94aa3445b97dd3337de700229fb371393f0
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 23:47:38 +0200

    backend api cqrs_q csrf.py - wip: colour log

--- backend/api/cqrs_q/csrf.py
+++ backend/api/cqrs_q/csrf.py
@@ -1,34 +1,5 @@
-import ast
-import json
+from backend.api.model_security.csrf import CSRF
 
-from django.core import serializers
-
-from backend.api.model.csrf import CSRF
-
-
-# def get_all():
-#     t = serializers.serialize('json', CSRF.objects.all())
-#     t = json.loads(t)
-#
-#     n = []
-#     for i in t:
-#         f = {}
-#         p = i["pk"]
-#         f["pk"] = p
-#         f.update(i["fields"])
-#         n.append(f)
-#
-#     t = n
-#
-#     try:
-#         t = [{k: ast.literal_eval(v)[0] for k, v in i.items()} for i in t]
-#     except ValueError:
-#         try:
-#             t = [{k: ast.literal_eval(v) for k, v in i.items()} for i in t]
-#         except ValueError:
-#             t = [{k: v for k, v in i.items()} for i in t]
-#
-#     return t
 
 def get_by_ip(ip):
     if CSRF.objects.filter(ip=ip).exists():
@@ -37,4 +8,3 @@ def get_by_ip(ip):
         return entry
 
     return None
-    # return CSRF.objects.filter(ip=ip)
