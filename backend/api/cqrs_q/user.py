5140a655909d93b0d38977705fef5fc28d71dc23
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 23:47:37 +0200

    backend api cqrs_q user.py - wip: colour log

--- backend/api/cqrs_q/user.py
+++ backend/api/cqrs_q/user.py
@@ -1,9 +1,9 @@
-from backend.api.model.user import User
+from backend.api.model.portfoliocolouradapter import PortfolioColourAdapter
 
 
 def get_colour_log(user, portfolio):
 
-    t = User\
+    t = PortfolioColourAdapter\
         .objects\
         .filter(portfolio=portfolio, username=user)\
         .select_related("history_colour_id")\
