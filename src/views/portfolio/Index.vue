25148e85ccaaf8d8cfee025cc40122707429b811
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 16:26:03 +0200

    src views portfolio Index.vue - wip: colour log

--- src/views/portfolio/Index.vue
+++ src/views/portfolio/Index.vue
@@ -4,34 +4,10 @@
         <hr>
         <portfolio-list />
         <hr>
-        <portfolio-details />
+        <!-- <portfolio-details /> -->
 
-        <hr>
+        <!-- <hr> -->
 
-
-
-        <div class="col">
-            <div class="row">
-                <input type="text" v-model="colourHex">
-                <button @click="addColour">add colour</button>
-                <hr>
-            </div>
-
-            <div class="row">
-                <button @click="refresh">refresh</button>
-                current state
-                <br>
-                <div v-for="i in colours" :key="i">
-                    {{i}}
-                </div>
-                <hr>
-            </div>
-
-            <div class="row">
-                <button @click="deleteAll">clear list</button>
-                <hr>
-            </div>
-        </div>
     </div>
 
 </template>
@@ -40,47 +16,20 @@
 <script>
 
 import PortfolioList from "./PortfolioList.vue";
-import PortfolioDetails from "./PortfolioDetails.vue";
-import { apiColour } from '../../scripts/api/colour';
+// import PortfolioDetails from "./PortfolioDetails.vue";
 
 export default {
-    name: "TestPag",
+    name: "PortfolioIndex",
     data() {
         return {
-            colourHex: "#p",
-            colours: ["a", "b"]
+
         }
     },
     components: {
         "portfolio-list": PortfolioList,
-        "portfolio-details": PortfolioDetails
+        // "portfolio-details": PortfolioDetails
     },
-    methods: {
-        async deleteAll() {
-            console.log("delete all");
-            let r = await apiColour.deleteColour("a");
-            console.log(r)
-        },
 
-        async refresh() {
-            let r = await apiColour.getColourHistory("a");
-            console.table(r["payload"]["value"])
-
-            this.colours = [];
-            for (const [key, value] of Object.entries(r["payload"]["value"])) {
-                console.log(key, value, typeof (key));
-                this.colours.push(value);
-            }
-        },
-
-        async addColour() {
-            console.log(this.colourHex)
-            console.log("add c")
-            let r = await apiColour.addColour("a", this.colourHex);
-            console.log(r);
-
-        }
-    }
 };
 
 </script> 
