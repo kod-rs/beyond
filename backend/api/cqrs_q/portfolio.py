840587beb284376e0f6cd3395fec6d7e3c527c3f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 23:47:38 +0200

    backend api cqrs_q portfolio.py - wip: colour log

--- backend/api/cqrs_q/portfolio.py
+++ backend/api/cqrs_q/portfolio.py
@@ -20,19 +20,15 @@ def get_portfolios_with_locations(username):
     try:
 
         ip_obj = Location.objects.filter(portfolio__username=username)
-        print(f"{ip_obj=}")
 
-        t = serializers.serialize('json', ip_obj)
-        print(t)
-        print("---")
-        # for i in t:
-        #     print(i)
+
         return ip_obj
 
     except Portfolio.DoesNotExist:
         return False
 
 def get_portfolio_names(username):
+    # fixme what if no portfolios
 
     try:
         return Portfolio.objects.filter(username=username).only("name")
