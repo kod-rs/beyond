4e8d1c701483d12899fd05deb1fb78380102f59a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 23:47:38 +0200

    backend api model portfoliocolouradapter.py - wip: colour log

--- backend/api/model/portfoliocolouradapter.py
+++ backend/api/model/portfoliocolouradapter.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 from backend.api.model.colourHistory import ColourHistory
-from backend.api.model.portfolioNameHistory import PortfolioNameHistory
 
 
-class PortfolioHistoryAdapter(models.Model):
+class PortfolioColourAdapter(models.Model):
     # alternative primary key
     portfolio = models.CharField(max_length=200, blank=True, null=True)
     username = models.CharField(max_length=200, blank=True, null=True)
@@ -12,12 +11,6 @@ class PortfolioHistoryAdapter(models.Model):
     # foreign key
     history_colour_id = models.ForeignKey(
         ColourHistory,
-        blank=True,
-        null=True,
-        on_delete=models.CASCADE
-    )
-    history_name_id = models.ForeignKey(
-        PortfolioNameHistory,
         blank=True,
         null=True,
         on_delete=models.CASCADE
