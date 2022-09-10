0ea39cb5de6dc56f7cdb375e8c3913e49c01788b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 11:39:38 +0200

    backend api cqrs_q user.py - impl: colour log

--- backend/api/cqrs_q/user.py
+++ backend/api/cqrs_q/user.py
@@ -6,7 +6,6 @@ def get_colour_log(user, portfolio):
     t = User\
         .objects\
         .filter(portfolio=portfolio, username=user)\
-        .select_related("history_colour_id")
-
-
+        .select_related("history_colour_id")\
+        .order_by('-timestamp_colour_change')
     return t
