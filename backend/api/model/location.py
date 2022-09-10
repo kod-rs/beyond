3dc0b6c7084cd0af21faae8747d8fb3757f8a6be
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 23:47:38 +0200

    backend api model location.py - wip: colour log

--- backend/api/model/location.py
+++ backend/api/model/location.py
@@ -1,8 +1,8 @@
 from django.db import models
 from backend.api.model.portfolio import Portfolio
 
+
 class Location(models.Model):
-    # portfolio = models.CharField(max_length=200)
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, null=True)
 
     section = models.CharField(max_length=200)
