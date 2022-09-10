7c0a7da5bed4019a2ec9b069eb1ceccfaed856eb
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-10 23:47:37 +0200

    backend api cqrs_c portfolio.py - wip: colour log

--- backend/api/cqrs_c/portfolio.py
+++ backend/api/cqrs_c/portfolio.py
@@ -3,6 +3,40 @@ from django.db import utils
 from backend.api.model.portfolio import Portfolio
 
 
+def create_portfolio(username, portfolio_name, colour):
+    i = Portfolio.objects.create(
+        username=username,
+        name=portfolio_name,
+        colour_tmp=colour
+    )
+    i.save()
+
+def update(username, current_name, params):
+
+    return Portfolio.objects\
+        .filter(username=username, name=current_name)\
+        .update(**params)
+
+def update_portfolio_colour(username, current_name, new_colour):
+    return Portfolio.objects\
+        .filter(username=username, name=current_name) \
+        .update(colour=new_colour)
+
+def update_portfolio_name(username, current_name, new_name):
+    print(f"{username=} {current_name=} {new_name=}")
+
+    return Portfolio.objects\
+        .filter(username=username, name=current_name) \
+        .update(name=new_name)
+
+
+def update_portfolio(username, current_name, new_name=None, new_colour=None):
+
+    return Portfolio.objects\
+        .filter(username=username, name=current_name)\
+        .update(name=new_name, colour=new_colour)
+
+
 def create_or_update(username, current_name, new_name, colour):
 
     print("create_or_update")
@@ -12,7 +46,7 @@ def create_or_update(username, current_name, new_name, colour):
     try:
         i, _ = Portfolio.objects.update_or_create(
             username=username, name=current_name,
-            defaults={"name": new_name, "colour_tmp": colour}
+            defaults={"name": new_name, "colour": colour}
         )
         print(i)
         i.save()
