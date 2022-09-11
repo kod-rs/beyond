828aab8e50030b12df1bb8c8a727c3e13d631e2e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-11 23:40:56 +0200

    backend api cqrs_c portfolio.py - ref: map

--- backend/api/cqrs_c/portfolio.py
+++ backend/api/cqrs_c/portfolio.py
@@ -1,63 +1,29 @@
-from django.db import utils
-
 from backend.api.model.portfolio import Portfolio
 
 
 def create_portfolio(username, portfolio_name, colour):
+    print(username, portfolio_name, colour)
     i = Portfolio.objects.create(
         username=username,
         name=portfolio_name,
-        colour_tmp=colour
+        colour=colour
     )
     i.save()
-
-def update(username, current_name, params):
-
-    return Portfolio.objects\
-        .filter(username=username, name=current_name)\
-        .update(**params)
-
-def update_portfolio_colour(username, current_name, new_colour):
-    return Portfolio.objects\
-        .filter(username=username, name=current_name) \
-        .update(colour=new_colour)
-
-def update_portfolio_name(username, current_name, new_name):
-    print(f"{username=} {current_name=} {new_name=}")
-
-    return Portfolio.objects\
-        .filter(username=username, name=current_name) \
-        .update(name=new_name)
+    return bool(i)
 
 
-def update_portfolio(username, current_name, new_name=None, new_colour=None):
+def update_portfolio(username, current_name, params):
+    params = {k: v for k, v in params.items() if k in ["name", "colour"]}
 
-    return Portfolio.objects\
-        .filter(username=username, name=current_name)\
-        .update(name=new_name, colour=new_colour)
-
-
-def create_or_update(username, current_name, new_name, colour):
-
-    print("create_or_update")
-
-    print(f"if have {username=} {current_name=}")
-    print(f"update this {new_name=} {colour=}")
-    try:
-        i, _ = Portfolio.objects.update_or_create(
-            username=username, name=current_name,
-            defaults={"name": new_name, "colour": colour}
-        )
-        print(i)
-        i.save()
-
-        # i, _ = Colour.objects.
-
-        return True
-    except utils.IntegrityError:
-        print("integrity err")
-        return False
+    return bool(
+        Portfolio.objects \
+            .filter(username=username, name=current_name) \
+            .update(**params)
+    )
 
 
 def delete_portfolio(username, portfolio_name):
-    Portfolio.objects.filter(username=username, name=portfolio_name).delete()
+    return bool(
+        Portfolio.objects.filter(username=username,
+                                 name=portfolio_name).delete()
+    )
