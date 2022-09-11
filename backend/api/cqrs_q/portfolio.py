95cca9e8e1246338c6dcd32e3848f55aa665cd14
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-11 23:40:56 +0200

    backend api cqrs_q portfolio.py - ref: map

--- backend/api/cqrs_q/portfolio.py
+++ backend/api/cqrs_q/portfolio.py
@@ -2,36 +2,33 @@ from backend.api.model.portfolio import Portfolio
 from backend.api.model.location import Location
 from django.core import serializers
 
-def get_portfolio(name):
-    return Portfolio.objects.get(name=name)
+
+def get_portfolio(username, portfolio_name):
+    return Portfolio.objects.get(username=username, name=portfolio_name)
 
 
 def get_portfolios(username):
 
     try:
-        return Portfolio.objects.filter(username=username)
+        return {"exists": True, "payload": Portfolio.objects.filter(username=username)}
 
     except Portfolio.DoesNotExist:
-        return False
+        return {"exists": False}
 
 
-def get_portfolios_with_locations(username):
+# def get_portfolios_with_locations(username):
+#
+#     try:
+#         return Location.objects.filter(portfolio__username=username)
+#
+#     except Portfolio.DoesNotExist:
+#         return False
 
-    try:
-
-        ip_obj = Location.objects.filter(portfolio__username=username)
-
-
-        return ip_obj
-
-    except Portfolio.DoesNotExist:
-        return False
-
-def get_portfolio_names(username):
-    # fixme what if no portfolios
-
-    try:
-        return Portfolio.objects.filter(username=username).only("name")
-
-    except Portfolio.DoesNotExist:
-        return False
+# def get_portfolio_names(username):
+#     # fixme what if no portfolios
+#
+#     try:
+#         return Portfolio.objects.filter(username=username).only("name")
+#
+#     except Portfolio.DoesNotExist:
+#         return False
