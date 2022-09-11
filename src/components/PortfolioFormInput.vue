5fb4219894442671fa4562e43fe2334d6acf6890
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-11 23:40:56 +0200

    src components PortfolioFormInput.vue - ref: map

---
+++ src/components/PortfolioFormInput.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div>
+        <input placeholder="Portfolio" class="form-control" list="browsers" name="browser">
+        <datalist id="browsers">
+            <option value="Internet Explorer" />
+            <option value="Firefox" />
+            <option value="Chrome" />
+            <option value="Opera" />
+            <option value="Safari" />
+        </datalist>
+
+    </div>
+
+</template>
+
+    
+
+
+<script>
+
+export default {
+
+    data() {
+        return {
+
+        };
+    },
+};
+</script> 
