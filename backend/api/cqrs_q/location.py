e4c02aeab681a7d36a77b45db4b35402d85706c0
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:16 +0200

    backend api cqrs_q location.py - wip: master detail

--- backend/api/cqrs_q/location.py
+++ backend/api/cqrs_q/location.py
@@ -3,7 +3,10 @@ from backend.api.model.location import Location
 
 def get_user_portfolio(username, portfolio_name):
 
-    return Location.objects.filter(portfolio__username=username, portfolio__name=portfolio_name)
+    return Location.objects.filter(
+        portfolio__username=username,
+        portfolio__name=portfolio_name
+    )
 
 
 
