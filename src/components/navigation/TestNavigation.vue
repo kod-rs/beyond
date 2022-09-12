394d3c2a9d8556d52bbc6d25909954f1fb76e241
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:15 +0200

    src components navigation TestNavigation.vue - wip: master detail

--- src/components/navigation/TestNavigation.vue
+++ src/components/navigation/TestNavigation.vue
@@ -1,35 +1,27 @@
 <template>
-
-    <div class="testnavigation">
-        test navigation routes:
-        <router-link :to="{ name: 'index' }">home</router-link> |
-        <router-link :to="{ name: 'addlocation' }">Create</router-link> |
-        <!-- <router-link :to="{ name: 'viewlocation' }">View</router-link> | -->
-        <router-link to="/logout">Logout</router-link> |
-        <router-link :to="{ name: 'portfolio' }">manage portfolio</router-link> |
-        <!-- <router-link :to="{ name: 'locations' }">manage locations</router-link> | -->
-        <router-link :to="{ name: 'test' }">test</router-link> |
-        <router-link :to="{ name: 'login' }">login</router-link> |
-
-
-    </div>
-
-
+  <div class="testnavigation">
+    test navigation routes:
+    <router-link :to="{ name: 'login' }">login</router-link> |
+    <router-link :to="{ name: 'index' }">home</router-link> |
+    <router-link :to="{ name: 'addlocation' }">Create</router-link> |
+    <router-link :to="{ name: 'portfolio' }">manage portfolio</router-link> |
+    <router-link :to="{ name: 'test' }">test</router-link> |
+    <router-link :to="{ name: 'history' }">history</router-link> |
+    <router-link to="/logout">Logout</router-link> |
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-
-        }
-    }
-}
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
 .testnavigation {
-    border: 1mm solid black;
+  border: 1mm solid black;
 }
 </style>
 
