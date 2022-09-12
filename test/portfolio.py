09d0f27fd21f2fd578927f72ed058f85b9606bdd
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:15 +0200

    test portfolio.py - wip: master detail

---
+++ test/portfolio.py
@@ -0,0 +1,78 @@
+import json
+import random
+import requests
+
+url = "http://localhost:8000/portfolio/"
+
+
+def get():
+
+    t = requests.get(
+        url,
+        data={
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def post():
+
+    t = requests.post(
+        url + "d",
+        data={
+            "colour": "clr1"
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def patch():
+
+    t = requests.patch(
+        url + "d",
+        data={
+            "name": "d",
+            "colour": "p"
+
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def delete():
+
+    t = requests.delete(
+        url + "c",
+        data={
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+
+
+    # url = "http://localhost:8000/location//7/7"
+    # # headers = get_valid_credentials()
+    #
+    # t = requests.delete(url, headers={
+    #
+    # }, data={
+    #
+    # }, verify=False)
+    # print(t)
+    # print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def main():
+    # get_all_locations()
+    # create()
+    # delete()
+    # patch()
+    # post()
+    get()
+
+if __name__ == '__main__':
+    main()
