fd3b0d08dbf5e9439433d214cb323d5f84154f7e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:15 +0200

    src components BaseNotification.vue - wip: master detail

---
+++ src/components/BaseNotification.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    showMessage(result, successMessage, errorMessage) {
+      if (result) {
+        if (successMessage) {
+          this.$waveui.notify(successMessage, "success");
+        }
+      } else {
+        if (errorMessage) {
+          this.$waveui.notify(errorMessage, "error");
+        }
+      }
+    },
+  },
+};
+</script>
+ 
+<style>
+</style>
