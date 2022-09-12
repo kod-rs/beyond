d4602e23c60395ba355cc60b71931b607c07bb91
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:15 +0200

    backend api model location.py - wip: master detail

--- backend/api/model/location.py
+++ backend/api/model/location.py
@@ -3,9 +3,10 @@ from backend.api.model.portfolio import Portfolio
 
 
 class Location(models.Model):
+    # alternative composite key
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, null=True)
-
     section = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
+
     latitude = models.FloatField()
     longitude = models.FloatField()
