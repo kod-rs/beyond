f3f8caabec2fc2ab743b3749eef3b4dd054f041c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:15 +0200

    backend api cqrs_q portfolio.py - wip: master detail

--- backend/api/cqrs_q/portfolio.py
+++ backend/api/cqrs_q/portfolio.py
@@ -3,11 +3,23 @@ from backend.api.model.location import Location
 from django.core import serializers
 
 
-def get_portfolio(username, portfolio_name):
-    return Portfolio.objects.get(username=username, name=portfolio_name)
+def get_single_portfolio(username, portfolio_name):
+    # if Portfolio.objects.filter(username=username, name=portfolio_name).exists():
+    #     return {"status": False, "description": EXISTS}
+
+    try:
+        return {
+            "exists": True,
+            "content":
+                Portfolio.objects.get(username=username, name=portfolio_name)
+
+        }
+    except Portfolio.DoesNotExist:
+        return {"exists": False}
 
 
-def get_portfolios(username):
+
+def get_all_portfolio(username):
 
     try:
         return {"exists": True, "payload": Portfolio.objects.filter(username=username)}
