667ecdf18c7a3f283b45ef476b136ef1bd50a8d3
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:15 +0200

    backend api view portfolioView.py - wip: master detail

--- backend/api/view/portfolioView.py
+++ backend/api/view/portfolioView.py
@@ -5,9 +5,10 @@ from rest_framework.views import APIView
 
 from backend.api.cqrs_c.portfolio import  delete_portfolio, \
     update_portfolio, create_portfolio
-from backend.api.cqrs_q.portfolio import get_portfolios
+from backend.api.cqrs_q.portfolio import get_all_portfolio, get_single_portfolio
 from backend.api.view.comm import get_auth_ok_response_template
 
+from backend.api.comm.constants import EXISTS,CREATED
 
 class PortfolioView(APIView):
 
@@ -15,56 +16,67 @@ class PortfolioView(APIView):
         print("LocationsView patch", name)
 
         response = get_auth_ok_response_template(request)
-        response["payload"]["status"] = update_portfolio(request.username, name, request.data)
+        response["payload"] = update_portfolio(request.username, name, request.data)
         return JsonResponse(response)
 
     def delete(self, request, name):
         print("PortfolioView delete", name)
 
         response = get_auth_ok_response_template(request)
-        response["payload"]["status"] = delete_portfolio(username=request.username, portfolio_name=name)
+        response["payload"] = delete_portfolio(username=request.username, portfolio_name=name)
         return JsonResponse(response)
 
     def post(self, request, name):
         print("PortfolioView post", name)
-
         response = get_auth_ok_response_template(request)
-        response["payload"]["status"] = create_portfolio(request.username, name, request.data["colour"])
+
+        response["payload"]= create_portfolio(request.username, name, request.data["colour"])
         return JsonResponse(response)
 
-    def get(self, request):
+    def get(self, request, name=None):
+        print(f"portfolio get {name=}")
+
         response = get_auth_ok_response_template(request)
-
-        c = get_portfolios(request.username)
-
-        if not c["exists"]:
+        if name:
+            # todo
+            # p = get_single_portfolio(request.username, name)
+            print("not implemented")
             response["payload"]["status"] = False
+            response["payload"]["description"] = "not implemented"
             return JsonResponse(response)
 
-        portfolios = c["payload"]
-        r = {}
+        else:
 
-        for i in portfolios:
-            hex_colour = i.colour[1:]
 
-            query = hex_colour
-            t = urllib.parse.quote(query)
+            c = get_all_portfolio(request.username)
 
-            r[i.name] = {
-                "name": i.name,
-                "colour": i.colour,
+            if not c["exists"]:
+                response["payload"]["status"] = False
+                return JsonResponse(response)
 
-                # "colourHex": pre_hex_colour,
-                "colourHexEncoded": t,
-                # todo log session state
-                "isExpanded": False
-            }
+            portfolios = c["payload"]
+            r = {}
 
-        response = get_auth_ok_response_template(request)
-        response["payload"]["status"] = True
+            for count, i in enumerate(portfolios):
+                hex_colour = i.colour[1:]
 
-        response["payload"]["portfolios"] = r
+                query = hex_colour
+                t = urllib.parse.quote(query)
 
-        response["payload"]["role"] = request.role
+                r[count] = {
+                    "name": i.name,
+                    "colour": i.colour,
+                    # todo remove
+                    "colourHexEncoded": t,
+                    # todo log session state
+                    "isExpanded": False
+                }
 
-        return JsonResponse(response)
+            response = get_auth_ok_response_template(request)
+            response["payload"]["status"] = True
+
+            response["payload"]["portfolios"] = r
+
+            response["payload"]["role"] = request.role
+
+            return JsonResponse(response)
