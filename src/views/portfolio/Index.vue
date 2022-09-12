c8b9e276cf6dc528467eaa798fddcfbfdbeea788
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-12 23:13:15 +0200

    src views portfolio Index.vue - wip: master detail

--- src/views/portfolio/Index.vue
+++ src/views/portfolio/Index.vue
@@ -1,35 +1,20 @@
 <template>
-
-    <div class="container mt-2">
-        <hr>
-        <portfolio-list />
-        <hr>
-        <!-- <portfolio-details /> -->
-
-        <!-- <hr> -->
-
-    </div>
-
+  <div class="container mt-2">
+    <portfolio-list />
+  </div>
 </template>
 
 
 <script>
-
-import PortfolioList from "./PortfolioList.vue";
-// import PortfolioDetails from "./PortfolioDetails.vue";
+import PortfolioList from "../../components/BasePortfolioList.vue";
 
 export default {
-    name: "PortfolioIndex",
-    data() {
-        return {
-
-        }
-    },
-    components: {
-        "portfolio-list": PortfolioList,
-        // "portfolio-details": PortfolioDetails
-    },
-
+  name: "PortfolioIndex",
+  data() {
+    return {};
+  },
+  components: {
+    "portfolio-list": PortfolioList,
+  },
 };
-
 </script> 
