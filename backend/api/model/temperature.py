f7493e5e2c0e465e7d7aecf19d3103d4d9236c00
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-13 20:47:17 +0200

    backend api model temperature.py - impl: colour api

--- backend/api/model/temperature.py
+++ backend/api/model/temperature.py
@@ -13,14 +13,12 @@ class TemperatureEntry(models.Model):
     # alt composite prim key
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
 
-    # values
     value = models.ForeignKey(
         TemperatureValues,
         blank=True,
         null=True,
         on_delete=models.CASCADE
     )
+
+    # value
     timestamp = models.DateTimeField(auto_now_add=True)
-
-    # colour = models.CharField(max_length=20, blank=True, null=True)
-
