572d983b09282c9c0d32b47af9cfb71fa8d00992
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-13 17:10:28 +0200

    src scripts api colour.js - fix: remove colour from portfolio model

--- src/scripts/api/colour.js
+++ src/scripts/api/colour.js
@@ -1,18 +1,18 @@
 import { apiCalls } from './comm';
 
 
-async function deleteColour(i) {
+// async function deleteColour(i) {
 
-    return await apiCalls.handleNewResponse(
-        await apiCalls.api.delete(
-            "colour/" + i,
-            JSON.stringify({
-            }),
-            apiCalls.get_auth_header()
-        )
-    );
+//     return await apiCalls.handleNewResponse(
+//         await apiCalls.api.delete(
+//             "colour/" + i,
+//             JSON.stringify({
+//             }),
+//             apiCalls.get_auth_header()
+//         )
+//     );
 
-}
+// }
 
 async function addColour(portfolio, colourHex) {
     return await apiCalls.handleNewResponse(
@@ -27,7 +27,7 @@ async function addColour(portfolio, colourHex) {
 
 }
 
-async function getColourHistory(portfolioName) {
+async function getAllColours(portfolioName) {
     console.log("portfolio name", portfolioName)
     return await apiCalls.handleNewResponse(
         await apiCalls.api.get(
@@ -37,8 +37,27 @@ async function getColourHistory(portfolioName) {
     );
 }
 
+async function getLastColour(portfolioName) {
+    console.log("portfolio name", portfolioName)
+let params = {"options": "last"};
+
+    return await apiCalls.handleNewResponse(
+        await apiCalls.api.get(
+            "colour/" + portfolioName,
+            {
+                ...apiCalls.get_auth_header(),
+                ...params
+
+            }
+        )
+    );
+}
+
+
 export const apiColour = {
-    deleteColour,
+    // deleteColour,
     addColour,
-    getColourHistory
+    // getColourHistory
+    getAllColours,
+    getLastColour
 }
