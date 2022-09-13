a8eb1379fd2e1440b2ad93d1bafaee8b8818d483
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-13 17:10:28 +0200

    src views test TestPage.vue - fix: remove colour from portfolio model

--- src/views/test/TestPage.vue
+++ src/views/test/TestPage.vue
@@ -1,129 +1,44 @@
 <template>
-  <div>
-    <header>Header</header>
-    <w-flex grow>
-      <aside>Nav menu</aside>
-      <main class="grow">
-        Main content
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <CChartLine :data="defaultData" />
 
-        <hr />
+        <!-- left -->
+      </div>
+      <div class="col">
+        <!-- <CChartLine :data="defaultData" /> -->
 
-        <hr />
-        <hr />
-        <hr />
-        <hr />
-        <hr />
-        <hr />
-      </main>
-    </w-flex>
-    <footer>Footer</footer>
+        <!-- right -->
+      </div>
+    </div>
+    <!-- <div class="row"></div> -->
   </div>
 </template>
 
-<style>
-/* Demo styles - can be discarded. */
-/* .w-app {
-  padding: 4px;
-  background-color: #fffeed;
-}
-header,
-footer,
-aside,
-main {
-  margin: 4px;
-  padding: 12px;
-  text-transform: uppercase;
-  color: #666;
-  border: 1px solid rgba(0, 0, 0, 0.07);
-}
-header,
-footer {
-  background-color: #e9f8fe;
-  min-height: 60px;
-}
-aside {
-  background-color: #fef2ff;
-}
-main {
-  background-color: #efffed;
-} */
-</style>
-
 <script>
+import { CChartLine } from "@coreui/vue-chartjs";
 export default {
-  name: "TestPag",
-  data() {
-    return {
-      data: [
-        {
-          id: 1,
-          dessert: "Frozen yogurt",
-          calories: 159,
-          fat: 6,
-          carbs: 24,
-          protein: 4,
-        },
-        {
-          id: 2,
-          dessert: "Ice cream sandwich",
-          calories: 237,
-          fat: 9,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          id: 3,
-          dessert: "Eclair",
-          calories: 262,
-          fat: 16,
-          carbs: 24,
-          protein: 6,
-        },
-      ],
-      thead: [
-        "ID",
-        "Dessert (100g serving)",
-        "Calories",
-        "Fat (g)",
-        "Carbs (g)",
-        "Protein (g)",
-      ],
-      tbody: ["id", "dessert", "calories", "fat", "carbs", "protein"],
-    };
-  },
-  methods: {
-    downoladImage() {
-      let text = `<svg width="120" height="120" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0h24v24H0z" fill="none"/>
-            <path fill="#F1FA2B" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
-        </svg>`;
-
-      const win = window.URL || window.webkitURL || window;
-
-      let img = new Image();
-      img.src = win.createObjectURL(
-        new Blob([text], { type: "image/svg+xml" })
-      );
-
-      img.onload = () => {
-        let canvas = document.createElement("canvas");
-        canvas.width = 120;
-        canvas.height = 120;
-        canvas.getContext("2d").drawImage(img, 0, 0);
-
-        let a = document.createElement("a");
-        a.href = canvas.toDataURL("octet/stream");
-        console.log(a.href);
-        a.download = "img.png";
-        a.click();
-
-        win.revokeObjectURL(img.src);
-        win.revokeObjectURL(a.href);
+  name: "CChartLineExample",
+  components: { CChartLine },
+  computed: {
+    defaultData() {
+      return {
+        labels: ["months", "a", "b", "c", "d"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "rgb(228,102,81,0.9)",
+            data: [30, 39, 10, 50, 30, 70, 35],
+          },
+          {
+            label: "Data Two",
+            backgroundColor: "rgb(0,216,255,0.9)",
+            data: [39, 80, 40, 35, 40, 20, 45],
+          },
+        ],
       };
     },
   },
-  mounted() {
-    console.log("m");
-  },
 };
-</script> 
+</script>
