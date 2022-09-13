d53b0c502594c94adb5d8fbdf66598e204301e1a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-13 17:10:28 +0200

    backend api cqrs_c portfolio.py - fix: remove colour from portfolio model

--- backend/api/cqrs_c/portfolio.py
+++ backend/api/cqrs_c/portfolio.py
@@ -1,8 +1,8 @@
 from backend.api.model.portfolio import Portfolio
 from backend.api.comm.constants import EXISTS,CREATED,NOT_EXISTS
+from backend.api.cqrs_c.colour import add_colour_to_log
 
-
-def create_portfolio(username, portfolio_name, colour):
+def create_portfolio(username, portfolio_name, colour=None):
     print(username, portfolio_name, colour)
 
     if Portfolio.objects.filter(username=username, name=portfolio_name).exists():
@@ -11,9 +11,15 @@ def create_portfolio(username, portfolio_name, colour):
     i = Portfolio.objects.create(
         username=username,
         name=portfolio_name,
-        colour=colour
+        # colour=colour
     )
     i.save()
+
+    if colour:
+        # todo log colour
+        print("log colour")
+        add_colour_to_log(username,portfolio_name,colour)
+
     return {"status": True}
 
     # return {"status":bool(i)}
@@ -23,13 +29,23 @@ def update_portfolio(username, current_name, params):
     if not Portfolio.objects.filter(username=username, name=current_name).exists():
         return {"status": False, "description": NOT_EXISTS}
 
-    params = {k: v for k, v in params.items() if k in ["name", "colour"]}
+    portfolio_name = current_name
+    if "name" in params:
+        Portfolio.objects \
+            .filter(username=username, name=current_name) \
+            .update(name=params["name"])
+            # .update(**params)
 
+        portfolio_name = params["name"]
+
+    if "colour" in params:
+        r = add_colour_to_log(username, portfolio_name, params["colour"])
+        return r
+
+    # params = {k: v for k, v in params.items() if k in ["name", "colour"]}
     # return bool(
-    Portfolio.objects \
-        .filter(username=username, name=current_name) \
-        .update(**params)
     # )
+    #
     return {"status": True}
 
 def delete_portfolio(username, portfolio_name):
