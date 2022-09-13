a39b70498d7f01fae0e76c1be6d86525e08d7299
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-13 17:10:28 +0200

    backend api model portfolio.py - fix: remove colour from portfolio model

--- backend/api/model/portfolio.py
+++ backend/api/model/portfolio.py
@@ -16,10 +16,10 @@ alter table api_portfolio add constraint table_pkey primary key (username, name)
 
 
 class Portfolio(models.Model):
+    # alt composite key
     username = models.CharField(max_length=200,unique=False)
-
-    # current
     name = models.CharField(max_length=200,unique=False)
-    colour = models.CharField(max_length=200)
+
+    # colour = models.CharField(max_length=200)
 
     # history in PortfolioHistoryAdapter
