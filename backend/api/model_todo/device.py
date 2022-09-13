4445d0581319bd60251b2ff802d88efcefddb307
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-13 17:10:28 +0200

    backend api model_todo device.py - fix: remove colour from portfolio model

--- backend/api/model_todo/device.py
+++ backend/api/model_todo/device.py
@@ -1,9 +1,9 @@
-from django.db import models
-
-
-class Device(models.Model):
-    # fixme check how to connect
-
-    data_id = models.PositiveIntegerField()
-    type_id = models.PositiveIntegerField()
-    consumption = models.FloatField()
+# from django.db import models
+#
+#
+# class Device(models.Model):
+#     # fixme check how to connect
+#
+#     data_id = models.PositiveIntegerField()
+#     type_id = models.PositiveIntegerField()
+#     consumption = models.FloatField()
