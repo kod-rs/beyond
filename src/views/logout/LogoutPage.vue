9bafe538b6ef3f59d3905352730d80de42ca03a3
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-13 17:10:28 +0200

    src views logout LogoutPage.vue - fix: remove colour from portfolio model

--- src/views/logout/LogoutPage.vue
+++ src/views/logout/LogoutPage.vue
@@ -1,40 +1,30 @@
 <template>
-
-    <BasePublicTemplate>
-
-        <div class="d-flex align-items-center mb-3 pb-1">
-            <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-            <div class="h1 fw-bold mb-0">
-                <div class="text-right">
-                    Goodbye
-                </div>
-            </div>
-        </div>
-
-    </BasePublicTemplate>
-
+  <BasePublicTemplate>
+    <div class="d-flex align-items-center mb-3 pb-1">
+      <div class="h1 fw-bold mb-0">
+        <div class="text-right">Goodbye</div>
+      </div>
+    </div>
+  </BasePublicTemplate>
 </template>
 
 
 <script>
 import BasePublicTemplate from "@/components/BasePublicTemplate.vue";
-import { router } from '../../router/router';
-import { apiAuth } from '../../scripts/api/auth';
+import { router } from "../../router/router";
+import { apiAuth } from "../../scripts/api/auth";
 
 export default {
-    data() {
-        return {};
-    },
-    async created() {
-        apiAuth.logout();
-        this.returnUrl = this.$route.query.returnUrl || '/';
-        await new Promise(r => setTimeout(r, 2000)).then(() => {
-            router.push(this.returnUrl);
-
-        });
-
-    },
-    components: { BasePublicTemplate }
-
+  data() {
+    return {};
+  },
+  async created() {
+    apiAuth.logout();
+    this.returnUrl = this.$route.query.returnUrl || "/";
+    await new Promise((r) => setTimeout(r, 2000)).then(() => {
+      router.push(this.returnUrl);
+    });
+  },
+  components: { BasePublicTemplate },
 };
 </script> 
