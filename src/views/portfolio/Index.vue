e3e987bc45b39623b1d95cbcf99b37b1f183bac7
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-14 11:32:55 +0200

    src views portfolio Index.vue - wip: graphs

--- src/views/portfolio/Index.vue
+++ src/views/portfolio/Index.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="container mt-2">
-    <portfolio-list />
+    <div class="row">
+      <div class="col">
+        <portfolio-list />
+      </div>
+      <div class="col">
+        <div class="row">
+          <BaseChart></BaseChart>
+        </div>
+        <div class="row">
+          <!-- <BaseChart></BaseChart> -->
+        </div>
+      </div>
+      <!-- <div class="row"> -->
+      <!-- <div class="col">
+          <BaseChart></BaseChart>
+        </div> -->
+      <!-- </div> -->
+    </div>
   </div>
 </template>
 
 
 <script>
 import PortfolioList from "../../components/BasePortfolioList.vue";
+import BaseChart from "@/components/BaseChart.vue";
 
 export default {
   name: "PortfolioIndex",
@@ -15,6 +33,7 @@ export default {
   },
   components: {
     "portfolio-list": PortfolioList,
+    BaseChart,
   },
 };
 </script> 
