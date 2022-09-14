1e7102e1a07a6a4942a49e56e95e48dc8f202841
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-14 11:32:54 +0200

    test temperature.py - wip: graphs

--- test/temperature.py
+++ test/temperature.py
@@ -1,134 +1,60 @@
-import collections
+import json
+import random
+import requests
 
-from django.http import JsonResponse
-from rest_framework.views import APIView
+url = "http://localhost:8000/temperature"
 
-from backend.api.comm.comm import bytes_to_json
-from backend.api.cqrs_c.location import add, delete, update
-from backend.api.cqrs_q.location import get_user_portfolio
-from backend.api.mode.type_validator import _check_request_data
-from backend.api.view.comm import get_auth_ok_response_template
+def get_last():
+    t = requests.get(
+        f"{url}/",
+        data={
+            "section": "section_1",
+            "type": "type_1",
+            "portfolio": "portfolio_1",
+            "options": "last"
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def get_all():
+    t = requests.get(
+        f"{url}/",
+        data={
+            "section": "section_1",
+            "type": "type_1",
+            "portfolio": "portfolio_1"
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+
+def post(colour,portfolio_name, section, _type):
+
+    t = requests.post(
+        f"{url}/{colour}",
+        data={
+            "portfolio": portfolio_name,
+            "section": section,
+            "type": _type,
+
+            # "portfolio": portfolio
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
 
 
-#
-def validate_actions(correct_actions, to_check_actions):
-    return collections.Counter(correct_actions) == collections.Counter(
-        to_check_actions)
 
+def main():
+    # for i in range(10):
+    #     post(str(i * 7.5), "portfolio_1", "section_1", "type_1")
 
-class LocationView(APIView):
-    def patch(self, request, portfolio, section, _type):
-        print("LocationsView patch")
+    get_all()
+    get_last()
 
-        response = get_auth_ok_response_template(request)
-        response["payload"]["status"] = update(request.username, portfolio, section, _type, request.data)
-        return JsonResponse(response)
-
-
-    def delete(self, request, portfolio, section, _type):
-        print("delete single", portfolio, section, _type)
-
-        response = get_auth_ok_response_template(request)
-
-        r = delete(request.username,portfolio, section, _type)
-
-        response["payload"]["status"] = r
-        return JsonResponse(response)
-
-    def get(self, request, portfolio=None,section=None, _type=None):
-        print("location get")
-        response = get_auth_ok_response_template(request)
-
-        if portfolio and section and _type:
-            """fetch data for graphs"""
-
-
-        elif portfolio and not section and not _type:
-            print(f"get all locations for {portfolio=}")
-
-            username_locations = get_user_portfolio(request.username, portfolio)
-
-            r = {}
-            j = 0
-            for i in username_locations:
-                r[j] = {
-                    "section": i.section,
-                    "type": i.type,
-                    # todo refactor to latitude & longitude
-                    "lat": i.latitude,
-                    "lon": i.longitude,
-                }
-
-                j += 1
-
-            payload = {"status": True, "content": r}
-            result = payload
-
-            response["payload"] = result
-            return JsonResponse(response)
-
-        print("todo")
-
-        print(f"locations get {portfolio=} {section=} {_type=}")
-
-        # response = get_auth_ok_response_template(request)
-        #
-        # username_locations = get_user_portfolio(request.username,portfolio)
-        #
-        # r = {}
-        # j = 0
-        # for i in username_locations:
-        #     r[j] = {
-        #         "section": i.section,
-        #         "type": i.type,
-        #         # todo refactor to latitude & longitude
-        #         "lat": i.latitude,
-        #         "lon": i.longitude,
-        #     }
-        #
-        #     j += 1
-        #
-        # payload = {"status": True, "content": r}
-        # result = payload
-
-        # response["payload"] = result
-        return JsonResponse(response)
-
-    # todo add type & section as param
-    def post(self, request):
-        print("post locations")
-
-        response = get_auth_ok_response_template(request)
-
-        # print(f"{_check_request_data(request.data)=}")
-        # print(f"{request.synchronizer_token_match=}")
-
-        # todo
-        if (
-                # not _check_request_data(request.data) or
-                not request.synchronizer_token_match
-                # or not request.body
-        ):
-            print('add not valid')
-            payload = {"status": False}
-            print(request)
-            result = payload
-
-        else:
-
-            status = add(
-                request.username,
-                request.data["portfolio"],
-                request.data["section"],
-                request.data["type"],
-                request.data["latitude"],
-                request.data["longitude"]
-            )
-
-            r = status == "created"
-
-            payload = {"status": r, "reason": status}
-            result = payload
-
-        response["payload"] = result
-        return JsonResponse(response)
+if __name__ == '__main__':
+    main()
