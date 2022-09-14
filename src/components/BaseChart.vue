f659d3b4ff7b195dfa2e2fa7f7e89ce2f5a1056d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-14 11:32:54 +0200

    src components BaseChart.vue - wip: graphs

--- src/components/BaseChart.vue
+++ src/components/BaseChart.vue
@@ -1,75 +1,99 @@
 <template>
-  <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />
+  <!-- <div class="container-fluid"> -->
+  <!-- <div class="row"> -->
+  <!-- <div class="col"> -->
+
+  <div class="row">
+    <CChartLine :data="defaultData" />
+  </div>
+
+  <div class="row">
+    <CChartLine :data="defaultData" />
+  </div>
+
+  <div class="row">
+    <button @click="refreshCharts">refresh</button>
+  </div>
+
+  <!-- left -->
+  <!-- </div> -->
+  <!-- <div class="col"> -->
+  <!-- <CChartLine :data="defaultData" /> -->
+
+  <!-- right -->
+  <!-- </div>
+    </div>
+  </div> -->
 </template>
-  
-  <script>
-import { Bar } from "vue-chartjs";
-import {
-  Chart,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
 
-Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+<script>
+import { apiTemperature } from "@/scripts/api/temperature";
 
+import { CChartLine } from "@coreui/vue-chartjs";
 export default {
-  name: "BarChart",
-  components: { Bar },
-  props: {
-    chartId: {
-      type: String,
-      default: "bar-chart",
-    },
-    datasetIdKey: {
-      type: String,
-      default: "label",
-    },
-    width: {
-      type: Number,
-      default: 400,
-    },
-    height: {
-      type: Number,
-      default: 400,
-    },
-    cssClasses: {
-      default: "",
-      type: String,
-    },
-    styles: {
-      type: Object,
-      default: () => {},
-    },
-    plugins: {
-      type: Object,
-      default: () => {},
-    },
-  },
   data() {
     return {
-      chartData: {
-        labels: ["January", "February", "March"],
-        datasets: [{ data: [40, 20, 12] }],
-      },
-      chartOptions: {
-        responsive: true,
-      },
+      location: undefined,
     };
+  },
+
+  components: { CChartLine },
+  computed: {
+    defaultData() {
+      return {
+        // labels: [],
+        labels: ["months", "a", "b", "c", "d", "e", "f", "g"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "rgb(228,102,81,0.9)",
+            data: [30, 39, 10, 50, 30, 70, 35],
+          },
+          {
+            label: "Data Two",
+            backgroundColor: "rgb(0,216,255,0.9)",
+            data: [39, 80, 40, 35, 40, 20, 45],
+          },
+        ],
+      };
+    },
+  },
+  mounted() {
+    console.log("mounted");
+    // this.location = this.$store.state.location;
+    // console.log(this.location);
+  },
+
+  methods: {
+    async refreshCharts() {
+      console.log("refresh");
+
+      await this.fetchTemperature();
+    },
+
+    async fetchTemperature() {
+      this.location = this.$store.state.location;
+      console.log(this.location);
+
+      console.log("get temp");
+
+      let r = await apiTemperature.getAllTemperature(
+        this.location.portfolio,
+        this.location.section,
+        this.location.type
+      );
+
+      if (r["payload"]["status"]) {
+        console.log(r["payload"]["result"]);
+        // this.locations[portfolioPayload.oldName] = Object.values(
+        //   r["payload"]["content"]
+        // ).map((item) => {
+        //   return this.castLocation(item.section, item.type, item.lat, item.lon);
+        // });
+      } else {
+        this.showMessage(false, "", "Error fetching data, contact admin");
+      }
+    },
   },
 };
 </script>
-  
