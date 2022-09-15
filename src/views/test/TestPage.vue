6983786b0fc03831c3a58165e260a9d4dcf9eb69
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-15 19:10:15 +0200

    src views test TestPage.vue - wip: graphs

--- src/views/test/TestPage.vue
+++ src/views/test/TestPage.vue
@@ -1,42 +1,157 @@
 <template>
+  <br />
+  <hr />
+  <br />
+  <hr />
+  <br />
+  <hr />
+  <br />
+  <hr />
+  <br />
+  <hr />
+  <br />
+  <hr />
+
   <div class="container-fluid">
-    <div class="row">
-      <div class="col">
-        <CChartLine :data="defaultData" />
+    <ui-table
+      v-model="selectedRows"
+      fullwidth
+      :data="data"
+      :thead="thead"
+      :tbody="tbody"
+      :tfoot="tfoot"
+      row-checkbox
+      selected-key="id"
+    >
+      <template #th-dessert> Dessert </template>
+      <template #dessert="{ data }">
+        <div class="dessert">{{ data.dessert }}</div>
+      </template>
+      <template #actions="{ data }">
+        <ui-icon @click="show(data)">description</ui-icon>
+        <ui-icon @click="show(data)">edit</ui-icon>
+        <ui-icon @click="show(data)">delete</ui-icon>
+      </template>
 
-        <!-- left -->
-      </div>
-      <div class="col">
-        <!-- <CChartLine :data="defaultData" /> -->
-
-        <!-- right -->
-      </div>
-    </div>
-    <!-- <div class="row"></div> -->
+      <ui-pagination
+        v-model="page"
+        :total="total"
+        show-total
+        @change="onPage"
+      ></ui-pagination>
+    </ui-table>
   </div>
 </template>
 
 <script>
-import { CChartLine } from "@coreui/vue-chartjs";
 export default {
-  components: { CChartLine },
-  computed: {
-    defaultData() {
-      return {
-        labels: ["months", "a", "b", "c", "d"],
-        datasets: [
-          {
-            label: "Data One",
-            backgroundColor: "rgb(228,102,81,0.9)",
-            data: [30, 39, 10, 50, 30, 70, 35],
+  data() {
+    return {
+      data: [],
+      thead: [
+        {
+          value: "ID",
+          sort: "asc",
+          columnId: "id",
+        },
+        {
+          slot: "th-dessert",
+          class: "good",
+          sort: "none",
+          columnId: "dessert",
+        },
+        "Section",
+        "Type",
+        "Location",
+        "Update",
+        "Delete",
+      ],
+      tbody: [
+        "id",
+        {
+          slot: "dessert",
+        },
+        {
+          field: "calories",
+          numeric: true,
+          class: "test",
+        },
+        {
+          field: "fat",
+          fn: (data) => {
+            return data.fat.toFixed(1);
           },
-          {
-            label: "Data Two",
-            backgroundColor: "rgb(0,216,255,0.9)",
-            data: [39, 80, 40, 35, 40, 20, 45],
+        },
+        "carbs",
+        {
+          field: "protein",
+          class: (data) => {
+            return data.protein > 5 ? "red" : "green";
           },
-        ],
-      };
+        },
+        {
+          slot: "actions",
+        },
+      ],
+      tfoot: [
+        {
+          field: "id",
+          fnName: "count",
+        },
+        null,
+        {
+          field: "calories",
+          fnName: "sum",
+          align: "right",
+          class: "test",
+        },
+        {
+          field: "fat",
+          fnName: "avg",
+        },
+        {
+          field: "carbs",
+          fnName: "max",
+        },
+        {
+          field: "protein",
+          fnName: "min",
+        },
+      ],
+      selectedRows: [],
+      page: 1,
+      total: 12,
+    };
+  },
+  async created() {
+    this.data = [
+      {
+        id: 1,
+        dessert: "Frozen yogurt",
+        calories: 159,
+        fat: 6,
+        carbs: 24,
+        protein: 4,
+      },
+      {
+        id: 2,
+        dessert: "Ice cream sandwich",
+        calories: 237,
+        fat: 9,
+        carbs: 37,
+        protein: 4.3,
+      },
+    ];
+    // let { data } = await this.$http.get("/api/getData");
+    // this.data = { l1: { id: 1, dessert: "d", calories: 1 } };
+  },
+  methods: {
+    show(data) {
+      console.log(data);
+    },
+    onPage(page) {
+      // your code
+      console.log("on page", page);
     },
   },
 };
