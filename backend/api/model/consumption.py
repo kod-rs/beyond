af0bfc0e142a3861439390ff85d0c2e48715eef5
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-15 19:10:15 +0200

    backend api model consumption.py - wip: graphs

---
+++ backend/api/model/consumption.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from backend.api.model.location import Location
+
+
+class consumptionValues(models.Model):
+    # id
+    value     = models.FloatField()
+
+
+class consumptionEntry(models.Model):
+
+    # alt composite prim key
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+
+    value = models.ForeignKey(
+        consumptionValues,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    # value
+    timestamp = models.DateTimeField(auto_now_add=True)
