51a54bcf949bdd74f223ca85547d86e96b80891c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-17 18:12:27 +0200

    playground marin-jovanovic git.py - wip: ui

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -30,7 +30,7 @@ def main():
     comment = 'ref: map'
     comment = "wip: master detail"
     comment = "fix: remove colour from portfolio model"
-    comment = "fix: graph loading, ref: optimization"
+    comment = "wip: ui"
 
     if same_comment:
         if not comment:
