c2a4bf23f7db0d313e32977f176871bd61098709
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-17 12:24:32 +0200

    src components TheOptimization.vue - fix: graph loading, ref: optimization

---
+++ src/components/TheOptimization.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ locations }}
+    <br />
+    <hr />
+    <button @click="performOptimization">optimize selected</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    locations: Object,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    performOptimization() {
+      console.log("optimize with");
+      console.log(this.locations);
+      // todo make api call for this optimization and show message if optimization
+    },
+  },
+};
+</script>
+
+<style>
+</style>
