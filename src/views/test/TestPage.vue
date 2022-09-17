7f45dccaa30710ac12d73306346026bda675967f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-17 20:25:48 +0200

    src views test TestPage.vue - ref: map popup

--- src/views/test/TestPage.vue
+++ src/views/test/TestPage.vue
@@ -1,5 +1,19 @@
 <template>
+  <button @click="openLeft">click</button>
+
   <TheMapFullSize></TheMapFullSize>
+
+  <w-drawer v-model="openDrawer" left>
+    <!-- <w-button
+      @click="openDrawer = false"
+      sm
+      outline
+      round
+      absolute
+      icon="wi-cross"
+    >
+    </w-button> -->
+  </w-drawer>
 </template>
 
 <script>
@@ -10,7 +24,15 @@ export default {
     TheMapFullSize,
   },
   data() {
-    return {};
+    return {
+      openDrawer: false,
+    };
+  },
+  methods: {
+    openLeft() {
+      console.log("left");
+      this.openDrawer = !this.openDrawer;
+    },
   },
 };
 </script>
