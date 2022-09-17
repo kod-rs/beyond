3ed8d98a67dcb08defe609f9925d4255a19f084d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-17 21:44:29 +0200

    src views test TestPage.vue - wip: map popup

--- src/views/test/TestPage.vue
+++ src/views/test/TestPage.vue
@@ -1,34 +1,49 @@
 <template>
   <button @click="openLeft">click</button>
 
-  <TheMapFullSize></TheMapFullSize>
-
+  <TheMapFullSize ref="map"></TheMapFullSize>
+  <!-- <MapPopup ref="mappopup"></MapPopup> -->
+  <TheAddLocationPopup ref="mappopup"></TheAddLocationPopup>
   <w-drawer v-model="openDrawer" left>
-    <!-- <w-button
-      @click="openDrawer = false"
-      sm
-      outline
-      round
-      absolute
-      icon="wi-cross"
-    >
-    </w-button> -->
+    <hr />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <!-- <TheLocationAddForm @closePopup="closePopup"></TheLocationAddForm> -->
   </w-drawer>
 </template>
 
 <script>
 import TheMapFullSize from "@/components/TheMapFullSize.vue";
+// import TheLocationAddForm from "@/components/TheLocationAddForm.vue";
+import TheAddLocationPopup from "../../components/TheAddLocationPopup.vue";
 
 export default {
   components: {
     TheMapFullSize,
+    // TheLocationAddForm,
+    // MapPopup,
+    TheAddLocationPopup,
   },
   data() {
     return {
       openDrawer: false,
     };
   },
+  async mounted() {
+    let map = this.$refs.map.map;
+    this.bindPopupToMap(map);
+  },
   methods: {
+    bindPopupToMap(map) {
+      map.addOverlay(this.$refs.mappopup.getOverlay());
+      map.on("singleclick", this.$refs.mappopup.clickEvent);
+    },
+    closePopup() {
+      this.$refs.mappopup.closePopup();
+    },
     openLeft() {
       console.log("left");
       this.openDrawer = !this.openDrawer;
@@ -38,4 +53,12 @@ export default {
 </script>
 
 <style>
+w-drawer {
+  margin: 0;
+  height: 100%;
+  overflow: hidden;
+  min-height: 75rem;
+  /* padding-top: 4.5rem; */
+  padding-top: 3.5rem;
+}
 </style>
