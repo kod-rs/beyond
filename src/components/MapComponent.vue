0b2ca6c127d785e1109321d09f42ef59e23703b3
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-17 21:44:29 +0200

    src components MapComponent.vue - wip: map popup

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,58 +1,44 @@
 <template>
-    <div id="map"></div>
+  <div id="map"></div>
 </template>
 
 <script>
-
-import Map from 'ol/Map';
-import OSM from 'ol/source/OSM';
-import TileLayer from 'ol/layer/Tile';
-import View from 'ol/View';
+import Map from "ol/Map";
+import OSM from "ol/source/OSM";
+import TileLayer from "ol/layer/Tile";
+import View from "ol/View";
 export default {
-    data() {
-        return {
-            map: undefined,
-            // view: undefined
-        }
-    },
-    methods: {
-    },
-    mounted() {
-        // this.view = new View({
-        //     center: [0, 0],
-        //     zoom: 2,
-        // });
+  data() {
+    return {
+      map: undefined,
+    };
+  },
 
-        this.map = new Map({
-            layers: [
-                new TileLayer({
-                    source: new OSM(),
-                }),
-            ],
-            target: 'map',
-            view: new View({
-                center: [0, 0],
-                zoom: 2,
-            }),
-        });
+  mounted() {
+    this.map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: "map",
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
 
-        this.$store.dispatch("setMap", this.map);
-
-    }
-
-}
+    // this.$store.dispatch("setMap", this.map);
+  },
+};
 </script>
 
 
 <style>
-/* #map:focus {
-    outline: #4A74A8 solid 0.15em;
-} */
-
 #map {
-    width: 45%;
-    height: 90%;
-    position: fixed;
-    border: 1px solid #ccc;
+  width: 45%;
+  height: 90%;
+  position: fixed;
+  border: 1px solid #ccc;
 }
 </style>
