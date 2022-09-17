5969dccd3c9ac781b9adac64adbdab717e38f0d8
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-17 12:24:32 +0200

    playground marin-jovanovic git.py - fix: graph loading, ref: optimization

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -30,7 +30,7 @@ def main():
     comment = 'ref: map'
     comment = "wip: master detail"
     comment = "fix: remove colour from portfolio model"
-    comment = "wip: graphs"
+    comment = "fix: graph loading, ref: optimization"
 
     if same_comment:
         if not comment:
