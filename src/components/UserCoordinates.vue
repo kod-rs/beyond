856a859d21930e1f1ac583424a333f9d738769da
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-19 15:34:24 +0200

    src components UserCoordinates.vue - fix: location name. wip: navbar

--- src/components/UserCoordinates.vue
+++ src/components/UserCoordinates.vue
@@ -1,69 +1,70 @@
 <template>
-    <div></div>
-    <!-- your coordinates -->
-    <!-- <input type="text" v-model="lat" :name="lat" class="form-control" /> -->
-    <!-- <input type="text" v-model="lon" :name="lon" class="form-control" /> -->
-    <!-- <button @click="$emit('userCoordinates', this.lat, this.lon)">click me</button> -->
-    <!-- <button @click="emitCoordinates()">zoom on my location</button> -->
+  <div></div>
+  <!-- your coordinates -->
+  <!-- <input type="text" v-model="lat" :name="lat" class="form-control" /> -->
+  <!-- <input type="text" v-model="lon" :name="lon" class="form-control" /> -->
+  <!-- <button @click="$emit('userCoordinates', this.lat, this.lon)">click me</button> -->
+  <!-- <button @click="emitCoordinates()">zoom on my location</button> -->
 </template>
 
 <script>
-import { apiExternal } from '@/scripts/api/external';
-
+import { apiExternal } from "@/scripts/api/external";
 
 export default {
-    props: { "canSend": Boolean },
-    emits: ["userCoordinates"],
-    data() {
-        return {
-            lat: 0,
-            lon: 0,
-            canEmit: false
-        };
+  props: { canSend: Boolean },
+  emits: ["userCoordinates"],
+  data() {
+    return {
+      lat: 0,
+      lon: 0,
+      canEmit: false,
+    };
+  },
+  methods: {
+    enableCoordinates() {
+      this.canEmit = true;
     },
-    methods: {
-        enableCoordinates() {
-
-            this.canEmit = true;
-        },
-        emitCoordinates() {
-            if (this.canEmit) {
-                this.$emit('userCoordinates', this.lat, this.lon);
-            }
-        }
-        ,
-
-        displayUserCoordinatesWillingly() {
-            navigator.geolocation.getCurrentPosition(this.showPosition, this.showError, { timeout: 5000 });
-        },
-        async displayUserCoordinatesUnwillingly() {
-            let r = await apiExternal.getCoordinates();
-            this.showPosition({
-                coords: r
-            });
-        },
-        showPosition(position) {
-            this.$store.commit("setLatitude", position.coords.latitude);
-            this.$store.commit("setLongitude", position.coords.longitude);
-
-            this.lat = this.$store.state.latitude;
-            this.lon = this.$store.state.longitude;
-            this.emitCoordinates();
-        },
-        showError() {
-            this.displayUserCoordinatesUnwillingly();
-        }
+    emitCoordinates() {
+      if (this.canEmit) {
+        this.$emit("userCoordinates", this.lat, this.lon);
+      }
     },
-    mounted() {
-        this.emitCoordinates();
+    displayUserCoordinatesWillingly() {
+      navigator.geolocation.getCurrentPosition(
+        this.showPosition,
+        this.showError,
+        { timeout: 5000 }
+      );
+    },
+    async displayUserCoordinatesUnwillingly() {
+      let r = await apiExternal.getCoordinates();
+      this.showPosition({
+        coords: r,
+      });
+    },
+    showPosition(position) {
+      this.$store.dispatch("setUserCoordinates", {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
 
-        if (navigator.geolocation) {
-            this.displayUserCoordinatesWillingly();
-        } else {
-            alert("Geolocation is not supported by this browser");
-            this.displayUserCoordinatesUnwillingly();
-        }
+      this.lat = position.coords.latitude;
+      this.lon = position.coords.longitude;
+      this.emitCoordinates();
+    },
+    showError() {
+      this.displayUserCoordinatesUnwillingly();
+    },
+  },
+  mounted() {
+    this.emitCoordinates();
 
+    if (navigator.geolocation) {
+      this.displayUserCoordinatesWillingly();
+    } else {
+      alert("Geolocation is not supported by this browser");
+      this.displayUserCoordinatesUnwillingly();
     }
-}
+  },
+};
 </script>
