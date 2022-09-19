2c7ea8b21724c5aefb54a9a0fe68e1f45654ded9
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-19 15:34:24 +0200

    backend api model location.py - fix: location name. wip: navbar

--- backend/api/model/location.py
+++ backend/api/model/location.py
@@ -3,10 +3,11 @@ from backend.api.model.portfolio import Portfolio
 
 
 class Location(models.Model):
-    # alternative composite key
+    # alternative key
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200)
+
     section = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
-
     latitude = models.FloatField()
     longitude = models.FloatField()
