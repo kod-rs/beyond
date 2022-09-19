b021f17c804ea5cdce903fe9d0e85bc30ea52be7
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-19 12:05:00 +0200

    playground marin-jovanovic git.py - fix: notifications

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -30,7 +30,7 @@ def main():
     comment = 'ref: map'
     comment = "wip: master detail"
     comment = "fix: remove colour from portfolio model"
-    comment = "ref: "
+    comment = "fix: notifications"
 
     if same_comment:
         if not comment:
