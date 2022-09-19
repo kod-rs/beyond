6d624dc1224d35c42c6de56666d219bb3dadb48b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-19 12:05:00 +0200

    src views index IndexPage.vue - fix: notifications

--- src/views/index/IndexPage.vue
+++ src/views/index/IndexPage.vue
@@ -1,14 +1,51 @@
 <template>
-  <div>index</div>
+  <div>t</div>
 </template>
 
 <script>
+import { useToast } from "vue-toastification";
+
 export default {
+  setup() {
+    const toast = useToast();
+
+    toast("notif test");
+
+    return { toast };
+  },
   data() {
-    return {};
+    return {
+      notification: {
+        show: false,
+        position: ["bottom", "right"],
+      },
+    };
+  },
+  methods: {
+    successNotify(content) {
+      this.toast.success(content, {
+        timeout: 2000,
+      });
+    },
+
+    showMessage(result, successMessage, errorMessage) {
+      if (result) {
+        if (successMessage) {
+          this.toast.success(successMessage, {
+            timeout: 2000,
+          });
+        }
+      } else {
+        if (errorMessage) {
+          this.toast.error(errorMessage, {
+            timeout: 2000,
+          });
+        }
+      }
+    },
+    notify() {
+      this.showMessage(false, "t", "f");
+    },
   },
 };
 </script>
-
-<style>
-</style>
