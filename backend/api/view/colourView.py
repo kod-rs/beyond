9cd02311f778d99682339149f0b663abd7ff8055
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-19 15:34:25 +0200

    backend api view colourView.py - fix: location name. wip: navbar

--- backend/api/view/colourView.py
+++ backend/api/view/colourView.py
@@ -11,8 +11,7 @@ class ColourView(APIView):
 
     def delete(self, request, name):
         print(f"colour delete {name=}", request.data)
-        # de
-        # portfolio = request.data["portfolio"]
+
         response = get_auth_ok_response_template(request)
         response["payload"] = delete_portfolio_colour_entries(request.username, name)
         return JsonResponse(response)
@@ -28,27 +27,12 @@ class ColourView(APIView):
             print("last")
             response["payload"] = get_last_colour(request.username, name)
 
-        print(response)
-
-
-        # t = get_colour_log(request.username, name)
-        #
-        # r = {
-        #     str(i.timestamp):
-        #         i.colour.colour for i in t
-        # }
-
-        # response["payload"]["status"] = True
         return JsonResponse(response)
 
     def post(self, request, name):
         print(f"colour post {name=}")
-        # print(request.data)
         response = get_auth_ok_response_template(request)
 
         portfolio = request.data["portfolio"]
-        # colour_hex = request.data["colourHex"]
         response["payload"] =add_colour_to_log(request.username, portfolio, name)
-         # response
-
         return JsonResponse(response)
