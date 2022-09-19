4b68dbd521e3d30b57a07bdd631a2d8cca273fca
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-19 15:34:25 +0200

    backend api model consumption.py - fix: location name. wip: navbar

--- backend/api/model/consumption.py
+++ backend/api/model/consumption.py
@@ -5,11 +5,10 @@ from backend.api.model.location import Location
 
 class consumptionValues(models.Model):
     # id
-    value     = models.FloatField()
+    value = models.FloatField()
 
 
 class consumptionEntry(models.Model):
-
     # alt composite prim key
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
 
