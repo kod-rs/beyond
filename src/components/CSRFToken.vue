11499b24ac69c25f9fbf963471aa2766ce97459c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-19 15:34:25 +0200

    src components CSRFToken.vue - fix: location name. wip: navbar

--- src/components/CSRFToken.vue
+++ src/components/CSRFToken.vue
@@ -1,54 +1,39 @@
 <template>
-    <input hidden :value="content" @change="updateMyValue" />
+  <input hidden :value="content" @change="updateMyValue" />
 </template>
 
 <script>
-
-import { apiCsrf } from '../scripts/api/csrf';
+import { apiCsrf } from "../scripts/api/csrf";
 
 export default {
-    data() {
-        return {
-            content: '',
-        }
+  data() {
+    return {
+      content: "",
+    };
+  },
+  async mounted() {
+    this.refresh();
+  },
+  methods: {
+    async refresh() {
+      const r = await apiCsrf.getCSRFAuthData();
+      if (r["auth"]["status"]) {
+        this.content = r["payload"]["synchronizer_token"];
+      } else {
+        this.content = "";
+
+        alert(
+          "error generating SynchronizerToken, you will not be able to submit form, contact admin"
+        );
+      }
+
+      this.$store.dispatch("setSynchronizerToken", this.content);
     },
-    async mounted() {
-        const r = await apiCsrf.getCSRFAuthData();
-        if (r["auth"]["status"]) {
 
-            this.content = r["payload"]["synchronizer_token"]
-            this.$store.commit('setSynchronizerToken', this.content)
-
-        } else {
-
-            this.content = ""
-            this.$store.commit('setSynchronizerToken', '')
-
-            alert("error generating SynchronizerToken, you will not be able to submit form, contact admin")
-        }
+    updateMyValue(event) {
+      this.content = event.target.value.trim();
+      this.$store.dispatch("setSynchronizerToken", this.content);
     },
-    methods: {
-        async refresh() {
-            const r = await apiCsrf.getCSRFAuthData();
-            if (r["auth"]["status"]) {
-
-                this.content = r["payload"]["synchronizer_token"]
-                this.$store.commit('setSynchronizerToken', this.content)
-
-            } else {
-
-                this.content = ""
-                this.$store.commit('setSynchronizerToken', '')
-
-                alert("error generating SynchronizerToken, you will not be able to submit form, contact admin")
-            }
-
-        },
-
-        updateMyValue(event) {
-            this.content = event.target.value.trim()
-            this.$store.commit('setSynchronizerToken', this.content)
-        }
-    }
+  },
 };
 </script>
