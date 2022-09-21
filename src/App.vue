ff2300c202c5040d9eaab2dd4a47ee27b1e173b5
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-09-21 19:07:03 +0200

    src App.vue - fix: remove navbar from public pages

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
 <template>
-  <TopNavigationBar></TopNavigationBar>
-  <w-app>
-    <div>
-      <div class="nonscrollable">
-        <router-view> </router-view>
-      </div>
-    </div>
-  </w-app>
+  <router-view> </router-view>
 </template>
 
 <style>
-html,
-body {
-  margin: 0;
-  height: 100%;
-  /* overflow: hidden; */
-}
-
-/* Show it is fixed to the top */
-body {
-  min-height: 75rem;
-  /* padding-top: 4.5rem; */
-  padding-top: 3.5rem;
-}
-
 @import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
 @import "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css";
 @import "https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css";
@@ -34,23 +13,22 @@ body {
 <script>
 import { router } from "./router/router";
 import { INACTIVE_THRESHOLD, TIME_RESOLUTION } from "./scripts/constants";
-
 import { activate_tab_name_changer } from "./scripts/tab_name_changer";
 import { apiAuth } from "./scripts/api/auth";
-import TopNavigationBar from "./components/TopNavigationBar.vue";
 
 export default {
-  name: "app",
   data() {
     return {
-      isPublic: sessionStorage.getItem("user") !== null,
       isInactive: false,
       userActivityThrottlerTimeout: null,
       userActivityTimeout: null,
-      type: 1,
     };
   },
   mounted() {
+    // console.log(this.$route.path);
+
+    // console.log(routerHelper.isPublicPath(this.$route.path));
+
     // this.importScript("sweetalert2.all.min.js");
 
     this.importScript(
@@ -70,7 +48,6 @@ export default {
     activate_tab_name_changer();
   },
   methods: {
-    setIsPublic() {},
     importScript(path) {
       let scriptElement = document.createElement("script");
       scriptElement.setAttribute("src", path);
@@ -125,6 +102,5 @@ export default {
     clearTimeout(this.userActivityThrottlerTimeout);
     window.sessionStorage.removeItem("user");
   },
-  components: { TopNavigationBar },
 };
 </script>
