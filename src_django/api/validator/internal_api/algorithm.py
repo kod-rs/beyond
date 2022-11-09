01aba819af49efbfebc1b3f5ded88c9c92ad24ba
Ivan <ivan.cindric95@gmail.com>
2022-11-09 15:55:52 +0100

    src django api validator internal api - rename input variable and change return

--- src_django/api/validator/internal_api/algorithm.py
+++ src_django/api/validator/internal_api/algorithm.py
@@ -1,8 +1,7 @@
 from src_django.api.validator import common
 
 
-def validate_algorithm_request(credentials: dict) -> bool:
+def validate_algorithm_request(data: dict) -> bool:
     yaml_file_path = (common.internal_api_dir /
                       'algorithm_request.yaml')
-    abc = common.validate(yaml_file_path, credentials)
-    return abc
+    return common.validate(yaml_file_path, data)
--- src_django/api/validator/internal_api/login.py
+++ src_django/api/validator/internal_api/login.py
@@ -1,7 +1,7 @@
 from src_django.api.validator import common
 
 
-def validate_internal_login(credentials: dict) -> bool:
+def validate_internal_login(data: dict) -> bool:
     yaml_file_path = (common.internal_api_dir /
                       'login_request.yaml')
-    return common.validate(yaml_file_path, credentials)
+    return common.validate(yaml_file_path, data)
