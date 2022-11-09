aed1620ed1841ede8874b74a42f8cc3880d63868
Ivan <ivan.cindric95@gmail.com>
2022-11-09 15:54:52 +0100

    src django api view - add flexibility demand

--- src_django/api/view/__init__.py
+++ src_django/api/view/__init__.py
@@ -1,3 +1,4 @@
 from src_django.api.view.algorithm import AlgorithmView
 from src_django.api.view.building import BuildingView
+from src_django.api.view.flexibility_demand import FlexibilityDemandView
 from src_django.api.view.login import LoginView
--- src_django/api/view/flexibility_demand.py
+++ src_django/api/view/flexibility_demand.py
@@ -1,22 +1,35 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from src_django.api.validator.internal_api.algorithm import \
-    validate_algorithm_request
+from src_django.api.validator.external_api.flexibility_demand_response import \
+    validate_flexibility_demand_response
+from src_django.api.validator.internal_api.flexibility_demand_request import \
+    validate_flexibility_demand_request
 from src_django.api.view import common
 
 
-class AlgorithmView(APIView):
+class FlexibilityDemandView(APIView):
     def __init__(self):
         super().__init__()
         self._request_type = 'flexibility_demand_request'
         self._response_type = 'flexibility_demand_response'
 
+        self._beyond = common.BeyondConnection()
+
     def post(self, request) -> JsonResponse:
         request_body = common.json_decode(request.body)
-        if not validate_algorithm_request(request_body):
+
+        if not validate_flexibility_demand_request(request_body):
             return common.false_status(msg='invalid request',
                                        response_type=self._response_type)
 
+        beyond_data = self._beyond.req_flex_demand(request_body['type'],
+                                                   request_body['date'])
+
+        if not validate_flexibility_demand_response(beyond_data):
+            return common.false_status(msg='request failed',
+                                       response_type=self._response_type)
+
         return JsonResponse({'type': self._response_type,
-                             'status': True})
+                             'status': True,
+                             'demands': beyond_data['demands']})
