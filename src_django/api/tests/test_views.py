a7adce9a806d85cc34e879cdfe6611948fe35c74
Ivan <ivan.cindric95@gmail.com>
2022-11-10 11:30:51 +0100

    src django api tests test views - fix, refactor

--- src_django/api/tests/test_views.py
+++ src_django/api/tests/test_views.py
@@ -70,7 +70,7 @@ class TestLocationView(TestCase):
         response = response.json()
 
         assert response['type'] == 'buildings_by_user_id_response'
-        assert len(response['buildings']) == 100
+        assert len(response['buildings']) > 0
         assert isinstance(response['buildings'][0]['building_id'], str)
         assert isinstance(response['buildings'][0]['building_name'], str)
         assert isinstance(response['buildings'][0]['longitude'], float)
@@ -78,8 +78,9 @@ class TestLocationView(TestCase):
 
     def test_get_building_info(self):
         client = Client()
+
         data = {'type': 'building_info_request',
-                'building_ids': ['b1', 'b2', 'b3']}
+                'building_ids': mocks.ids[:3]}
 
         response = client.post('/buildings/',
                                json.dumps(data),
@@ -88,9 +89,34 @@ class TestLocationView(TestCase):
 
         assert response['type'] == 'building_info_response'
         assert len(response['buildings_info']) != 0
-        assert len(response['buildings_info'][0]['info']) != 0
-        timeseries = response['buildings_info'][0]['info'][0]
+        assert len(response['buildings_info'][0]['energy_info']) != 0
+        timeseries = response['buildings_info'][0]['energy_info'][0]
         assert set(timeseries.keys()) == {'value', 'timestamp'}
+
+
+class TestFlexibilityDemand(TestCase):
+    def setUp(self):
+        common.BeyondConnection.req_flex_demand = MagicMock(
+            side_effect=mocks.mock_get_flexibility_demand)
+
+    def test_flexibility_demand(self):
+        client = Client()
+        date = datetime.datetime(year=2022, month=4, day=10, hour=0)
+        date = date.replace(tzinfo=datetime.timezone.utc).isoformat()
+        data = {'type': 'flexibility_demand_request',
+                'date': date}
+
+        response = client.post('/flexibility_demand/',
+                               json.dumps(data),
+                               content_type="application/json")
+        response = response.json()
+        assert response['type'] == 'flexibility_demand_response'
+        assert response['status'] is True
+        assert isinstance(response['demands'], list)
+        assert len(response['demands']) > 0
+        assert set(response['demands'][0].keys()) == {'end_time',
+                                                      'flexibility',
+                                                      'start_time'}
 
 
 class TestAlgorithmView(TestCase):
@@ -117,28 +143,3 @@ class TestAlgorithmView(TestCase):
         assert (isinstance(response['offered_flexibility'], float)
                 or isinstance(response['offered_flexibility'], int))
         assert isinstance(response['building_info'], list)
-
-
-class TestFlexibilityDemand(TestCase):
-    def setUp(self):
-        common.BeyondConnection.req_flex_demand = MagicMock(
-            side_effect=mocks.mock_get_flexibility_demand)
-
-    def test_flexibility_demand(self):
-        client = Client()
-        date = datetime.datetime(year=2022, month=4, day=10, hour=0)
-        date = date.replace(tzinfo=datetime.timezone.utc).isoformat()
-        data = {'type': 'flexibility_demand_request',
-                'date': date}
-
-        response = client.post('/flexibility_demand/',
-                               json.dumps(data),
-                               content_type="application/json")
-        response = response.json()
-        assert response['type'] == 'flexibility_demand_response'
-        assert response['status'] is True
-        assert isinstance(response['demands'], list)
-        assert len(response['demands']) > 0
-        assert set(response['demands'][0].keys()) == {'end_time',
-                                                      'flexibility',
-                                                      'start_time'}
