600f5dc11d1449f59b06b62d4c378db70e8b2e97
Ivan <ivan.cindric95@gmail.com>
2022-12-02 10:24:33 +0100

    docker backend - remove files

--- docker/backend/docker_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo cp -R ../../src_django ./src_django/
-sudo cp -R ../../schemas ./schemas/
-sudo cp ../../manage.py ./manage.py
-sudo cp ../../requirements.pip.txt ./requirements.pip.txt
-sudo cp ../../.env ./.env
-sudo docker build . -t backend
