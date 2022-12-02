3d3cdf98c8b945ece68e07634d0dbd88d311f135
Ivan <ivan.cindric95@gmail.com>
2022-12-02 10:25:05 +0100

    docker - update

---
+++ docker/docker_builder.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from distutils.dir_util import copy_tree
+from pathlib import Path
+from shutil import copyfile, rmtree
+
+
+def copy_dir(src, dst, dir_name):
+    copy_tree(str(src / dir_name), str(dst / dir_name))
+
+
+def copy_file(src, dst, file_name):
+    copyfile(str(src / file_name), str(dst / file_name))
+
+
+def rm_dir(dst, dir_name):
+    rmtree(str(dst / dir_name))
+
+
+def rm_file(dst, file_name):
+    os.remove(str(dst / file_name))
+
+
+def copy_to_backend(parent_dir):
+    src = parent_dir.parent
+    dst = parent_dir / 'backend'
+
+    copy_dir(src, dst, 'src_django')
+    copy_dir(src, dst, 'schemas')
+    copy_file(src, dst, 'manage.py')
+    copy_file(src, dst, 'requirements.pip.txt')
+    copy_file(src, dst, '.env')
+
+
+def copy_to_frontend(parent_dir):
+    src = parent_dir.parent
+    dst = parent_dir / 'frontend'
+
+    copy_dir(src, dst, 'src_front')
+
+    dst = dst / 'src_front'
+    rm_dir(dst, '.vs')
+    rm_dir(dst, 'node_modules')
+    rm_file(dst, 'package-lock.json')
+
+
+def main():
+    parent_dir = Path(__file__).resolve().parent
+    copy_to_backend(parent_dir)
+    copy_to_frontend(parent_dir)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
