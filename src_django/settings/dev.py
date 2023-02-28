a5005d24b5ecad595b804fe2b16cdb423c5a656e
Domagoj <domamaric20@gmail.com>
2023-02-28 15:48:48 +0100

    src django dev - add production options

--- src_django/settings/dev.py
+++ src_django/settings/dev.py
@@ -21,10 +21,48 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_2h5+q8a^%(6b^(pd#rt7i2m)0tjvie9-f*qb_+x7#$9kz(cwa'
+
+with open('/home/byte/Desktop/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
+
+DEVELOPMENT_MODE = False
+
+# SECURITY WARNING: Set this to True to avoid transmitting the
+# session cookie over HTTP accidentally.
+
+SESSION_COOKIE_SECURE = True
+
+# SECURITY WARNING: Set this to True to avoid transmitting the CSRF cookie over
+# HTTP accidentally.
+
+CSRF_COOKIE_SECURE = True
+
+# SECURITY WARNING: This setting is required to protect your site against some
+# CSRF attacks. If you use a wildcard, you must perform your own validation
+# of the Host HTTP header, or otherwise ensure that you aren’t vulnerable to
+# this category of attacks (e.g ['www.mysite.hr']). This value defaults to
+# localhost.
+
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: If True, the SecurityMiddleware redirects all non-HTTPS
+# requests to HTTPS.
+
+SECURE_SSL_REDIRECT = False
+
+# SECURITY WARNING: For sites that should only be accessed over HTTPS, you
+# can instruct modern browsers to refuse to connect to your domain name via
+# an insecure connection (for a given period of time). When enabling HSTS,
+# it’s a good idea to first use a small value for testing, for example,
+# SECURE_HSTS_SECONDS = 3600 for one hour. Each time a web browser sees the
+# HSTS header from your site, it will refuse to communicate non-securely
+# (using HTTP) with your domain for the given period of time.
+
+SECURE_HSTS_SECONDS = 0
 
 # Application definition
 
