ebd3cb6922b0a1945ee4c2ac0ecea0dc4d7d3085
Domagoj <domamaric20@gmail.com>
2023-02-28 15:29:56 +0100

    src django api middleware - add temporary login solution

--- src_django/api/middleware/session.py
+++ src_django/api/middleware/session.py
@@ -1,9 +1,6 @@
-import datetime
-
 from django.utils import timezone
 
 from src_django.api import common
-from src_django.api.controller import user_sess
 
 
 class CustomSessionMiddleware:
@@ -13,19 +10,29 @@ class CustomSessionMiddleware:
 
     def __call__(self, request):
         request_body = common.json_decode(request.body)
+        # if request_body.get('type') in common.token_types:
+        #     if not request_body.get('access_token'):
+        #         return common.false_status(self._mapping[request_body.get('type')], 'token fail')
+        #     # read from db GET EXP_TIME WHERE TOKEN = REQ(TOKEN)) (kroz dj)
+        #     sess_data = user_sess.get_by_token(
+        #         user_token=request_body['access_token'])
+        #
+        #     if not sess_data:
+        #         return common.false_status(self._mapping[request_body.get('type')], 'token fail')
+        #
+        #     sess_expiry, sess_start = sess_data
+        #     expiry_time = sess_start + datetime.timedelta(seconds=sess_expiry)
+        #     if timezone.now() >= expiry_time:
+        #         return common.false_status(self._mapping[request_body.get('type')], 'session expired')
+        # # Ako je apsolutno sve okej onda vracam zadnju liniju
+        # NOTE: temporary fix, TODO permanent fix (uncomment the above)
+        if request_body.get('type') == 'login_request':
+            from src_django.api.controller import user_sess
+            user_sess.add(user_token='usr_tkn',
+                          expires_in=5000,
+                          session_start=timezone.now())
         if request_body.get('type') in common.token_types:
-            if not request_body.get('access_token'):
-                return common.false_status(self._mapping[request_body.get('type')], 'token fail')
-            # read from db GET EXP_TIME WHERE TOKEN = REQ(TOKEN)) (kroz dj)
-            sess_data = user_sess.get_by_token(
-                user_token=request_body['access_token'])
-
-            if not sess_data:
-                return common.false_status(self._mapping[request_body.get('type')], 'token fail')
-
-            sess_expiry, sess_start = sess_data
-            expiry_time = sess_start + datetime.timedelta(seconds=sess_expiry)
-            if timezone.now() >= expiry_time:
-                return common.false_status(self._mapping[request_body.get('type')], 'session expired')
-        # Ako je apsolutno sve okej onda vracam zadnju liniju
+            import json
+            abc = {**request_body, 'access_token': 'usr_token'}
+            request._body = json.dumps(abc)
         return self.get_response(request)
