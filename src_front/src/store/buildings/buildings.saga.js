bf01d9726a57322a77a67962648caf6e2ca03b9c
bodonat <48962310+bodonat@users.noreply.github.com>
2023-07-11 10:07:04 +0200

    Update buildings.saga.js
    added functions for generating random colors
    added additional attribute "color" for buildings

--- src_front/src/store/buildings/buildings.saga.js
+++ src_front/src/store/buildings/buildings.saga.js
@@ -16,12 +16,16 @@ called by the saga middleware. */
 export function* getBuildingsForCurrentUser({ payload: { user } }) {
     try {
         yield put(setIsLoadingBuildings(true));
-        let buildings = yield call(
+        let buildingsWrapper = yield call(
             getBuildingsForUser,
             user
         );
-        if (buildings) {
-            yield put(getBuildingsSuccess(buildings.buildings));
+        if (buildingsWrapper) {
+            let colors = generateRandomColors(buildingsWrapper.buildings.length);
+            buildingsWrapper.buildings.forEach((element, index) => {
+                element.color = colors[index];
+            });
+            yield put(getBuildingsSuccess(buildingsWrapper.buildings));
         } else {
             yield put(getBuildingsFailed("Buildings for user " + user.displayName + " were null!"));
         }
@@ -30,6 +34,32 @@ export function* getBuildingsForCurrentUser({ payload: { user } }) {
     }
 }
 
+/* A function that generates "n" random colors */
+function generateRandomColors(n) {
+    var colors = [];
+  
+    while (colors.length < n) {
+      var color = generateRandomColor();
+  
+      if (!colors.includes(color)) {
+        colors.push(color);
+      }
+    }
+  
+    return colors;
+  }
+
+/* A function that generates random color */
+function generateRandomColor() {
+var webSafeColors = [
+  "#800000", "#e6194B", "#f58231", "#ffe119", "#bfef45", "#42d4f4",
+  "#911eb4", "#f032e6", "#469990", "#9A6324", "#3cb44b", "#4363d8",
+  "#FF33FF", "#FFFF00"
+];
+
+var randomIndex = Math.floor(Math.random() * webSafeColors.length);
+return webSafeColors[randomIndex];
+}
 
 /* A generator function that is called by the saga middleware. It is a generator function that is
 called by the saga middleware.*/
